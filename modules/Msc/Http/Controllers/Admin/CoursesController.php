8beec14ff95609d5372a15024506a06b07c46d80
gaoshichong <gaoshichong@163.com>
2015-12-16 18:46:29 +0800

    解决错误

--- modules/Msc/Http/Controllers/Admin/CoursesController.php
+++ modules/Msc/Http/Controllers/Admin/CoursesController.php
@@ -1887,7 +1887,7 @@ class CoursesController extends MscController
                 'total'                  =>    40,
                 'unabsence'              =>    39,
             ];
-            dd($data);
+
             //PC-Admin-002-课程监管.png
             return view('msc::admin.coursemanage.course_observe_detail',$data);
         }catch (\Exception $ex){
