67f653b43b068cc4a0d4985ef895e7521e17b685
limingyao <limingyao@misrobot.com>
2015-12-17 10:26:55 +0800

    更新数据库连接配置文件为远程本地数据库连接

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'msc_mis' => [
             'driver'    => 'mysql',
-			'host'      => env('DB_HOST_MSC', '192.168.0.254'),
+			'host'      => env('DB_HOST_MSC', 'localhost'),
             'database'  => env('DB_DATABASE_MSC', 'msc_mis'),
-            'username'  => env('DB_USERNAME_MSC', 'msc_mis'),
-            'password'  => env('DB_PASSWORD_MSC', 'msc_mis'),
+            'username'  => env('DB_USERNAME_MSC', 'limingyao'),
+            'password'  => env('DB_PASSWORD_MSC', 'limingyao123'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -65,10 +65,10 @@ return [
         ],
         'sys_mis' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST_SYS', '192.168.0.254'),
+            'host'      => env('DB_HOST_SYS', 'localhost'),
             'database'  => env('DB_DATABASE_SYS', 'sys_mis'),
-			'username'  => env('DB_USERNAME_SYS', 'sys_mis'),
-			'password'  => env('DB_PASSWORD_SYS', 'sys_mis'),
+			'username'  => env('DB_USERNAME_SYS', 'limingyao'),
+			'password'  => env('DB_PASSWORD_SYS', 'limingyao123'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -128,22 +128,19 @@ return [
 
         //队列
         'queue' => [
-            'host'     => '192.168.0.254',
-//			'host'     => '127.0.0.1',
+			'host'     => '127.0.0.1',
             'port'     => 6379,
             'database' => 0,
         ],
 
         'cache' => [
-            'host'     => '192.168.0.254',
-//			'host'     => '127.0.0.1',
+			'host'     => '127.0.0.1',
             'port'     => 6379,
             'database' => 1,
         ],
 
         'session' => [
-            'host'     => '192.168.0.254',
-//			'host'     => '127.0.0.1',
+			'host'     => '127.0.0.1',
             'port'     => 6379,
             'database' => 2,
         ],
