773d50d4f69ce03b08c4d2febfbeb443ccb88222
zhouqiang <candyphp@163.com>
2015-12-18 20:00:30 +0800

    cesi

--- modules/Msc/Http/Controllers/Admin/UserController.php
+++ modules/Msc/Http/Controllers/Admin/UserController.php
@@ -809,12 +809,14 @@ class UserController extends MscController
 
     public function getExportStudentUser(Request $request)
     {
-//        dd($request);
-//        dd($keyword);exit;
+//        dd($request->keyword);
+//        if($request->keyword){
+//
+//        }
         //同步学生列表的数据
         $studentInfo = $this->getStudentList($request);
-//        var_dump($studentInfo);
-        dd($studentInfo->list);
+
+//        dd($studentInfo->list);
 
         $str = iconv('utf-8', 'gb2312', '序号,姓名,学号,年级,类别,专业,手机号,证件号,性别,状态') . "\n";
         if (empty($studentInfo->list)) {
@@ -836,6 +838,8 @@ class UserController extends MscController
         }
         $filename = date('Ymd') . '.csv';
         $this->export_csv($filename, $str);
+
+//        return 'res=1';
 
     }
 
