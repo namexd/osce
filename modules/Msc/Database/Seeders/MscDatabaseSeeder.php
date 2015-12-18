1959d167070e8c758d43cc724bf780724fc17ae0
limingyao <limingyao@misrobot.com>
2015-12-18 16:24:05 +0800

    更新msc模块的seeder到模块下。

--- modules/Msc/Database/Seeders/MscDatabaseSeeder.php
+++ modules/Msc/Database/Seeders/MscDatabaseSeeder.php
@@ -13,7 +13,24 @@ class MscDatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
+		
 		Model::unguard();
+		$this   ->  call(ResourcesLabSeeder::class);
+		$this   ->  command ->  info('lab Data Creating......');
+        $this   ->  call(StudentProfessionalSeeder::class);
+		$this   ->  command ->  info('Professional Data Creating......');
+        $this   ->  call(TeacherDeptSeeder::class);
+        $this   ->  command ->  info('TeacherDept Data Creating......');
+        $this   ->  call(GroupsSeeder::class);
+        $this   ->  command ->  info('Group Data Creating......');
+        $this   ->  call(StudentGroupSeeder::class);
+        $this   ->  command ->  info('StudentGroup Data Creating......');
+        $this   ->  call(StudentClassSeeder::class);
+        $this   ->  command ->  info('StudentClass Data Creating......');
+        $this   ->  call(TeacherSeeder::class);
+        $this   ->  command ->  info('Teacher Data Creating......');
+		Model::reguard();
+		
 
 	}
 }
