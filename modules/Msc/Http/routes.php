b0e2f05ddd0b488a25dd7b24aa80d1f619e34703
weihuiguo <weihuiguo@misrobot.com>
2015-12-18 18:00:10 +0800

    路由报错修改

--- modules/Msc/Http/routes.php
+++ modules/Msc/Http/routes.php
@@ -102,7 +102,7 @@ Route::group(['prefix' => "msc",'namespace' => 'Modules\Msc\Http\Controllers','m
 		Route::get('lab/agree-emergency-apply',['uses'=>'LabController@getAgreeEmergencyApply','as'=>'msc.admin.lab.getAgreeEmergencyApply']);
 		Route::post('lab/refund-emergency-apply',['uses'=>'LabController@postRefundEmergencyApply','as'=>'msc.admin.lab.postRefundEmergencyApply']);
 		Route::get('lab/urgent-apply-list',['uses'=>'LabController@getUrgentApplyList','as'=>'msc.admin.lab.getUrgentApplyList']);//TDDO ：luohaihua 以当前别名为准
-		Rote:Route::get('lab/had-open-lab-list',['uses'=>'LabController@hadOpenlabList','as'=>'msc.admin.lab.hadOpenlablist']););
+		Rote:Route::get('lab/had-open-lab-list',['uses'=>'LabController@hadOpenlabList','as'=>'msc.admin.lab.hadOpenlablist']);
 		Route::get('lab/open-lab-apply-list',['uses'=>'LabController@getOpenLabApplyList','as'=>'msc.admin.lab.openLabApplyList']);
 		Route::post('lab/import-lab',['uses'=>'LabController@postImportLab','as'=>'msc.admin.lab.postImportLab']);
 		Route::post('lab/add-lab',['uses'=>'LabController@postAddLab','as'=>'msc.admin.lab.postAddLab']);//唐俊
