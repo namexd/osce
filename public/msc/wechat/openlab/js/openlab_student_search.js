56a9830f63a5c0d63e9b0390fa542965735c2e5f
luohaihua <luohaihua@misrobot.com>
2015-12-22 20:26:34 +0800

    修复BUG622

--- public/msc/wechat/openlab/js/openlab_student_search.js
+++ public/msc/wechat/openlab/js/openlab_student_search.js
@@ -31,7 +31,6 @@ function gethistory(qj,url,getdetail){
                     }
                 }else if(this.status=="1"){
                     status="";
-                    alert();
                     opera='<div class="opera"><span  class="State3">已被预订</span></div>';
                 }
 
