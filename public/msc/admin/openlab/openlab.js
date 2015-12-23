b3343d25664882ad8bf216d890e11f5f889c5a73
May <wulengmei@misrobot.com>
2015-12-23 10:47:07 +0800

    修改插件验证

--- public/msc/admin/openlab/openlab.js
+++ public/msc/admin/openlab/openlab.js
@@ -22,7 +22,7 @@ function lab_add(){
         values: ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"],
         type: "double",
         from:start,
-        to:start,
+        to:end,
         hasGrid: true,
         onChange: function(obj){        // function-callback, is called on every change
 
