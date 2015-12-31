f09ccf0f40d03e0fa67698631a73d37fb17befaf
mao <maoyungang@misrobot.com>
2015-12-31 11:39:22 +0800

    样式调整

--- modules/Osce/Http/routes.php
+++ modules/Osce/Http/routes.php
@@ -20,7 +20,12 @@ Route::group(['prefix' => "osce", 'namespace' => 'Modules\Osce\Http\Controllers'
         //设备
 		Route::get('machine/category-list', 	['uses'=>'MachineController@getCategoryList','as'=>'osce.admin.machine.getCategoryList']);
 		//考场
+		Route::get('place/place-list', 	['uses'=>'PlaceController@getPlaceList','as'=>'osce.admin.Place.getPlaceList']);
 		Route::get('place/edit-place', 	['uses'=>'PlaceController@getEditPlace','as'=>'osce.admin.Place.getEditPlace']);
+		Route::get('place/add-place', 	['uses'=>'PlaceController@getAddPlace','as'=>'osce.admin.Place.getAddPlace']);
+		//测试
+		Route::get('place/test', 	['uses'=>'PlaceController@getTest','as'=>'osce.admin.Place.getTest']);
+
 	});
 	//TODO:请前端开发尽早删除这个测试的/test路由。罗海华  2015-12-29 18:31
 	Route::get('/test','TestController@test');
