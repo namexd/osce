912bddefb1cdc347a69eb8048a3d17e10f389134
lizhiyuan <lizhiyuan@misrobot.com>
2016-01-07 17:05:57 +0800

    微信部分页面

---
+++ modules/Osce/Resources/views/wechat/exammanage/exam_notice.blade.php
@@ -0,0 +1,63 @@
+@extends('osce::wechat.layouts.admin')
+
+@section('only_head_css')
+    <link rel="stylesheet" href="{{asset('osce/wechat/personalcenter/css/documentation.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('osce/wechat/personalcenter/css/jalendar.css')}}" type="text/css" />
+    <style type="text/css">
+        .title{
+            font-size: 16px;
+            font-weight: 700;
+            margin-bottom: 10px;
+        }
+        .history-box{
+            text-align: left;
+        }
+        .history-list li{
+            background-color: #fff;
+            padding: 15px;
+            margin: 10px;
+            border-radius: 4px;
+            border: 1px solid #eee;
+        }
+        .year{
+            margin-right: 20px;
+        }
+        .time{
+            color: #999;
+        }
+    </style>
+
+
+@stop
+@section('only_head_js')
+    <script type="text/javascript" src="{{asset('osce/wechat/personalcenter/js/jalendar.js')}}"></script>
+    <script type="text/javascript">
+
+    </script>
+
+@stop
+
+
+@section('content')
+    <div class="user_header">
+        <a class="left header_btn" href="javascript:history.back(-1)">
+            <i class="fa fa-angle-left clof font26 icon_return"></i>
+        </a>
+        资讯&通知
+        <a class="right header_btn" href="">
+            <i class="fa fa-home clof font26 icon_return"></i>
+        </a>
+    </div>
+    <div class="history-box">
+        <ul class="history-list">
+            <li>
+                <p class="title">OSCE考试2015年第3期考试考前通知</p>
+                <p class="time"><span class="year">2015-12-1</span><span>12:00:00</span></p>
+            </li>
+            <li>
+                <p class="title">OSCE考试2015年第3期考试考前通知</p>
+                <p class="time"><span class="year">2015-12-1</span><span>12:00:00</span></p>
+            </li>
+        </ul>
+    </div>
+@stop
