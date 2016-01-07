3e942111203622d8e2b922ce9a5e00ae4ab21f41
zengjie <zengjie@misrobot.com>
2016-01-07 10:58:24 +0800

    实验室资源维护楼栋一直跳转bug

--- modules/Msc/Resources/views/admin/labmanage/resource_maintain.blade.php
+++ modules/Msc/Resources/views/admin/labmanage/resource_maintain.blade.php
@@ -2,6 +2,7 @@
 @section('only_css')
     <style>
         label{margin-bottom: 0;}
+        .treeview .lab_num{background-color: #f5f5f5;}
     </style>
 @stop
 
@@ -12,19 +13,22 @@
             $(document).ajaxSuccess(function(event, request, settings) {
                 //楼栋选项卡切换
                 ban();
+                //实验室数据加载
+                labdata();
             });
-
 //            楼栋选项卡切换
             function ban(){
-                $(".list-group-parent").click(function(){
-                    $(this).toggleClass("checked").next(".lab_num").toggle("200");
-                    $(this).children(".fa").toggleClass("deg");
+                $(".list-group-parent").unbind().click(function(){
+                    $(this).addClass("checked").parent(".list-group").siblings().children(".list-group-parent").removeClass("checked");
+                   if($(this).next(".lab_num").children(".list-group-child").size()!="0"){
+                       $(this).next(".lab_num").slideToggle("200");
+                       $(this).children(".fa").toggleClass("deg");
+                   }
                     if($(this).parent().next(".list-group").length=="1"){
                         $(this).next(".lab_num").children().last().addClass("border-bottom");
                     }
-
                 });
-                $(".list-group-child").click(function(){
+                $(".list-group-child").unbind().click(function(){
                     $(".list-group-parent").removeClass("checked");
                     $(".list-group-child").removeClass("checked");
                     $(this).addClass("checked");
@@ -32,17 +36,12 @@
             }
 
 //            新增、编辑切换
-            $("#add_device").click(function(){
-                $("#add_device_form").show();
-                $("#edit_form").hide();
-            });
             $("#edit").click(function(){
                 $("#add_device_form").hide();
                 $("#edit_form").show();
             });
 //            楼栋数据绑定
             $("#ban_select").change(function(){
-
                 var $treeview=$(".treeview");
                 $treeview.empty();
                 var $thisId=$(this).val();
@@ -63,7 +62,8 @@
                             );
                             if(this.lab!=""){
                                 $(this.lab).each(function(){
-                                    $(".treeview #"+ this.floor +" .lab_num").append("<div class='list-group-item list-group-child'>"+this.name+"</div>")
+                                    console.log(this.id);
+                                    $(".treeview #"+ this.floor +" .lab_num").append("<div class='list-group-item list-group-child  labdetail'  data='"+this.total+"' lab_id='"+this.id+"'>"+this.name+"</div>")
                                 });
                                 $(".treeview #"+ this.floor +" .list-group-parent").append("<i class='fa fa-angle-right right'></i>");
                             }
@@ -71,6 +71,17 @@
                         })
                     }
                 })
+            });
+//              人数数量
+            $('.ibox-content').delegate('.labdetail','click',function(){
+                var total = $(this).attr('data');
+                if(total == 'null'){
+                    total = 0;
+                }
+                var labname = $(this).html();
+                $('.labname').html(labname);
+                $('.labtotal').html(total+'人');
+
             });
 //            删除
             $(".delete").click(function(){
@@ -91,53 +102,57 @@
                     $(".check_one").children(".check_icon").addClass("check");
                 }
             });
-            $(".check_one").click(function(){
-               if($(this).children(".check_icon").hasClass("check")){
-                   $(this).children(".check_icon").removeClass("check");
-                   $(".check_all").children(".check_icon").removeClass("check");
-               }else{
-                   $(this).children(".check_icon").addClass("check");
-                   if($(".check_one").size() == $(".check_one").children(".check").size()){
+            $("#add_device_form").delegate(".check_one","click",function(){
+                if($(this).children(".check_icon").hasClass("check")){
+                    $(this).children(".check_icon").removeClass("check");
+                    $(".check_all").children(".check_icon").removeClass("check");
+                }else{
+                    $(this).children(".check_icon").addClass("check");
+                    if($(".check_one").size() == $(".check_one").children(".check").size()){
                         $(".check_all").children(".check_icon").addClass("check");
-                   }
-               }
-            });
-
-            $('.treeview').delegate('.list-group-child','click',function(){
-//                alert(11);
-                var lab_id = $(this).attr('lab_id');//lad_id待确定位置
-
-                var url = "{{ route('msc.admin.LadMaintain.LabIdGetLaboratoryDeviceList')}}";
-                $.ajax({
-                    type:"get",
-                    url:url+'?lab_id='+lab_id,
-                    async:true,
-                    success:function(res){
-                        var str = '';
-                        if(res.code == 1){
-                            var data = res.data.rows.LadDeviceList.data;
-                            console.log(data);
-                            for(var i=0;i<data.length;i++){
-                                str += '<tr>' +
-                                        '<td>'+data[i].id+'</td>' +
-                                        '<td>'+data[i].device_info.name+'</td>' +
-                                        '<td>'+data[i].device_info.devices_info.name+'</td>' +
-                                        '<td>'+data[i].total+'</td>' +
-                                        '<td>' +
-                                        '<a class="state1 edit"  data-toggle="modal" data-target="#myModal"  style="text-decoration: none" id="edit">' +
-                                        '<span>编辑数量</span></a><a class="state2 delete">删除</a>' +
-                                        '</td>' +
-                                        '</tr>';
-                            }
-                        }
-                        $('#table-striped tbody').html(str);
                     }
-                });
-
-            })
-
-            $('.right').click(function(){
-//                alert('11111');
+                }
+            });
+            //实验室数据显示
+            function labdata(){
+                $('.treeview .list-group-child').click(
+                        function(){
+                            var lab_id = $(this).attr('lab_id');
+                            var url = "{{ route('msc.admin.LadMaintain.LaboratoryDeviceList')}}";
+                            $.ajax({
+                                type:"get",
+                                url:url+'?lab_id='+lab_id,
+                                async:true,
+                                success:function(res){
+                                    var str = '';
+                                    if(res.code == 1){
+                                        var data = res.data.rows.LadDeviceList.data;
+                                        for(var i=0;i<data.length;i++){
+                                            str += '<tr>' +
+                                                    '<td>'+data[i].id+'</td>' +
+                                                    '<td>'+data[i].device_info.name+'</td>' +
+                                                    '<td>'+data[i].device_info.devices_cate_info.name+'</td>' +
+                                                    '<td>'+data[i].total+'</td>' +
+                                                    '<td>' +
+                                                    '<a class="state1 edit"  data-toggle="modal" data-target="#myModal"  style="text-decoration: none" id="edit">' +
+                                                    '<span>编辑数量</span>' +
+                                                    '</a>' +
+                                                    '<a class="state2 delete"><span>删除</span></a>' +
+                                                    '</td>' +
+                                                    '</tr>';
+                                        }
+                                    }
+                                    $('#table-striped tbody').html(str);
+                                }
+                            });
+                        }
+                )
+            }
+//            设备添加回显数据
+            $('#add_device').click(function(){
+//                点击新增按钮显示当前实验室设备弹出层
+                $("#add_device_form").show();
+                $("#edit_form").hide();
                 var url  = "{{route('msc.admin.LadMaintain.LaboratoryListData')}}";
                 $.ajax({
                     type:"get",
@@ -146,15 +161,12 @@
                     success:function(result){
                         var html = '';
                         var list ='';
-                        console.log(result);
-                        console.log(result.data.rows.list);
                         $(result.data.rows.deviceType).each(function(){
                                      html+='<li>' +
                                          '<a href="">'+this.name+'</a>'+
                                          ' </li>'
-                        })
+                        });
                         $('#device-type').html(html);
-
                         $(result.data.rows.list).each(function(){
                                  list+='<tr>' +
                                     '<td>' +
@@ -174,19 +186,57 @@
                     }
                 })
             })
-
+//            添加中  关键字搜索
+            $('#search').click(function(){
+                var url  = "{{route('msc.admin.LadMaintain.LaboratoryListData')}}";
+                $.ajax({
+                    type:"get",
+                    url:url+'?keyword='+$('#keyword').val(),
+                    async:true,
+                    success:function(result){
+                        var html = '';
+                        var list ='';
+                        console.log(result);
+                        console.log(result.data.rows.list);
+                        $(result.data.rows.deviceType).each(function(){
+                            html+='<li>' +
+                                    '<a href="">'+this.name+'</a>'+
+                                    ' </li>'
+                        });
+                        $('#device-type').html(html);
+                        $(result.data.rows.list).each(function(){
+                            list+='<tr>' +
+                                    '<td>' +
+                                    '<label class="check_label checkbox_input check_one"> ' +
+                                    '<div class="check_real check_icon display_inline">' +
+                                    '</div> <input type="hidden" name="" value="">' +
+                                    '</label>' +
+                                    '</td>' +
+                                    ' <td>1</td>' +
+                                    ' <td> <input type="number"></td>' +
+                                    ' <td>'+this.name+'</td> ' +
+                                    '<td>'+this.catename+'</td> ' +
+                                    '</tr> '
+                            console.log(this.name);
+                        });
+                        $('#addition tbody').html(list);
+                    }
+                });
+                return false;
+            });
+            //编辑数量
           $('.edit').click(function(){
               if($(this).attr("")){
                   $('input[name=name]').val($(this).parent().parent().find('.device').html());
                   $('input[name=type]').val($(this).parent().parent().find('.deviceType').html());
                   $('input[name=total]').val($(this).parent().parent().find('.total').html());
               }
-              $('#edit_form').attr('action','{{route("")}}');
+              $('#edit_form').attr('action','');
               var id = $(this).attr("data");
               $('#edit_form').append('<input type="hidden" name="id" value="'+id+'">');
           })
-
         })
+
     </script>
 @stop
 
@@ -217,11 +267,11 @@
                 <div class="ibox-title overflow">
                     <div class="left">
                         <p class="left">已选实验室：</p>
-                        <h5 class="left">临床技能室（3-13）</h5>
+                        <h5 class="left labname">临床技能室（3-13）</h5>
                     </div>
                     <div class="left" style="margin-left: 20px">
                         <p class="left">容量：</p>
-                        <h5 class="left">30人</h5>
+                        <h5 class="left  labtotal " >30人</h5>
                     </div>
                     <input type="button" class="btn btn_pl btn-success right" data-toggle="modal" data-target="#myModal" value="添加设备" id="add_device">
                 </div>
@@ -237,16 +287,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td class="midbody_id">1</td>
-                            <td  class="device">听诊器</td>
-                            <td class="deviceType">耗材</td>
-                            <td class="total">30</td>
-                            <td>
-                                <a class="state1 edit"  data-toggle="modal" data-target="#myModal"  style="text-decoration: none" id="edit"><span>编辑数量</span></a>
-                                <a class="state2 delete">删除</a>
-                            </td>
-                        </tr>
+
                         </tbody>
                     </table>
                 </div>
@@ -257,7 +298,7 @@
 
 @section('layer_content')
     {{--新增--}}
-    <form class="form-horizontal" id="add_device_form" novalidate="novalidate" action="{{route('msc.admin.LadMaintain.DevicesAdd')}}" method="post">
+    <form class="form-horizontal" id="add_device_form" novalidate="novalidate" action="" method="post">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             <h4 class="modal-title" id="myModalLabel">添加设备</h4>
@@ -269,8 +310,8 @@
                         <div class="input-group">
                             <input type="text" id="keyword" name="keyword" placeholder="搜索" class="input-sm form-control" value="">
                             <span class="input-group-btn">
-                            <button type="submit" class="btn btn-sm btn-primary" id="search"><i class="fa fa-search"></i></button>
-                        </span>
+                                <button class="btn btn-sm btn-primary" id="search"><i class="fa fa-search"></i></button>
+                            </span>
                         </div>
                     </form>
                 </div>
@@ -300,26 +341,7 @@
                 </tr>
                 </thead>
                 <tbody>
-                {{--<tr>--}}
-                    {{--<td>--}}
-                        {{--<label class="check_label checkbox_input check_one">--}}
-                            {{--<div class="check_real check_icon display_inline"></div>--}}
-                            {{--<input type="hidden" name="" value="">--}}
-                        {{--</label>--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--1--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--<input type="number">--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--听诊器--}}
-                    {{--</td>--}}
-                    {{--<td>--}}
-                        {{--耗材--}}
-                    {{--</td>--}}
-                {{--</tr>--}}
+
                 </tbody>
             </table>
             <div class="hr-line-dashed"></div>
