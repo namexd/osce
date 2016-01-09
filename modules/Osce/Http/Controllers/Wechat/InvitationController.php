3babcc50eb6f6191970aec9896e64566d8512139
zhouqiang <candyphp@163.com>
2016-01-09 13:29:45 +0800

    邀请

--- modules/Osce/Http/Controllers/Wechat/InvitationController.php
+++ modules/Osce/Http/Controllers/Wechat/InvitationController.php
@@ -10,26 +10,43 @@ namespace Modules\Osce\Http\Controllers\Wechat;
 
 
 use Illuminate\Http\Request;
+use Modules\Msc\Entities\Teacher;
 use Modules\Osce\Entities\ExamScreening;
 use Modules\Osce\Entities\ExamScreeningStudent;
+use Modules\Osce\Entities\Station;
 use Modules\Osce\Http\Controllers\CommonController;
 
 class InvitationController extends CommonController
 {
+
+
+    //获取要发送给sp老师的数据
     public function getInvitation(Request $request)
     {
         //验证略
         $this->validate($request,[
-            'id' => 'required|integer'
+            'tid' => 'required|integer',
+            'sid' => 'required|integer'
         ],[
-            'id.required'   =>  '邀请编号必须',
-            'id.integer'    =>  '邀请编号必须是数字',
+            'tid.required'   =>  '邀请编号必须',
+            'tid.integer'    =>  '邀请编号必须是数字',
+
         ]);
 
-        $id =   $request    -> get('id');
+        $teacher_id =   $request    -> get('tid');
+        $station_id =   $request    -> get('sid');
+
+        $TeacherList= Teacher::find($teacher_id)->get();
+        $StationList = Station::find($station_id)->get();
+
+          $Invitationdata= [
 
 
-        //获取要发送给sp老师的数据
+
+          ];
+
+
+
 
     }
 }
