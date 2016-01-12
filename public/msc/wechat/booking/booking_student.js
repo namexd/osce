60c3b81373fabffa185cfc5200b0a6ba0044e44c
zengjie <zengjie@misrobot.com>
2016-01-12 15:57:29 +0800

    老师预约开放实验室详情页

--- public/msc/wechat/booking/booking_student.js
+++ public/msc/wechat/booking/booking_student.js
@@ -6,11 +6,22 @@ $(function(){
     pars = JSON.parse(($("#parameter").val()).split("'").join('"'));
     switch(pars.pagename){
         case "booking_student":booking_student();break; //预约实验室（学生）详情页
+        case "open_teacher_detail":open_teacher_detail();break; //老师预约开放实验室详情页
     }
 });
 
 //预约实验室（学生）详情页
 function booking_student(){
+    var $check_one=$(".check_one");
+    $check_one.click(function(){
+        if($(this).children(".check_icon").hasClass("check")){
+            $(this).children(".check_icon").removeClass("check");
+        }else{
+            $(this).children(".check_icon").addClass("check");
+        }
+    });
+}
+function open_teacher_detail(){
     var $check_one=$(".check_one");
     $check_one.click(function(){
         if($(this).children(".check_icon").hasClass("check")){
