d3c6ee9ce6618ae6305ebd03151ca0fbe4e66d8f
zengjie <zengjie@misrobot.com>
2016-01-12 14:47:30 +0800

    预约实验室（学生）详情页面

---
+++ public/msc/wechat/booking/booking_student.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2016/1/8 0008.
+ */
+var pars;
+$(function(){
+    pars = JSON.parse(($("#parameter").val()).split("'").join('"'));
+    switch(pars.pagename){
+        case "booking_student":booking_student();break; //预约实验室（学生）详情页
+    }
+});
+
+//预约实验室（学生）详情页
+function booking_student(){
+    var $check_one=$(".check_one");
+    $check_one.click(function(){
+        if($(this).children(".check_icon").hasClass("check")){
+            $(this).children(".check_icon").removeClass("check");
+        }else{
+            $(this).children(".check_icon").addClass("check");
+        }
+    });
+}
