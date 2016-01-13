d7d0a324a67c95b80198275e5360b8f2ef7ba18f
zengjie <zengjie@misrobot.com>
2016-01-13 11:03:44 +0800

    专业列表页面表单bug修改及js整合提取

--- public/msc/admin/systemtable/systemtable.js
+++ public/msc/admin/systemtable/systemtable.js
@@ -8,6 +8,7 @@ $(function(){
     switch(pars.pagename){
         case "resource_table":resource_table();break; //资源列表页面
         case "title_table":title_table();break; //职称列表页面
+        case "major_table":major_table();break; //专业列表页面
     }
 
 });
@@ -369,7 +370,189 @@ function title_table(){
         $("#edit_from").hide();
         add_from();
     });
-    //$(".modal,#close").click(function(event){
-    //    event.stopPropagation();
-    //})
+}
+//专业列表页面
+function major_table(){
+    //            新增表单验证
+    function add_form(){
+        $('#add_from').bootstrapValidator({
+            message: 'This value is not valid',
+            feedbackIcons: {/*输入框不同状态，显示图片的样式*/
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {/*验证*/
+                code: {/*键名username和input name值对应*/
+                    message: 'The username is not valid',
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '专业代码不能为空'
+                        }
+                    }
+                },
+                name: {/*键名username和input name值对应*/
+                    message: 'The username is not valid',
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '专业名称不能为空'
+                        }
+                    }
+                },
+                status: {
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '状态不能为空'
+                        }
+                    }
+                }
+            }
+        });
+    }
+    //            编辑表单验证
+    function edit_form(){
+        $('#edit_from').bootstrapValidator({
+            message: 'This value is not valid',
+            feedbackIcons: {/*输入框不同状态，显示图片的样式*/
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {/*验证*/
+                code: {/*键名username和input name值对应*/
+                    message: 'The username is not valid',
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '专业代码不能为空'
+                        }
+                    }
+                },
+                name: {/*键名username和input name值对应*/
+                    message: 'The username is not valid',
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '专业名称不能为空'
+                        }
+                    }
+                },
+                status: {
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '状态不能为空'
+                        }
+                    }
+                }
+            }
+        });
+    }
+    //编辑
+    $('.edit').click(function () {
+        var $editUrl=$("#editUrl").val();
+        var $my_edit=$("#my_edit");
+        $("#my_add").hide();
+        $("#edit_from").remove();
+        $my_edit.show().empty().append(
+            '<form class="form-horizontal" id="edit_from" novalidate="novalidate" action="" method="post">'+
+            '<div class="modal-header">' +
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+            '<h4 class="modal-title" id="myModalLabel">编辑专业</h4>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>专业代码</label>' +
+            '<div class="col-sm-9">' +
+            '<input type="text" class="form-control name add-name" name="code" value="" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>专业名称</label>' +
+            '<div class="col-sm-9">' +
+            '<input type="text" class="form-control describe add-describe" name="name" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>状态</label>' +
+            '<div class="col-sm-9">' +
+            '<select id="select_Category"   class="form-control m-b state" name="status">' +
+            '<option value="1">正常</option>' +
+            '<option value="0">禁用</option>' +
+            '</select>' +
+            '</div>' +
+            '</div>' +
+            '<div class="hr-line-dashed"></div>' +
+            '<div class="form-group">' +
+            '<div class="col-sm-4 col-sm-offset-2 right">' +
+            '<button class="btn btn-primary sure_btn"  type="submit" >确&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;定</button>' +
+            '<button class="btn btn-white2 right" type="button" data-dismiss="modal">取&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;消</button>' +
+            '</div>' +
+            '</div>' +
+            '</div>'+
+            '</form>'
+        );
+        $("#edit_from").attr("action",$editUrl);
+        edit_form();
+        if($(this).attr("data")){
+            $('input[name=name]').val($(this).parent().parent().find('.name').html());
+            $('input[name=code]').val($(this).parent().parent().find('.code').html());
+            var status = '';
+            if($(this).parent().parent().find('.status').html() == '正常'){
+                status = 1;
+            }else{
+                status = 0;
+            }
+            $('.state option').each(function(){
+                if($(this).val() == status){
+                    $(this).attr('selected','selected');
+                }
+            });
+            var id = $(this).attr("data");
+            $('#edit_from').append('<input type="hidden" name="id" value="'+id+'">');
+        }
+    });
+    $('#addprofession').click(function(){
+        var $addUrl=$("#addUrl").val();
+        var $my_add=$("#my_add");
+        $("#my_edit").hide();
+        $("#add_from").remove();
+        $my_add.show().empty().append(
+            '<form class="form-horizontal" id="add_from" novalidate="novalidate" action="" method="post">'+
+            '<div class="modal-header">' +
+            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+            '<h4 class="modal-title" id="myModalLabel">新增专业</h4>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>专业代码</label>' +
+            '<div class="col-sm-9">' +
+            '<input type="text" class="form-control name add-name" name="code" value="" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>专业名称</label>' +
+            '<div class="col-sm-9">' +
+            '<input type="text" class="form-control describe add-describe" name="name" />' +
+            '</div>' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label class="col-sm-3 control-label"><span class="dot">*</span>状态</label>' +
+            '<div class="col-sm-9">' +
+            '<select id="select_Category"   class="form-control m-b state" name="status">' +
+            '<option value="1">正常</option>' +
+            '<option value="0">禁用</option>' +
+            '</select>' +
+            '</div>' +
+            '</div>' +
+            '<div class="hr-line-dashed"></div>' +
+            '<div class="form-group">' +
+            '<div class="col-sm-4 col-sm-offset-2 right">' +
+            '<button class="btn btn-primary sure_btn"  type="submit" >确&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;定</button>' +
+            '<button class="btn btn-white2 right" type="button" data-dismiss="modal">取&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;消</button>' +
+            '</div>' +
+            '</div>' +
+            '</div>'+
+            '</form>'
+        );
+        $("#add_from").attr("action",$addUrl);
+        add_form();
+    })
 }
