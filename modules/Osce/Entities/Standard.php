4f52df850dfdb145290acc712147ed73b1f6cbb0
zhouqiang <candyphp@163.com>
2016-01-13 21:00:58 +0800

    修改路由

---
+++ modules/Osce/Entities/Standard.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/1/13 0013
+ * Time: 20:54
+ */
+
+namespace Modules\Osce\Entities;
+
+
+class Standard extends CommonModel
+{
+    protected $connection = 'osce_mis';
+    protected $table = 'subject_item';
+    public $timestamps = true;
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $guarded = [];
+    protected $hidden = [];
+    protected $fillable = ['subject_id', 'content', 'sort', 'score', 'created_user_id','pid','level','answer'];
+    public $search = [];
+
+
+    
+
+
+
+}
