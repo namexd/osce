7f58d4a1235d0fd6b9e6ae8fd1ac843634946cf0
zhoufuxiang <zhoufuxiang@163.com>
2016-01-14 18:08:53 +0800

    修改 新增监考设备里的取消按钮

--- modules/Osce/Resources/views/admin/resourcemanage/vcr_add.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/vcr_add.blade.php
@@ -161,7 +161,7 @@
                             <div class="form-group">
                                 <div class="col-sm-4 col-sm-offset-2">
                                     <button class="btn btn-primary" type="submit">保存</button>
-                                    <button class="btn btn-white" type="submit">取消</button>
+                                    <a class="btn btn-white" href="{{route('osce.admin.machine.getMachineList')}}">取消</a>
 
                                 </div>
                             </div>
