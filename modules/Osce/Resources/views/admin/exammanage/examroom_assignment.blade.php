589f869974c5cb5665554ea1a6a6ed8bcb56ac9e
luohaihua <luohaihua@misrobot.com>
2016-01-14 10:57:56 +0800

    考试安排导航选项卡 修正

--- modules/Osce/Resources/views/admin/exammanage/examroom_assignment.blade.php
+++ modules/Osce/Resources/views/admin/exammanage/examroom_assignment.blade.php
@@ -89,9 +89,8 @@
                     <ul class="nav nav-tabs">
                         <li class=""><a href="{{route('osce.admin.exam.getEditExam')}}?id={{$id}}">基础信息</a></li>
                         <li class="active"><a href="{{route('osce.admin.exam.getExamroomAssignment',['id'=>$id])}}">考场安排</a></li>
-                        <li class=""><a href="{{route('osce.admin.spteacher.getShow',['id'=>$id])}}">邀请SP</a></li>
                         <li class=""><a href="{{route('osce.admin.exam.getExamineeManage',['id'=>$id])}}">考生管理</a></li>
-                        <li class=""><a href="#">智能排考</a></li>
+                        <li class=""><a href="{{route('osce.admin.exam.getIntelligence',['id'=>$id])}}">智能排考</a></li>
                     </ul>
                 </div>
             </div>
