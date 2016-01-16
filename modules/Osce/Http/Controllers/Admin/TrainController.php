7c1148f86faa334137a9a9ae13534d0d9783ac09
zhouchong <zhouchong@misrobot.com>
2016-01-16 17:52:21 +0800

    微信端考前培训

--- modules/Osce/Http/Controllers/Admin/TrainController.php
+++ modules/Osce/Http/Controllers/Admin/TrainController.php
@@ -9,6 +9,7 @@ namespace Modules\Osce\Http\Controllers\Admin;
 
 
 
+use App\Entities\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -49,7 +50,7 @@ class TrainController extends  CommonController{
 
         $list=InformTrain::select()->orderBy('begin_dt')->get();
 
-        return view('osce::admin.train.train_list')->with(['list'=>$list,'pagination'=>$pagination]);
+        return view('osce::wechat.train.train_list')->with(['list'=>$list,'pagination'=>$pagination]);
 
     }
 
@@ -88,11 +89,14 @@ class TrainController extends  CommonController{
         ]);
         $user=Auth::user();
         $userId=$user->id;
-        $attachments=$this->postUploadFile();
         $data=$request->only(['name','address','begin_dt','end_dt','teacher','content','status']);
         $data['create_user_id']=$userId;
-        $data['attachments']=serialize($attachments);
-        $result=InformTrain::insert($data);
+        $id=InformTrain::insertGetId($data);
+        $array=array(
+            'id'      =>$id,
+            'userId'  =>$userId,
+        );
+        $result= $this->postUploadFile($request,$array);
         if($result){
          return view()->with('success','新增成功');
         }
@@ -184,7 +188,6 @@ class TrainController extends  CommonController{
             'status'                  =>'required',
             'create_user_id'          =>'required',
         ]);
-        $attachments=$this->postUploadFile();
         $data=$request->only(['name','address','begin_dt','end_dt','teacher','content','status','create_user_id']);
         $user=Auth::user();
         $userId=$user->id;
@@ -195,7 +198,6 @@ class TrainController extends  CommonController{
                 $this->success_rows(3,'false')
             );
         }
-        $data['attachments']=unserialize($attachments);
         $result=InformTrain::where('id',$data['id'])->update($data);
         if($result){
             return view()->with('success','编辑成功');
@@ -246,6 +248,32 @@ class TrainController extends  CommonController{
     }
 
     /**
+     *查看考前培训
+     * @method GET
+     * @url /user/
+     * @access public
+     *
+     * @param Request $request post请求<br><br>
+     * <b>post请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     * * string        参数英文名        参数中文名(必须的)
+     * * string        参数英文名        参数中文名(必须的)
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return ${response}
+     *
+     * @version 1.0
+     * @author zhouchong <zhouchong@misrobot.com>
+     * @date ${DATE} ${TIME}
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function getTrainDetail(Request $request){
+        $id=$request->get('id');
+        $train=InformTrain::find($id);
+        return view('osce::wechat.train.train_detail')->with('train',$train);
+    }
+
+    /**
      *上传文件
      * @method GET
      * @url /osce/wechat/train/upload-file
@@ -265,51 +293,88 @@ class TrainController extends  CommonController{
      * @date ${DATE} ${TIME}
      * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
      */
-    public function postUploadFile(){
-        $user=Auth::user();
-        $userId=$user->id;
-        $file=\Input::file('doc');
-        $time=date('Y-m-d',time());
-        $fileName=$file->getFilename();
-        $file_ex=$file->getClientOriginalExtension();
-//         $file_size=round($file->getSize() /1024);
-//         $file_mime=$file->getMimeType();
-        $uploadDir='/uploads/'.$time.'/doc';
-        if(!dir($uploadDir)){
-            mkdir('uploads',777,true);
-        }
-        if (!in_array($file_ex, array('doc', 'xlsx'))) {
-            return \Redirect::to('/')->withErrors('上传类型不合法');
-        }
-        $newname=strtotime(date('Ymd')).'-'.$fileName.$userId;
-        if(\Request::file('doc')){
-            $result= \Request::file()->move(base_path().$uploadDir.'/',$newname);
-            if(!$result){
-                return \Response::json('false',400);
-            }
-        }
-        $path['doc']=\Input::file('doc')->getRealPath();
+    public function postUploadFile(Request $request, Array $array){
+       try {
+           $time = date('Y-m-d');
+           list($id, $userId) = $array;
+           $username=User::findOrFail($userId)->first()->name;
+           $params=[
+               'username' =>$username,
+               'userId'   =>$userId,
+           ];
+           list($userId,$time)=$array;
+           if(!$request->hasFile('word')){
+               throw new \Exception('上传的word文档不存在');
+           }
+           if(!$request->hasFile('excel')){
+               throw new \Exception('上传的excel文件不存在');
+           }
 
-        $file=\Input::file('xlsx');
-        $fileName=$file->getFilename();
-        $file_ex=$file->getClientOriginalExtension();
-        $uploadDir='/uploads/'.$time.'/xlsx';
-        if(!dir($uploadDir)){
-            mkdir('uploads',777,true);
-        }
-        if (!in_array($file_ex, array('doc', 'xlsx'))) {
-            return \Redirect::to('/')->withErrors('上传类型不合法');
-        }
-        $newname=strtotime(date('Ymd')).'-'.$fileName.$userId;
-        if(\Request::file('xlsx')){
-            $result= \Request::file()->move(base_path().$uploadDir.'/',$newname);
-            if(!$result){
-                return \Response::json('false',400);
-            }
-        }
-        $path['xlsx']=base_path().$uploadDir.'/'.$newname;
-        return $path;
+           $words=$request->file('word');
+           $excels=$request->file('excel');
+
+           if (!$words->isValid()) {
+               throw new \Exception('上传的word文档出错');
+           }
+           if (!$excels->isValid()) {
+               throw new \Exception('上传的excel文件出错');
+           }
+           //拼装文件名
+           $resultPhoto[] = self::uploadFile($words, $time, $params, $id);
+           $resultRadio[] = self::uploadFile($excels, $time, $params, $id);
+
+           $result = [$resultPhoto, $resultRadio];
+           return $result;
+       }catch (\Exception $ex){
+           throw $ex;
+       }
     }
 
 
+    protected function uploadFile($files, $date, array $params,$id){
+        try {
+            //将上传的文件遍历
+            foreach ($files as $key => $file) {
+                //拼凑文件名字
+                $fileName = '';
+                $fileMime = $file->getMimeType();
+                foreach ($params as $param) {
+                    $fileName .= $param;
+                }
+                $fileName .= $file->getClientOriginalExtension(); //获取文件名的正式版
+
+                $savePath = public_path('osce/Train/') . $fileMime . '/' . $date . '/' . $params['username'] . '/';
+                $savePath = realpath($savePath);
+
+
+                if (!file_exists($savePath)) {
+                    mkdir($savePath, 0755, true);
+                }
+
+                //将文件放到自己的定义的目录下
+                if (!$file->move($savePath, $fileName)) {
+                    throw new \Exception('文件保存失败！请重试！');
+                }
+
+                //生成附件url地址
+                $attachUrl = $savePath . $fileName;
+
+                //将要插入数据库的数据拼装成数组
+                $data = [
+                    'url' => $attachUrl,
+                    'type' => $fileMime,
+                    'name' => $fileName,
+                    'description' => $date . '-' . $params['username'],
+                ];
+
+                //将内容插入数据库
+                if (!$result = InformTrain::where('id',$id)->update(['attachments'=>serialize($data)])) {
+                    throw new \Exception('附件数据保存失败');
+                }
+                return $result;
+            }
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
+    }
 }
