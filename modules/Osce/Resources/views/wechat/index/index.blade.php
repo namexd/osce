6365d2c5ebf4c8f7c3876bd1055b2ddba2788485
zhoufuxiang <zhoufuxiang@163.com>
2016-01-19 20:00:16 +0800

    微信端

--- modules/Osce/Resources/views/wechat/index/index.blade.php
+++ modules/Osce/Resources/views/wechat/index/index.blade.php
@@ -40,8 +40,8 @@
      <li><a href="{{route('osce.wechat.notice.getSystemList')}}">资讯通知</a><li>
      <li><a href="#">成绩查询</a><li>
      <li><a href="{{route('osce.wechat.invitation.getList')}}">预约邀请</a><li>
-     <li><a href="{{route('osce.wechat.getQuestionList')}}">讨论区</a><li>
-     <li><a href="{{route('osce.wechat.getTrainList')}}">考前培训</a><li>
+     <li><a href="{{route('osce.wechat.getDiscussionLists')}}">讨论区</a><li>
+     <li><a href="{{route('osce.wechat.getTrainlists')}}">考前培训</a><li>
      <li><a href="{{route('osce.wechat.notice.getSystemList')}}">系统消息</a><li>
   </ul>
 </div>
