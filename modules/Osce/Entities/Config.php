1419a94d0406171b1f02d58039ab5b549aa9816e
jiangzhiheng <j5110021@misrobot.com>
2016-01-19 11:50:00 +0800

    解决了系统设置的问题

--- modules/Osce/Entities/Config.php
+++ modules/Osce/Entities/Config.php
@@ -29,9 +29,9 @@ class Config extends CommonModel
     public function store(array $formData)
     {
         try {
+//            dd($formData);
             //$key就是字段名,$item为键值数组，有必要再遍历一次，将每个值都插入数据库
             foreach ($formData as $key => $item) {
-
                 //如果$item是数组的话，就说明是多选
                 $cate = strstr($key, '_', true);  //从传入的字段表里获取cate
                 $nameLen = strpos($key, '_'); //获取_在字符串中第一次出现的位置
@@ -51,13 +51,41 @@ class Config extends CommonModel
                     'type' => $type,
                     'value' => $item,
                 ];
-                if ($result = $this->create($data)) {
-                    return $result;
+                //先判断是修改还是添加
+                if (Config::where('name', $name)->where('cate', $cate)->get()->isEmpty()) { //如果是空，就证明是添加
+                    if ($result = $this->create($data)) {
+                        return $result;
+                    } else {
+                        throw new \Exception('配置插入数据库失败！');
+                    }
                 } else {
-                    throw new \Exception('配置插入数据库失败！');
+                    //否则就是编辑
+                    return $this->saveDBConfig($data);
                 }
             }
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
+    }
 
+    /**
+     * 具体的服务器数据的修改方法
+     * @param $data
+     * @return bool
+     * @return bool
+     * @throws \Exception
+     * @auther Jiangzhiheng <Jiangzhiheng@misrobot.com>
+     */
+    private function saveDBConfig($data)
+    {
+        try {
+            $config = $this->where('name', $data['name'])->where('cate', $data['cate'])->first();
+            $config->value = $data['value'];
+
+            if (!$result = $config->save()) {
+                throw new \Exception('保存失败，请重试！');
+            }
+            return $result;
         } catch (\Exception $ex) {
             throw $ex;
         }
@@ -85,27 +113,28 @@ class Config extends CommonModel
                 <<<EOT
 env('WECHAT_USE_ALIAS', $formData[wechat_use_alias])
 EOT;
-            $config['messages']['wechat']['app_id'] = "env('WECHAT_APP_ID','"  . $formData['wechat_app_id'] . "')," . '//必填';
-            $config['messages']['wechat']['secret'] = "env('WECHAT_SECRET','"  . $formData['wechat_secret'] . "')," . '//必填';
-            $config['messages']['wechat']['token'] = "env('WECHAT_TOKEN','"  . $formData['wechat_token'] . "')," . '// 必填,http://good-doctor.cn/hx/wx_sample.php';
-            $config['messages']['wechat']['encoding_key'] = "env('WECHAT_ENCODING_KEY','"  . $formData['wechat_encoding_key'] . "')," . '// 加密模式需要，其它模式不需要';
+            $config['messages']['wechat']['app_id'] = "env('WECHAT_APP_ID','" . $formData['wechat_app_id'] . "')," . '//必填';
+            $config['messages']['wechat']['secret'] = "env('WECHAT_SECRET','" . $formData['wechat_secret'] . "')," . '//必填';
+            $config['messages']['wechat']['token'] = "env('WECHAT_TOKEN','" . $formData['wechat_token'] . "')," . '// 必填,http://good-doctor.cn/hx/wx_sample.php';
+            $config['messages']['wechat']['encoding_key'] = "env('WECHAT_ENCODING_KEY','" . $formData['wechat_encoding_key'] . "')," . '// 加密模式需要，其它模式不需要';
 
             $config['messages']['email']['server'] = $formData['email_server'];
-            $config['messages']['email']['port'] = $formData['email_port'] ;
+            $config['messages']['email']['port'] = $formData['email_port'];
             $config['messages']['email']['ssl'] = $formData['email_ssl'];
             $config['messages']['email']['username'] = $formData['email_username'];
             $config['messages']['email']['password'] = $formData['email_password'];
 
 
             //将修改后的数据重新写回文件
-            $str    =   "<?php\nreturn " . var_export($config,true).";";
-            $str    =   str_replace("'env(",'env(',$str);
-            $str    =   str_replace(")',",'),',$str);
+            $str = "<?php\nreturn " . var_export($config, true) . ";";
+            $str = str_replace("'env(", 'env(', $str);
+            $str = str_replace(")',", '),', $str);
 
-            $str    =   stripslashes($str);
-            if( !file_put_contents(MESSAGE_CONFIG, $str)) {
+            $str = stripslashes($str);
+            if (!file_put_contents(MESSAGE_CONFIG, $str)) {
                 throw new \Exception('配置插入文件失败！');
             };
+            return true;
         } catch (\Exception $ex) {
             throw $ex;
         }
