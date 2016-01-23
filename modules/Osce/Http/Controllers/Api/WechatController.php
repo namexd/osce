f562bcd50643fc8f4329b1cc33a2c9ba5bcae092
luohaihua <luohaihua@misrobot.com>
2016-01-23 12:05:43 +0800

    测试公众号认证

--- modules/Osce/Http/Controllers/Api/WechatController.php
+++ modules/Osce/Http/Controllers/Api/WechatController.php
@@ -9,10 +9,14 @@
 namespace Modules\Osce\Http\Controllers\Api;
 
 use Modules\Osce\Http\Controllers\CommonController;
+use Overtrue\Wechat\Server;
 
 class WechatController extends CommonController
 {
-    public function service(){
-        echo config('wechat.token');
+    public function service(Server $server){
+        $server ->  on('message',function(){
+            return "获取成功";
+        });
+        return $server->serve();
     }
 }
