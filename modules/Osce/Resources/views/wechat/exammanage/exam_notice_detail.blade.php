c3daf2d929ea98c0d60596804afaf3380d402743
zhouqiang <candyphp@163.com>
2016-01-24 21:54:40 +0800

    添加地址

--- modules/Osce/Resources/views/wechat/exammanage/exam_notice_detail.blade.php
+++ modules/Osce/Resources/views/wechat/exammanage/exam_notice_detail.blade.php
@@ -64,7 +64,11 @@
                       {!! $notice->content !!}
                     </div>
                     <div>
-
+                        @if($data['attachments'])
+                            @foreach($data['attachments'] as $key=>$list)
+                                <a href="{{ route('osce.admin.getDownloadDocument',['id'=>$data['id'],'attch_index'=>$key])}}"><?php $pathInfo=explode('/',$list)?>{{array_pop($pathInfo)}}</a><br />
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </li>
