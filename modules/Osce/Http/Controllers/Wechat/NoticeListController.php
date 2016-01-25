09034f087b7481d6efa7138b4411207ac22582a9
zhouqiang <candyphp@163.com>
2016-01-25 17:40:54 +0800

    修改咨询通知

--- modules/Osce/Http/Controllers/Wechat/NoticeListController.php
+++ modules/Osce/Http/Controllers/Wechat/NoticeListController.php
@@ -96,9 +96,9 @@ class NoticeListController   extends CommonController
         $id     =   $request    ->  get('id');
         $notice =   UsersPm::find($id);
 
-        if($notice->attachments){
-            $notice->attachments = explode(',', $notice->attachments);
-        }
+//        if($notice->attachments){
+//            $notice->attachments = explode(',', $notice->attachments);
+//        }
         if(is_null($notice))
         {
             //消息不存在
