f31e44618004979e4dcd9620e9f69ce4cfc992e6
zhoufuxiang <zhoufuxiang@163.com>
2016-01-25 16:38:42 +0800

    监考设备新增

--- modules/Osce/Resources/views/admin/resourcemanage/pad_add.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/pad_add.blade.php
@@ -118,45 +118,44 @@
                                     <input type="hidden"  class="form-control" id="cate_id" name="cate_id" value="2" />
                                 </div>
                             </div>
-
                             <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">设备ID</label>
                                 <div class="col-sm-10">
                                     <input type="text"  class="form-control" id="code" name="code">
                                 </div>
                             </div>
-
                             <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">厂家</label>
                                 <div class="col-sm-10">
                                     <input type="text"  class="form-control" id="factory" name="factory">
                                 </div>
                             </div>
-
                             <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">型号</label>
                                 <div class="col-sm-10">
                                     <input type="text"  class="form-control" id="sp" name="sp">
                                 </div>
                             </div>
-
                             <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">采购日期</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="purchase_dt" name="purchase_dt" value="">
-
-                                    {{--<input type="text"  class="form-control" id="purchase_dt" name="purchase_dt">--}}
+                                    <input type="text" class="form-control" id="purchase_dt" name="purchase_dt">
                                 </div>
                             </div>
                             <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <label class="col-sm-2 control-label">状态</label>
                                 <div class="col-sm-10">
-                                    <select id=""   class="form-control m-b" name="status">
+                                    <select id="status"   class="form-control m-b" name="status">
                                         <option value="0">正常</option>
                                         @foreach($status as $key => $value)
                                             @if($key >1)
@@ -166,6 +165,8 @@
                                     </select>
                                 </div>
                             </div>
+                            <div class="hr-line-dashed"></div>
+
                             <div class="form-group">
                                 <div class="col-sm-4 col-sm-offset-2">
                                     <button class="btn btn-primary" type="submit">保存</button>
