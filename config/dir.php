477888dda8fb67bac26cec9a1833428107f3e642
luohaihua <luohaihua@misrobot.com>
2016-01-29 12:55:21 +0800

    配置 微信文件

--- config/dir.php
+++ config/dir.php
@@ -1,5 +1,6 @@
 <?php
     //定义常量路径
     define("MESSAGE_CONFIG",dirname(__FILE__) . '/message.php');
+    define("WECHAT_CONFIG",dirname(__FILE__) . DIRECTORY_SEPARATOR.'wechat.php');
     define("OSCE_ATTACH",dirname(dirname(__FILE__)) . '/modules/Osce/Attach/');
 
