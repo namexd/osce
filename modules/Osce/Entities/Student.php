322eb9376ec275c7cbc18e49ef0b8a45baac6882
zhouchong <zhouchong@misrobot.com>
2016-01-29 18:20:41 +0800

    提交腕表查询学生

--- modules/Osce/Entities/Student.php
+++ modules/Osce/Entities/Student.php
@@ -334,8 +334,8 @@ class Student extends CommonModel
         }
         $builder= $this->leftjoin('exam_order',function($join){
             $join ->on('student.id','=','exam_order.student_id');
-        })->where('exam_order.exam_id',$exam_id)->where('exam_screening_id',$screen_id)->where('exam_order.status',0)->orWhere('exam_order.status',4)->orderBy('begin_dt')
-            ->select([
+        })->where('exam_order.exam_id',$exam_id)->where('exam_screening_id',$screen_id)->where('exam_order.status',0)->orWhere('exam_order.status',4)->orderBy('begin_dt');
+        $builder=$builder->select([
                 'student.name as name',
                 'student.idcard as idcard',
                 'student.code as code',
