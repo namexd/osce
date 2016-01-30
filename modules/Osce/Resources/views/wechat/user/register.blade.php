21274f314c94509fbae6943649c5d463786bf75a
lizhiyuan <lizhiyuan@misrobot.com>
2016-01-30 19:40:44 +0800

    手机号验证

--- modules/Osce/Resources/views/wechat/user/register.blade.php
+++ modules/Osce/Resources/views/wechat/user/register.blade.php
@@ -265,7 +265,7 @@
                     },
                     threshold :  1 , //有6字符以上才发送ajax请求，（input中输入一个字符，插件会向服务器发送一次，设置限制，6字符以上才开始）
                     remote: {//ajax验证。server result:{"valid",true or false} 向服务发送当前input name值，获得一个json数据。例表示正确：{"valid",true}
-                        url: '{{route('')}}',//验证地址
+                        url: '{{route('osce.wechat.user.getProofNumber')}}',//验证地址
                         message: '该手机号已经存在',//提示消息
                         delay :  2000,//每输入一个字符，就发ajax请求，服务器压力还是太大，设置2秒发送一次ajax（默认输入一个字符，提交一次，服务器压力太大）
                         type: 'get'//请求方式
