2d1639d29b22316bdf11eb63630a80f79f603656
zhouchong <zhouchong@misrobot.com>
2016-01-30 21:46:23 +0800

    测试调试

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -199,9 +199,14 @@ class IndexController extends CommonController
         }
         $student_id=$student_id->student_id;
         $screen_id=ExamOrder::where('exam_id','=',$exam_id)->where('student_id','=',$student_id)->first();
-        \Log::info($student_id);
-        \Log::info($exam_id);
-        \Log::info($screen_id);
+        if($screen_id){
+            $result=Watch::where('id',$id)->update(['status'=>0]);
+            if($result){
+                return \Response::json(array('code'=>2));
+            }else{
+                return \Response::json(array('code'=>0));
+            }
+        }
         $exam_screen_id=$screen_id->exam_screening_id;
         $ExamFinishStatus = ExamQueue::where('status', '=', 3)->where('student_id', '=', $student_id)->count();
         $ExamFlowModel = new  ExamFlow();
