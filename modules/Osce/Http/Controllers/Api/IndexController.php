dde250df1fc3f2676aa94e5442b51b48591445ef
zhouchong <zhouchong@misrobot.com>
2016-01-30 16:15:20 +0800

    测试调试

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -135,8 +135,8 @@ class IndexController extends CommonController
         if(!in_array($id_card,$idcards)){
             return \Response::json(array('code'=>5));
         }
-        \Log::info($idcards);
-        \Log::info($id_card);
+        \Log::info($idcards.'1');
+        \Log::info($id_card.'2');
         $screen_id=ExamOrder::where('exam_id',$exam_id)->where('student_id',$student_id)->select('exam_screening_id')->first();
         $exam_screen_id=$screen_id->exam_screening_id;
         $result = ExamScreeningStudent::create(['watch_id' => $id,'student_id'=>$student_id,'exam_screening_id'=>$exam_screen_id,'is_signin'=>1]);
@@ -156,7 +156,7 @@ class IndexController extends CommonController
             $watchModel = new WatchLog();
             $watchModel->historyRecord($data,$student_id,$exam_id,$exam_screen_id);
             ExamOrder::where('exam_id',$exam_id)->where('student_id',$student_id)->update(['status'=>1]);
-            Exam::where('exam_id',$exam_id)->update(['status'=>1]);
+            Exam::where('id',$exam_id)->update(['status'=>1]);
             return \Response::json(array('code' => 1));
         } else {
             return \Response::json(array('code' => 0));
