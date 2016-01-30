ef8da8be83dbcd604a9779d6c4ac445a595577e2
jiangzhiheng <j5110021@misrobot.com>
2016-01-30 17:19:32 +0800

    测试

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -241,13 +241,13 @@ class DrawlotsController extends CommonController
             $station = ExamQueue::where('room_id' , '=' , $roomId)
                 ->where('status' , '=' , 0)
                 ->get();
-            dd($station);
+            
             //获得该场考试的exam_id
             if ($station->isEmpty()) {
                 throw new \Exception('当前队列中找不到符合的考试');
             }
 
-            $examId = $station->exam_id;
+            $examId = $station->first()->exam_id;
 
             //判断如果是以考场分组，就抽签
             if (Exam::findOrFail($examId)->sequence_mode == 1) {
