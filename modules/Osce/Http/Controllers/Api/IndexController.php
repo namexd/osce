c7647a48ce2f4f9fdd833957ebebf8c277c78d42
zhouchong <zhouchong@misrobot.com>
2016-01-30 16:18:08 +0800

    测试调试

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -135,8 +135,8 @@ class IndexController extends CommonController
         if(!in_array($id_card,$idcards)){
             return \Response::json(array('code'=>5));
         }
-        \Log::info($idcards.'1');
-        \Log::info($id_card.'2');
+        \Log::info($idcards);
+        \Log::info($id_card);
         $screen_id=ExamOrder::where('exam_id',$exam_id)->where('student_id',$student_id)->select('exam_screening_id')->first();
         $exam_screen_id=$screen_id->exam_screening_id;
         $result = ExamScreeningStudent::create(['watch_id' => $id,'student_id'=>$student_id,'exam_screening_id'=>$exam_screen_id,'is_signin'=>1]);
