96e82ef16d439118eb8a546f0cc6b76eae7f95aa
jiangzhiheng <j5110021@misrobot.com>
2016-01-30 16:57:44 +0800

    测试提交

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -207,10 +207,10 @@ class DrawlotsController extends CommonController
             }
 
             $studentId = $watchLog->student_id;
-            return [$studentId,$roomId];
             //如果考生走错了房间
+            $temp = [$studentId,$roomId];
             if (!is_null(ExamQueue::where('room_id',$roomId)->where('student_id',$studentId)->select('id')->first())) {
-                throw new \Exception('该名学生走错了考场！');
+                throw new \Exception($temp);
             }
 
             //使用抽签的方法进行抽签操作
