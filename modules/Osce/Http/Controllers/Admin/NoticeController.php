a81327b1896b27069b151d50132c5a72d64a2fd2
luohaihua <luohaihua@misrobot.com>
2016-02-01 20:32:40 +0800

    uuu

--- modules/Osce/Http/Controllers/Admin/NoticeController.php
+++ modules/Osce/Http/Controllers/Admin/NoticeController.php
@@ -110,11 +110,11 @@ class NoticeController extends CommonController
             }
             $noticeModel = new Notice();
             if ($noticeModel->sendNotice($title, $content, $exam_id, $groups, $attach)) {
+                dd(123);
                 return redirect()->route('osce.admin.notice.getList');
             } else {
                 throw new \Exception('通知创建失败');
             }
-            dd(123);
         } catch (\Exception $ex) {
             return redirect()->back()->withErrors($ex->getMessage());
         }
