2dda6b910cf77cbf14fb6bea38f1aa7f9cba67ff
jiangzhiheng <j5110021@misrobot.com>
2016-02-02 11:46:06 +0800

    测试提交

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -270,8 +270,10 @@ class DrawlotsController extends CommonController
             //将考场名字和考站名字封装起来
             $station->name = $room->name . '-' . $station->name;
 
+            //将考场的id封装进去
+            $station->room_id = $room->id;
 
-
+            //将考试的id封装进去
             $station->exam_id = $exam->id;
 
             return response()->json($this->success_data($station));
