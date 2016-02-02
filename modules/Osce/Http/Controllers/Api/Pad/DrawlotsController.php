3e941f2385de428080d2b0c5cc662dfd7b04a272
jiangzhiheng <j5110021@misrobot.com>
2016-02-02 17:18:01 +0800

    测试

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -151,7 +151,7 @@ class DrawlotsController extends CommonController
             $examId = $exam->id;
 
             list($room_id, $station, $stationNum) = $this->getRoomIdAndStation($id,$exam);
-
+            dd($stationNum);
 
             $examQueue = ExamQueue::nextExamineeByRoomId($room_id, $examId,$stationNum);
     //        $examQueue = [
