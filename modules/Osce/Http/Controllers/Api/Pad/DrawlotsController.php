6403db9faaac84287e61e7cc588364d35d425289
jiangzhiheng <j5110021@misrobot.com>
2016-02-02 14:55:54 +0800

    测试

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -225,8 +225,8 @@ class DrawlotsController extends CommonController
             }
 
             $studentId = $watchLog->student_id;
+        dd($studentId);
             //如果考生走错了房间
-//            dd($studentId,$roomId);
             if (ExamQueue::where('room_id',$roomId)->where('student_id',$studentId)->get()->isEmpty()) {
                 throw new \Exception('当前考生走错了考场');
             }
