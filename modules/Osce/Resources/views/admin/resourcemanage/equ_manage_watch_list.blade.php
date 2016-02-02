683f9368e8d9a26c4d28344d7c7721d844d81138
mao <maoyungang@misrobot.com>
2016-02-02 17:34:03 +0800

    使用记录

--- modules/Osce/Resources/views/admin/resourcemanage/equ_manage_watch_list.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/equ_manage_watch_list.blade.php
@@ -27,14 +27,14 @@
             elem: '#start', //需显示日期的元素选择器
             event: 'click', //触发事件
             format: 'YYYY-MM-DD hh:mm:ss', //日期格式
-            istime: false, //是否开启时间选择
+            istime: true, //是否开启时间选择
             isclear: true, //是否显示清空
             istoday: true, //是否显示今天
             issure: true, //是否显示确认
             festival: true, //是否显示节日
             min: '1900-01-01 00:00:00', //最小日期
             max: '2099-12-31 23:59:59', //最大日期
-            start: '2014-6-15 23:00:00',    //开始日期
+            start: layer.now,    //开始日期
             fixed: false, //是否固定在可视区域
             zIndex: 99999999, //css z-index
             choose: function(dates){ //选择好日期的回调
@@ -46,14 +46,14 @@
             elem: '#end', //需显示日期的元素选择器
             event: 'click', //触发事件
             format: 'YYYY-MM-DD hh:mm:ss', //日期格式
-            istime: false, //是否开启时间选择
+            istime: true, //是否开启时间选择
             isclear: true, //是否显示清空
             istoday: true, //是否显示今天
             issure: true, //是否显示确认
             festival: true, //是否显示节日
             min: '1900-01-01 00:00:00', //最小日期
             max: '2099-12-31 23:59:59', //最大日期
-            start: '2014-6-15 23:00:00',    //开始日期
+            start: layer.now,    //开始日期
             fixed: false, //是否固定在可视区域
             zIndex: 99999999, //css z-index
             choose: function(dates){ //选择好日期的回调
@@ -98,13 +98,13 @@
                     <div class="col-md-2 col-sm-2 col-xs-12">
                         <label class="pull-left left-text">使用时间:</label>
                         <div class="pull-left right-list">
-                            <input class="form-control m-b" name="begin_dt"  id="start"/>
+                            <input class="form-control" name="begin_dt"  id="start"/>
                         </div>
                     </div>
                     <div class="col-md-2 col-sm-2 col-xs-12">
                         <label class="pull-left left-text">到</label>
                         <div class="pull-left right-list">
-                            <input class="form-control m-b" name="end_dt" id="end"/>
+                            <input class="form-control" name="end_dt" id="end"/>
                         </div>
                     </div>
 
