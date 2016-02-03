ebf7f611da4964afa0e2840b65a839a2e43bd7eb
zhouqiang <candyphp@163.com>
2016-02-03 16:10:26 +0800

    监考老师页面绑定

--- modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
+++ modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
@@ -103,6 +103,7 @@ class StudentExamQueryController extends CommonController
         try {
             //TODO 根据学生id查出学生姓名和电话监考老师成绩查询时用
 
+
             $examTime = Exam::where('id', $examId)->select('begin_dt', 'end_dt', 'name')->first();
 
             //根据考试id找到对应的考试场次
@@ -266,7 +267,7 @@ class StudentExamQueryController extends CommonController
      * @access public
      * @param Request $request get请求<br><br>
      * <b>get请求字段：</b>
-     * @return json
+     * @return  json
      * @version 0.4
      * @author zhouqiang <zhouqiang@misrobot.com>
      * @date
@@ -282,6 +283,7 @@ class StudentExamQueryController extends CommonController
         $examId = $request->get('exam_id');
         $stationId = $request->get('station_id');
         //根据考站找到对应的科目
+        $examTime = Exam::where('id', $examId)->select('begin_dt', 'end_dt', 'name')->first();
         $subjectId = Station::find($stationId)->subject_id;
         $subjectName = Subject::find($subjectId)->title;
         //调用科目成绩统计查询的接口方法
@@ -308,24 +310,29 @@ class StudentExamQueryController extends CommonController
 
         //获取该考试科目所有的学生
         $studentData = $studentModel->getStudentByExamAndSubject($examId, $subjectId);
-        $subjectData=[];
+        $subjectData = [];
         //根据考生id查出该考试在本考试的总成绩
         foreach ($studentData as $studentId) {
             //调用查看总成绩的方法
             $tesresultModel = new TestResult();
             $StudentScores = $tesresultModel->AcquireExam($studentId->student_id);
             $item[$studentId->student_name] = $StudentScores;
-            $subjectData[]=[
-                'avg_score'=>$item['avg_score'],
-                'avg_time'=>$item['avg_time'],
-                'avg_total'=>$item['avg_total'],
-                'student_name'=>$studentId->student_name,
-                'subject_name'=>$subjectName,
-                'student_id'=>$studentId->student_id,
-                'Scores'=>$StudentScores,
+            $subjectData[] = [
+                'avg_score' => $item['avg_score'],
+                'avg_time' => $item['avg_time'],
+                'avg_total' => $item['avg_total'],
+                'student_name' => $studentId->student_name,
+                'subject_name' => $subjectName,
+                'student_id' => $studentId->student_id,
+                'exam_id' => $examId,
+                'Scores' => $StudentScores,
+                'exam_begin_dt' => $examTime->begin_dt,
+                'exam_end_dt' => $examTime->end_dt,
             ];
         }
-          dd($subjectData);
+        return response()->json(
+            $this->success_data($subjectData, 1, '科目数据传送成功')
+        );
     }
 
 
