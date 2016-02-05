bff87d1d64d64fe8c6efdf090382002298c0941a
lizhiyuan <lizhiyuan@misrobot.com>
2016-02-05 15:16:19 +0800

    下拉列表输入框长度

--- modules/Osce/Resources/views/admin/resourcemanage/examroom_edit.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/examroom_edit.blade.php
@@ -154,11 +154,13 @@
 @section('footer_js')
     @parent
     <script src="{{asset('/osce/common/select2-4.0.0/js/select2.full.min.js')}}"></script>
+    <script src="{{asset('/osce/common/select2-4.0.0/js/i18n/zh-CN.js')}}"></script>
     <script>
         $(function(){
             $('[name=cate]').select2({
                 tags: true,
-                tokenSeparators: [',', ' ']
+                tokenSeparators: [',', ' '],
+                maximumInputLength: 12
             }).change(function(){
                 var val =   $(this).val();
                 val     =   val.toString();
