f2bf83668d59adbc262ebb12c0efdc198c6a162b
zhouqiang <candyphp@163.com>
2016-02-05 17:37:59 +0800

    修改监考老师成绩查询

--- modules/Osce/Resources/views/wechat/resultquery/examination_list.blade.php
+++ modules/Osce/Resources/views/wechat/resultquery/examination_list.blade.php
@@ -30,36 +30,5 @@
 		<span class="tit">&nbsp;&nbsp;考试时间</span>&nbsp;&nbsp;<span class="time"></span>
 	</div>
     <ul id="exmination_ul">
-    	<!--<li>
-    		<dl>
-    			<dd>考站1：82分</dd>
-    			<dd>用时：18分23秒</dd>
-    			<dd>评价老师：李老师</dd>
-    		</dl>
-    		<p class="clearfix see_msg">
-    			<a class="nou right" href="#">考卷详情&nbsp;&gt;&nbsp;&nbsp;</a>
-    		</p>
-    	</li>
-    	<li>
-    		<dl>
-    			<dd>考站2：82分</dd>
-    			<dd>用时：18分23秒</dd>
-    			<dd>评价老师：李老师</dd>
-    			<dd>SP病人：SP病人</dd>
-    		</dl>
-    		<p class="clearfix see_msg">
-    			<a class="nou right" href="#">考卷详情&nbsp;&gt;&nbsp;&nbsp;</a>
-    		</p>
-    	</li>
-    	<li>
-    		<dl>
-    			<dd>考站3：82分</dd>
-    			<dd>用时：18分23秒</dd>
-    			<dd>理论考试</dd>
-    		</dl>
-    		<p class="clearfix see_msg">
-    			<a class="nou right" href="#">考卷详情&nbsp;&gt;&nbsp;&nbsp;</a>
-    		</p>
-    	</li>-->
     </ul>
 @stop
