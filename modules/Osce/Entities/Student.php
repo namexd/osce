24335f3a7d47a71b0171ff52a68babf6bb6d0c92
zhoufuxiang <zhoufuxiang@163.com>
2016-02-17 15:45:32 +0800

    修改：接口，考生列表

--- modules/Osce/Entities/Student.php
+++ modules/Osce/Entities/Student.php
@@ -346,10 +346,10 @@ class Student extends CommonModel
                 })->where('exam_order.exam_id','=',$exam_id)->where('exam_order.exam_screening_id','=',$screen_id);
 
         //剔除本场考试中 已考试过的 学生 //TODO zhoufuxiang
-        $builder= $builder->leftjoin('exam_screening_student',function($join){
-                    $join ->on('exam_order.student_id', '=', 'exam_screening_student.student_id');
-                    $join ->on('exam_order.exam_screening_id', '=', 'exam_screening_student.exam_screening_id');
-                })->where('exam_screening_student.is_end', '<>', 1);
+//        $builder= $builder->leftjoin('exam_screening_student',function($join){
+//                    $join ->on('exam_order.student_id', '=', 'exam_screening_student.student_id');
+//                    $join ->on('exam_order.exam_screening_id', '=', 'exam_screening_student.exam_screening_id');
+//                })->where('exam_screening_student.is_end', '<>', 1);
 
         $builder=$builder->where(function($query){
                     $query->where('exam_order.status','=',0)->orWhere('exam_order.status','=',4);
