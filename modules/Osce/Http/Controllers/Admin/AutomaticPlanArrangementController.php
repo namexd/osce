9f85a45af9417e8d274ed94e1f6203749cf9dd64
jiangzhiheng <j5110021@misrobot.com>
2016-02-22 10:01:22 +0800

    智能排考的新内容

---
+++ modules/Osce/Http/Controllers/Admin/AutomaticPlanArrangementController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j5110
+ * Date: 2016/2/19
+ * Time: 9:32
+ */
+
+namespace Modules\Osce\Http\Controllers\Admin;
+
+
+use Modules\Osce\Entities\AutomaticPlanArrangement\AutomaticPlanArrangement;
+use Modules\Osce\Entities\AutomaticPlanArrangement\ExamPlaceEntity;
+use Modules\Osce\Entities\AutomaticPlanArrangement\Exam;
+use Modules\Osce\Http\Controllers\CommonController;
+use Illuminate\Http\Request;
+
+class AutomaticPlanArrangementController extends CommonController
+{
+
+    function getIndex() {
+        //todo 着陆页，先不忙
+    }
+
+    /**
+     * 开始排考
+     * @author Jiangzhiheng
+     * @time 2016-02-19 09:34
+     * @param Request $request
+     */
+    function postBegin(Request $request) {
+        $this->validate($request,[
+            'exam_id' => 'required|integer'
+        ]);
+
+        $examId = $request->input('exam_id');
+
+        $automaticPlanArrangement = new AutomaticPlanArrangement($examId,new ExamPlaceEntity(),new Exam());
+        $automaticPlanArrangement->plan($examId);
+    }
+}
