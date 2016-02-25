34a2059f2feb8e6ee0bd77476cd660160f74fe35
zhouqiang <candyphp@163.com>
2016-02-25 14:20:32 +0800

    测试抽签

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -199,7 +199,7 @@ class DrawlotsController extends CommonController
      */
     public function getStation(Request $request)
     {
-//        try {
+        try {
             //验证
             $this->validate($request, [
                 'uid' => 'required|string',
@@ -273,9 +273,9 @@ class DrawlotsController extends CommonController
 
             return response()->json($this->success_data($result));
 
-//        } catch (\Exception $ex) {
-//            return response()->json($this->fail($ex));
-//        }
+        } catch (\Exception $ex) {
+            return response()->json($this->fail($ex));
+        }
     }
 
     /**
@@ -334,7 +334,7 @@ class DrawlotsController extends CommonController
     private function drawlots($student, $roomId)
     {
 
-//        try {
+        try {
             //获取正在考试中的考试
             $examId = $student->exam_id;
 
@@ -448,9 +448,9 @@ class DrawlotsController extends CommonController
                 return Station::findOrFail($stationId);
             }
 
-//        } catch (\Exception $ex) {
-//            throw $ex;
-//        }
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
     }
 
     /**
