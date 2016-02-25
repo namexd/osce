10f4a41521cc2525c0ee4878eba42be9d848cc6d
zouzhiqiang <zouzhiqiang@misrobot.com>
2016-02-25 15:28:07 +0800

    新增场所

--- modules/Osce/Resources/views/admin/resourcemanage/examroom_add.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/examroom_add.blade.php
@@ -1,6 +1,10 @@
 @extends('osce::admin.layouts.admin_index')
 @section('only_css')
     <link href="{{asset('/osce/common/select2-4.0.0/css/select2.min.css')}}" rel="stylesheet">
+    <style type="text/css">
+    	.select2-container--default .select2-selection--single{border:1px solid #e5e6e7;height:34px;line-height:34px;}
+    	.select2-container--default .select2-selection--single .select2-selection__rendered{line-height:34px;}
+    </style>
 @stop
 
 @section('only_js')
