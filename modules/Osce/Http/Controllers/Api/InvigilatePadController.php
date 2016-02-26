bef88afc16216713d087b25142104547b1d6a5b5
jiangzhiheng <j5110021@misrobot.com>
2016-02-26 09:57:39 +0800

    test commit

--- modules/Osce/Http/Controllers/Api/InvigilatePadController.php
+++ modules/Osce/Http/Controllers/Api/InvigilatePadController.php
@@ -354,7 +354,7 @@ class InvigilatePadController extends CommonController
             $studentName = $student->name;
             $studentCode = $student->code;
             $stationName = Station::findOrFail($stationId)->first()->name;
-            if (is_null$exam) {
+            if (is_null($exam)) {
                 throw new \Exception('当前没有正在进行的考试！',-701);
             }
             $examName = $exam->name;
