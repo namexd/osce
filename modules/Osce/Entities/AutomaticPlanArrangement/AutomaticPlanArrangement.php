25deb4ba652dd7e67ed49a29a1a9ca04cd9c2b34
jiangzhiheng <j5110021@misrobot.com>
2016-02-27 12:18:21 +0800

    测试提交

--- modules/Osce/Entities/AutomaticPlanArrangement/AutomaticPlanArrangement.php
+++ modules/Osce/Entities/AutomaticPlanArrangement/AutomaticPlanArrangement.php
@@ -241,8 +241,8 @@ class AutomaticPlanArrangement
             ->  select(\DB::raw(
                 implode(
                     [
-                        'count(`id`) as flowsNum',
-                        'id',
+                        'count(`id`) as flowsNum,',
+                        'id,',
                         'student_id',
                     ]
                 )
