c5586b90cd5b380a7bb90c9e46bf9c4e10d59156
zengjie <zengjie@misrobot.com>
2016-03-01 15:41:09 +0800

    考生科目分析视图

---
+++ modules/Osce/Resources/views/admin/statisticalanalysis/statistics_student_subject.blade.php
@@ -0,0 +1,80 @@
+@extends('osce::admin.layouts.admin_index')
+
+@section('only_css')
+
+@stop
+
+@section('only_js')
+    <!-- ECharts -->
+    <script src="{{asset('osce/admin/plugins/js/plugins/echarts/echarts-all.js')}}"></script>
+<script>
+
+
+</script>
+@stop
+
+
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row table-head-style1 ">
+            <div class="col-xs-6 col-md-2">
+                <h5 class="title-label">考生成绩分析</h5>
+            </div>
+        </div>
+        <div class="container-fluid ibox-content">
+            <div class="panel-heading">
+                <div class="panel-options">
+                    <ul class="nav nav-tabs">
+                        <li><a href="">考生成绩分析</a></li>
+                        <li class="active"><a href="">考生科目分析</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="panel blank-panel">
+            <div class="container-fluid ibox-content" style="border: none;">
+                <div class="input-group" style="margin:20px 0;">
+                    <label for="" class="pull-left exam-name">考生名称：</label>
+                    <select name="name" class="input-sm form-control student_select" style="width: 210px;height: 34px">
+
+                            <option value=""></option>
+
+                    </select>
+                    <label for="" class="pull-left exam-name" style="margin-left: 20px;">科目名称：</label>
+                    <select name="name" class="input-sm form-control subject_select" style="width: 210px;height: 34px">
+
+                            <option value=""></option>
+
+                    </select>
+                    <button type="submit" class="btn btn-sm btn-primary marl_10" id="search">搜索</button>
+                </div>
+                <div class="list_all">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="ibox-content" style="border: none;">
+                                <div class="echarts" id="echarts-Subject"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <table class="table table-striped" id="table-striped" style="background:#fff">
+                        <thead>
+                        <tr>
+                            <th>序号</th>
+                            <th>考试</th>
+                            <th>考试时间</th>
+                            <th>平均耗时</th>
+                            <th>平均成绩</th>
+                            <th>用时</th>
+                            <th>成绩</th>
+                            <th>操作</th>
+                        </tr>
+                        </thead>
+                        <tbody class="subjectBody">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop{{-- 内容主体区域 --}}
