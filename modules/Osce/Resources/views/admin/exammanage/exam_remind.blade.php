91e54cfda604a220a3c976b1754ac274c4adf361
mao <maoyungang@misrobot.com>
2016-03-01 09:54:12 +0800

    大屏

--- modules/Osce/Resources/views/admin/exammanage/exam_remind.blade.php
+++ modules/Osce/Resources/views/admin/exammanage/exam_remind.blade.php
@@ -8,16 +8,16 @@
         dt, dd, ol, ul, li, fieldset, form, label, legend{margin: 0px;padding: 0px}
         body{width:100%;height:100%;background:#E1E1E8;font-size:16px;line-height:1em;font-family: "微软雅黑";position:relative}
         ul li{ list-style:none}
-        .pin_box{width:720px;height:500px;margin:50px auto 0;}
+        .pin_box{width:100%;height:auto;margin:50px auto 0;padding: 20px 20px 0 20px;font-size: 44px;}
         .pin_box .red{float:right;color:#ED5565;}
         .clearfix:after{content:""; display:table;  clear:both; }
         .clearfix{*zoom:1}
-        .pin_title{height:40px;line-height:40px;color:#000;font-weight:bold;text-align: center;}
+        .pin_title{height:80px;line-height:80px;color:#000;font-weight:bold;text-align: center;}
         #marquee{margin-top:20px}
         #marquee p{margin:0;line-height:2em;}
-        #name_list{width:100%;height:240px;background:#fff}
+        #name_list{width:100%;height:auto;min-height:420px;background:#fff}
         #name_list dl{float:left}
-        #name_list dl dt{height:40px;line-height:40px;background:#2B3A40;color:#fff;text-align:center}
+        #name_list dl dt{height:80px;line-height:80px;background:#2B3A40;color:#fff;text-align:center}
         #name_list dl dd{text-align: center;line-height:1.5em;padding:10px;color:#333;}
     </style>
 @stop
@@ -36,7 +36,7 @@
             @endforeach
         </div>
         <marquee id="marquee" style="width:100%;height:200px" Behaviour="alternate" scrollamount="2" direction="up" >
-            <p style="font-size:18px;color:#ED5565;">考场纪律说明：</p>
+            <p style="font-size:44px;color:#ED5565;">考场纪律说明：</p>
             {!! $exams->rules !!}
         </marquee>
     </div>
