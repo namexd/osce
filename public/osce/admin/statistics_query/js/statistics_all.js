5fbea2ae655dd178f947dddb8b3c6b049cba1b1d
May <wulengmei@misrobot.com>
2016-03-01 09:22:36 +0800

    折线图

--- public/osce/admin/statistics_query/js/statistics_all.js
+++ public/osce/admin/statistics_query/js/statistics_all.js
@@ -7,6 +7,7 @@ $(function(){
     pars = JSON.parse(($("#parameter").val()).split("'").join('"'));
     switch(pars.pagename){
         case "subject_statistics":subject_statistics();break;//科目成绩分析
+        case "subject_level":subject_level();break;//科目难度分析
     }
 });
 
@@ -37,6 +38,115 @@ function subject_statistics(){
                     }]
             };
         t.setOption(n);
+    }
+    //默认加载最近一次考试
+    var $subId = $(".subject_select").children().first().val();
+    var url = pars.ajaxUrl;
+    $.ajax({
+        url:url+'?id='+$subId,
+        type:'get',
+        cache:false,
+        success:function(res){
+            var standardStr = res.data.StrList.standardStr.split(",");
+            var scoreAvgStr=res.data.StrList.scoreAvgStr.split(",");
+            if(standardStr){echartsSubject(standardStr,scoreAvgStr);}//科目成绩分析。
+            $(res.data.list).each(function(){
+                $(".subjectBody").append('<tr>' +
+                    '<td>'+this.number+'</td>' +
+                    '<td>'+this.title+'</td>' +
+                    '<td>'+this.mins+'</td>' +
+                    '<td>'+this.timeAvg+'</td>' +
+                    '<td>'+this.scoreAvg+'</td>' +
+                    '<td>'+this.studentQuantity+'</td>' +
+                    '<td>'+this.qualifiedPass+'</td>' +
+                    '<td>' +
+                    '<a href="">' +
+                    '<span class="read state1 detail"><i class="fa fa-search fa-2x"></i></span>' +
+                    '</a>' +
+                    '</td></tr>')
+            })
+        }
+    });
+    //筛选
+    $("#search").click(function(){
+        var id = $(".subject_select").val();
+        $.ajax({
+            url:url+'?id='+id,
+            type:'get',
+            cache:false,
+            success:function(res){
+                console.log(res);
+                $(".subjectBody").empty();
+                var standardStr = res.data.StrList.standardStr.split(",");
+                var scoreAvgStr=res.data.StrList.scoreAvgStr.split(",");
+                if(standardStr){echartsSubject(standardStr,scoreAvgStr);}//科目成绩分析。
+                $(res.data.list).each(function(){
+                    $(".subjectBody").append('<tr>' +
+                        '<td>'+this.number+'</td>' +
+                        '<td>'+this.title+'</td>' +
+                        '<td>'+this.mins+'</td>' +
+                        '<td>'+this.timeAvg+'</td>' +
+                        '<td>'+this.scoreAvg+'</td>' +
+                        '<td>'+this.studentQuantity+'</td>' +
+                        '<td>'+this.qualifiedPass+'</td>' +
+                        '<td>' +
+                        '<a href="">' +
+                        '<span class="read state1 detail"><i class="fa fa-search fa-2x"></i></span>' +
+                        '</a>' +
+                        '</td></tr>')
+                })
+            }
+        })
+    });
+};
+
+
+function subject_level(){
+    function echartsSubject(standardStr,scoreAvgStr){//科目成绩分析。
+        var e = echarts.init(document.getElementById("echarts-Subject")),
+            a = {
+
+                tooltip: {
+                    trigger: "axis"
+                },
+                legend: {
+                    data: ["合格率"]
+                },
+                calculable: !0,
+                xAxis: [{
+                    type: "category",
+                    boundaryGap: !1,
+                    data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+                }],
+                yAxis: [{
+                    type: "value",
+                    axisLabel: {
+                        formatter: "value"
+                    }
+                }],
+                series: [{
+                    name: "最高气温",
+                    type: "line",
+                    data: [11, 11, 15, 13, 12, 13, 10],
+                    markPoint: {
+                        data: [{
+                            type: "max",
+                            name: "最大值"
+                        },
+                            {
+                                type: "min",
+                                name: "最小值"
+                            }]
+                    },
+                    markLine: {
+                        data: [{
+                            type: "average",
+                            name: "平均值"
+                        }]
+                    }
+                }]
+            };
+        e.setOption(a);
     }
     //默认加载最近一次考试
     var $subId = $(".subject_select").children().first().val();
