269c4cd07889224aea72221d821b8145094e83ac
weihuiguo <weihuiguo@misrobot.com>
2016-03-03 14:44:53 +0800

    调试

--- modules/Osce/Http/Controllers/Admin/TestScoresController.php
+++ modules/Osce/Http/Controllers/Admin/TestScoresController.php
@@ -108,14 +108,14 @@ class TestScoresController  extends CommonController
         $student_id = $request->student_id;
         //$examid = $request->examid;
         $subid = $request->subid;
+        $examid = [];
         //获取学生科目成绩
-        $singledata = $TestScoreRepositories->getStudentScoreAvg($student_id,$subid);
-
-        dd($singledata);
+        $singledata = $TestScoreRepositories->getStudentHistoryScoreCount($student_id,$subid);
+        if(!empty($singledata)){
+            $examid = $singledata->pluck('exam_id');
+        }
         //获取科目平均成绩
-        //dd($singledata);
-        $avgdata = $TestScoreRepositories->getStudentScoreCount('',0,$subid)->toArray();
-        dd($avgdata);
+        $avgdata = $TestScoreRepositories->getStudentHistoryScoreCount($student_id,$subid,$examid);
         foreach($singledata as $k=>$v){
             foreach($avgdata as $kk=>$vv){
                 if($v['id'] == $vv['id']){
@@ -310,16 +310,16 @@ class TestScoresController  extends CommonController
                 $allData .= $vv['avgScore'].',';
             }
             $classData .= $v['avgScore'].',';
-            $timeData .= $v['begin_dt'].',';
+            $timeData .= $v['name'].',';
         }
-
+        //dd($datalist);
         $data = [
             'datalist' => $datalist,//列表数据
             'classData' => trim($classData,','),//班级平均分
             'allData' => trim($allData,','),//考试平均分
             'timeData' => trim($timeData,',')//考试时间
         ];
-        //dd($data);
+        dd($data);
         return $this->success_data(['data'=>$data]);
     }
 
