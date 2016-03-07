5af8f0eef4b643205b160ffa7ad910c38288e23d
jiangzhiheng <j5110021@misrobot.com>
2016-03-07 18:34:41 +0800

    测试提交

--- modules/Osce/Http/Controllers/Api/InvigilatePadController.php
+++ modules/Osce/Http/Controllers/Api/InvigilatePadController.php
@@ -483,12 +483,11 @@ class InvigilatePadController extends CommonController
 
             //获取当前日期
             $date = date('Y-m-d');
-            $radios = $request->file('radio');
-            \Log::info('123',[$radios]);
+            \Log::info('file', [$_FILES]);
             if (!$request->hasFile('radio')) {
                 throw new \Exception('上传的音频不存在', -120);
             } else {
-//                $radios = $request->file('radio');
+                $radios = $request->file('radio');
                 if (!$radios->isValid()) {
                     throw new \Exception('上传的音频出错', -130);
                 }
