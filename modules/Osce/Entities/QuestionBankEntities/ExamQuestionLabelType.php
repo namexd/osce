9d75de72236d08fe8e377a4ea5674845ef323102
tangjun <tangjun@misrobot.com>
2016-03-07 16:57:30 +0800

    模型修改

---
+++ modules/Osce/Entities/QuestionBankEntities/ExamQuestionLabelType.php
@@ -0,0 +1,26 @@
+<?php
+namespace Modules\Osce\Entities\QuestionBankEntities;
+use Illuminate\Database\Eloquent\Model;
+class ExamQuestionLabelType extends  Model
+{
+    protected $connection	=	'osce_mis';
+    protected $table 		= 	'exam_question_label_type';
+    public $timestamps	=	true;
+    protected $primaryKey	=	'id';
+    protected $fillable 	=	['id', 'name','status'];
+    /**获取标签类型列表
+     * @method
+     * @url /osce/
+     * @access public
+     * @author xumin <xumin@misrobot.com>
+     * @date
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function labelTypeList(){
+        $data = $this->select('id','name')->orderBy('created_at','desc')->get();
+        return $data;
+    }
+
+}
+
+
