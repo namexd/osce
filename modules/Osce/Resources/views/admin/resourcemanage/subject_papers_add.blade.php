46a888c6b1f0a7c544c25bd83ff7760c2f8acd84
May <wulengmei@misrobot.com>
2016-03-08 11:20:08 +0800

    新增页面

--- modules/Osce/Resources/views/admin/resourcemanage/subject_papers_add.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/subject_papers_add.blade.php
@@ -13,83 +13,75 @@
     <div class="wrapper wrapper-content animated fadeInRight">
         <div class="row table-head-style1 ">
             <div class="col-xs-6 col-md-2">
-                <h5 class="title-label">题库管理</h5>
-            </div>
-        </div>
-        <div class="container-fluid ibox-content" style="padding-bottom: 0;">
-            <div class="panel-heading">
-                <div class="panel-options">
-                    <ul class="nav nav-tabs">
-                        <li><a href="{{route('osce.admin.ExamLabelController.getExamLabel')}}">考核标签</a></li>
-                        <li><a href="">题库管理</a></li>
-                        <li  class="active"><a href="{{route('osce.admin.ExamPaperController.getExamList')}}">试卷管理</a></li>
-                    </ul>
-                </div>
+                <h5 class="title-label">新增试卷</h5>
             </div>
         </div>
         <div class="panel blank-panel">
-            <div class="container-fluid ibox-content" style="border: none;">
-                <div class="input-group" style="width: 100%;margin:20px 0;">
-                    <label for="" class="pull-left exam-name">试卷名称：</label>
-                    <input type="text" placeholder="请输入标签名称" name="tagName" class="input-md form-control" style="width: 250px;">
+            <form method="post" class="form-horizontal" id="sourceForm" action="{{route('osce.admin.machine.postAddMachine')}}">
 
-                    <button type="submit" class="btn btn-sm btn-primary marl_10" id="search">查询</button>
-                    <button type="submit" class="btn btn-sm btn-primary marl_10 pull-right" id="add">新增</button>
-                </div>
-                <div class="list_all">
-                    <table class="table table-striped" id="table-striped" style="background:#fff">
-                        <thead>
-                            <tr>
-                                <th>序号</th>
-                                <th>试卷名称</th>
-                                <th>题目数量</th>
-                                <th>总分</th>
-                                <th>试卷类型</th>
-                                <th>操作</th>
-                            </tr>
-                        </thead>
-                        <tbody class="subjectBody">
-                            {{$data['examList']}}
-                            @if(!empty(@$data['examList']))
-                                @foreach(@$data['examList'] as $val)
-                                    <tr>
-                                        <td>{{@$val+1}}</td>
-                                        <td>{{@$val['ExaminationPaper']['name']}}</td>
-                                        <td>{{@$val['ExaminationPaper']['num']}}</td>
-                                        <td>{{@$val['ExaminationPaper']['total_score']}}</td>
-                                        @if($val['ExaminationPaper']['type']===1)
-                                            <td>随机试卷</td>
-                                        @else
-                                            <td>统一试卷</td>
-                                        @endif
-                                        <td>
-                                            <a href="javascript:void(0)">
-                                        <span class="read state1 detail">
-                                            <i class="fa fa-cog fa-2x"></i>
-                                        </span>
-                                            </a>
-                                            <a href="javascript:void(0)">
-                                        <span class="read state2">
-                                            <i class="fa fa-trash-o fa-2x"></i>
-                                        </span>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
-                    <div class="pull-left">共10条</div>
-                    <div class="btn-group pull-right">
-                        <ul class="pagination">
-                            <li class="disabled"><span>«</span></li>
-                            <li class="active"><span>1</span></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="" rel="next">»</a></li>
-                        </ul>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">设备名称</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name">
+                        <input type="hidden"  class="form-control" id="cate_id" name="cate_id" value="2" />
                     </div>
                 </div>
-            </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">设备ID</label>
+                    <div class="col-sm-10">
+                        <input type="text"  class="form-control" id="code" name="code">
+                    </div>
+                </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">厂家</label>
+                    <div class="col-sm-10">
+                        <input type="text"  class="form-control" id="factory" name="factory">
+                    </div>
+                </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">型号</label>
+                    <div class="col-sm-10">
+                        <input type="text"  class="form-control" id="sp" name="sp">
+                    </div>
+                </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">采购日期</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="laydate-icon" id="purchase_dt" name="purchase_dt" readonly="readonly">
+                    </div>
+                </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">状态</label>
+                    <div class="col-sm-10">
+                        <select id="status"   class="form-control m-b" name="status">
+                            <option value="0">正常</option>
+                            @foreach($status as $key => $value)
+                                @if($key >1)
+                                    <option value="{{$key}}">{{$value}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="hr-line-dashed"></div>
+
+                <div class="form-group">
+                    <div class="col-sm-4 col-sm-offset-2">
+                        <button class="btn btn-primary" type="submit">保存</button>
+                        <a class="btn btn-white" href="{{route("osce.admin.machine.getMachineList",["cate_id"=>2])}}">取消</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @stop{{-- 内容主体区域 --}}
