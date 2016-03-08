62ecf6dd65addae326ed4294e2cad5d2f6585bef
May <wulengmei@misrobot.com>
2016-03-08 11:01:37 +0800

    试卷页面

--- modules/Osce/Resources/views/admin/resourcemanage/subject_papers_add.blade.php
+++ modules/Osce/Resources/views/admin/resourcemanage/subject_papers_add.blade.php
@@ -5,7 +5,6 @@
 @stop
 
 @section('only_js')
-    <script src="{{asset('osce/admin/subjectManage/check_tag.js')}}"></script>
     <script src="{{asset('osce/admin/plugins/js/plugins/layer/layer.min.js')}}"></script>
 @stop
 
@@ -21,9 +20,9 @@
             <div class="panel-heading">
                 <div class="panel-options">
                     <ul class="nav nav-tabs">
-                        <li class="active"><a href="{{route('osce.admin.ExamLabelController.getExamLabel')}}">考核标签</a></li>
+                        <li><a href="{{route('osce.admin.ExamLabelController.getExamLabel')}}">考核标签</a></li>
                         <li><a href="">题库管理</a></li>
-                        <li><a href="{{route('osce.admin.ExamPaperController.getExamList')}}">试卷管理</a></li>
+                        <li  class="active"><a href="{{route('osce.admin.ExamPaperController.getExamList')}}">试卷管理</a></li>
                     </ul>
                 </div>
             </div>
@@ -31,13 +30,9 @@
         <div class="panel blank-panel">
             <div class="container-fluid ibox-content" style="border: none;">
                 <div class="input-group" style="width: 100%;margin:20px 0;">
-                    <label for="" class="pull-left exam-name">标签名称：</label>
+                    <label for="" class="pull-left exam-name">试卷名称：</label>
                     <input type="text" placeholder="请输入标签名称" name="tagName" class="input-md form-control" style="width: 250px;">
-                    <label for="" class="pull-left exam-name" style="margin-left: 20px;">标签类型：</label>
-                    <select name="name" id="subject-id" class="input-sm form-control subject_select" style="width: 250px;height: 34px">
-                        <option value="">难度标签</option>
-                        <option value="">能力标签</option>
-                    </select>
+
                     <button type="submit" class="btn btn-sm btn-primary marl_10" id="search">查询</button>
                     <button type="submit" class="btn btn-sm btn-primary marl_10 pull-right" id="add">新增</button>
                 </div>
@@ -46,31 +41,42 @@
                         <thead>
                             <tr>
                                 <th>序号</th>
-                                <th>标签</th>
-                                <th>标签类型</th>
-                                <th>描述</th>
+                                <th>试卷名称</th>
+                                <th>题目数量</th>
+                                <th>总分</th>
+                                <th>试卷类型</th>
                                 <th>操作</th>
                             </tr>
                         </thead>
                         <tbody class="subjectBody">
-                            <tr>
-                                <td>1</td>
-                                <td>基础医学</td>
-                                <td>科目标签</td>
-                                <td>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</td>
-                                <td>
-                                    <a href="javascript:void(0)">
+                            {{$data['examList']}}
+                            @if(!empty(@$data['examList']))
+                                @foreach(@$data['examList'] as $val)
+                                    <tr>
+                                        <td>{{@$val+1}}</td>
+                                        <td>{{@$val['ExaminationPaper']['name']}}</td>
+                                        <td>{{@$val['ExaminationPaper']['num']}}</td>
+                                        <td>{{@$val['ExaminationPaper']['total_score']}}</td>
+                                        @if($val['ExaminationPaper']['type']===1)
+                                            <td>随机试卷</td>
+                                        @else
+                                            <td>统一试卷</td>
+                                        @endif
+                                        <td>
+                                            <a href="javascript:void(0)">
                                         <span class="read state1 detail">
                                             <i class="fa fa-cog fa-2x"></i>
                                         </span>
-                                    </a>
-                                    <a href="javascript:void(0)">
+                                            </a>
+                                            <a href="javascript:void(0)">
                                         <span class="read state2">
                                             <i class="fa fa-trash-o fa-2x"></i>
                                         </span>
-                                    </a>
-                                </td>
-                            </tr>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
                     </table>
                     <div class="pull-left">共10条</div>
