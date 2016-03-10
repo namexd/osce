66ca89a42959e0eddd43f30a067f0275028675f9
tangjun <tangjun@misrobot.com>
2016-03-10 14:38:36 +0800

    数据补足

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -9,6 +9,7 @@
 namespace Modules\Osce\Http\Controllers\Admin\Branch;
 use Modules\Osce\Http\Controllers\CommonController;
 use Modules\Osce\Entities\QuestionBankEntities\ExamQuestionLabelType;
+use Modules\Osce\Entities\QuestionBankEntities\ExamQuestionType;
 class ApiController extends CommonController
 {
     /**
@@ -21,13 +22,19 @@ class ApiController extends CommonController
      * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
      */
     public function GetEditorExamPaperItem(){
+        //获取题目类型列表
+        $examQuestionTypeModel= new ExamQuestionType();
+        $examQuestionTypeList = $examQuestionTypeModel->examQuestionTypeList();
         //获取考核范围列表（标签类型列表）
         $examQuestionLabelTypeModel = new ExamQuestionLabelType();
         $examQuestionLabelTypeList = $examQuestionLabelTypeModel->examQuestionLabelTypeList();
         foreach($examQuestionLabelTypeList as $k=>$v){
             $examQuestionLabelTypeList[$k]['examQuestionLabelList'] = $v->examQuestionLabel;
         }
-        return  view('osce::admin.resourcemanage.subject_papers_add_detail.blade',['examQuestionLabelTypeList'=>$examQuestionLabelTypeList]);
+        return  view('osce::admin.resourcemanage.subject_papers_add_detail.blade',[
+            'examQuestionLabelTypeList'=>$examQuestionLabelTypeList,
+            'examQuestionTypeList'=>$examQuestionTypeList
+        ]);
     }
     public function PostEditorExamPaperItem(){
 
