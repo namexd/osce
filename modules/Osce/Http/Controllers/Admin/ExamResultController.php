9fc87b4c2fd8b6e2377ddbb2d612cd18b30eb5c8
zhouqiang <candyphp@163.com>
2016-03-12 16:47:51 +0800

    tupianxaizai

--- modules/Osce/Http/Controllers/Admin/ExamResultController.php
+++ modules/Osce/Http/Controllers/Admin/ExamResultController.php
@@ -282,6 +282,7 @@ class ExamResultController extends CommonController{
         $tFile=array_pop($file);
         $filename=md5($filename).'.'.$tFile;
         $filepath   =   iconv('utf-8', 'gbk', $filepath);
+
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename='.basename($filename));
