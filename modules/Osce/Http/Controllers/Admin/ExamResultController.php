5ae92716589862eb44394821ef4142c21f3ada3c
zhouqiang <candyphp@163.com>
2016-03-12 16:23:26 +0800

    tupianxiazai

--- modules/Osce/Http/Controllers/Admin/ExamResultController.php
+++ modules/Osce/Http/Controllers/Admin/ExamResultController.php
@@ -276,8 +276,8 @@ class ExamResultController extends CommonController{
         dump($attchments);
          $fileNameArray   =  explode('/',$attchments);
         dump($fileNameArray);
-        dump(array_pop($fileNameArray),public_path($fileNameArray[4]).'/'.$fileNameArray[5]);
-        $this->downloadfile(array_pop($fileNameArray),public_path($fileNameArray[4]).'/'.$fileNameArray[5]);
+        dump(array_pop($fileNameArray),public_path($fileNameArray[4]).'/'.array_pop($fileNameArray));
+        $this->downloadfile(array_pop($fileNameArray),public_path($fileNameArray[4]).'/'.array_pop($fileNameArray));
 
     }
     private function downloadfile($filename,$filepath){
