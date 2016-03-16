2d587477e35017c6e5bf364af76b5dd2aa9ff09d
jiangzhiheng <j5110021@misrobot.com>
2016-03-16 16:41:20 +0800

    完善了一点小细节

--- modules/Osce/Resources/views/admin/resourceManage/subject_manage_edit.blade.php
+++ modules/Osce/Resources/views/admin/resourceManage/subject_manage_edit.blade.php
@@ -76,6 +76,27 @@
                             message: '描述不能为空'
                         }
                     }
+                },
+                stem: {
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '题干不能为空'
+                        }
+                    }
+                },
+                goods: {
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '物品不能为空'
+                        }
+                    }
+                },
+                equipments: {
+                    validators: {
+                        notEmpty: {/*非空提示*/
+                            message: '设备不能为空'
+                        }
+                    }
                 }
             }
         });
