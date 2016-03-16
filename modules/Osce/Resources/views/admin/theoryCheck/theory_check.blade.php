3e84fa74b4ba9d8ae0a034a215f80a602ddc829b
May <wulengmei@misrobot.com>
2016-03-16 18:05:46 +0800

    选项框

--- modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
+++ modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
@@ -19,19 +19,35 @@
         .wizard > .steps > ul > li{;margin-right: 5px;border-radius: 2px;cursor: pointer;
             width: auto!important;
         }
+        .check_label{cursor: pointer}
     </style>
     <link href="{{asset('osce/admin/plugins/css/plugins/iCheck/custom.css')}}" rel="stylesheet">
-    <link href="{{asset('osce/admin/plugins/css/plugins/steps/jquery.steps.css')}}" rel="stylesheet">
+    <link href="{{asset('msc/admin/plugins/css/plugins/steps/jquery.stepschange.css')}}" rel="stylesheet">
 
 @stop
 
 @section('only_js')
-    <script src="{{asset('osce/admin/js/all_checkbox.js')}}"> </script>
-    <script src="{{asset('osce/admin/plugins/js/plugins/layer/layer.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('osce/admin/js/all_checkbox.js')}}"> </script>
     <script src="{{asset('msc/admin/plugins/js/plugins/staps/jquery.stepschange.js')}}"></script>
     <script>
         $(document).ready(function() {
             $(".wizard").steps();
+            $(".check_label").unbind().click(function(){
+                if($(this).children("input").checked=="true"){
+                    alert($(this).children("input").checked);
+                    $(this).children(".check_icon").removeClass("check");
+                }else{
+                    $(this).children(".check_icon").addClass("check");
+                }
+            });
+            $(".radio_label").click(function(){//单选按钮
+                if($(this).children("input").checked=="true"){
+                    $(this).children(".radio_icon").removeClass("check");
+                }else{
+                    $(this).parent().siblings(".answerBox").find(".radio_icon").removeClass("check");
+                    $(this).children(".radio_icon").addClass("check");
+                }
+            });
         });
     </script>
 @stop
@@ -56,49 +72,51 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-lg-12">
                 <div class="ibox float-e-margins">
-                    <div class="ibox-content text-left p-md">
-                        @if(!empty($examCategoryFormalData))
-                            @foreach(@$examCategoryFormalData as $val )
-                                <div class="bigTitle">
-                                    <span class="font20">{{@$val["name"]}}</span>
-                                    <span style="margin-left: 1em;">共<span class="subjectNum">{{@$val["number"]}}</span>题，</span>
-                                    <span>每题<span class="subjectScore">{{@$val["score"]}}</span>分</span>
-                                </div>
-                                <div class="wizard">
-                                @if(!empty($val["exam_question_formal"]))
-                                    @foreach($val["exam_question_formal"] as $k => $val2 )
-                                        <h1>{{$val2["serialNumber"]}} </h1>
+                    <div class="ibox-content text-left">
+                        <div class="p-md cBorder">
+                            <div class="wizard">
+                                @if(!empty($examCategoryFormalData))
+                                    @foreach(@$examCategoryFormalData as $val )
+                                        <h1>{{$val["serialNumber"]}} </h1>
                                         <div class="step-content">
+                                            <span class="font20">{{@$val["examCategoryFormalName"]}}</span>
+                                            <span style="margin-left: 1em;">共<span class="subjectNum">{{@$val["examCategoryFormalNumber"]}}</span>题，</span>
+                                            <span>每题<span class="subjectScore">{{@$val["examCategoryFormalScore"]}}</span>分</span>
                                             <div class="allSubject">
-                                                <div class="subjectBox">
-                                                    <span class="font20 subjectNo"></span>
-                                                    <span class="font20 marl_10 subjectContent">{{ $val2["name"]}}</span>
+                                                <div class="subjectBox   mart_10 ">
+                                                    <span class="font16 subjectContent">{{ $val["name"]}}(　　　)</span>
                                                 </div>
-                                                <div class="answerBox">
-                                                    @if(!empty($val2["content"]))
-                                                        @foreach($val2["content"] as $k=> $val3 )
+                                                @if($val["examCategoryFormalId"]===1||$val["examCategoryFormalId"]===4)
+                                                    @foreach($val["content"] as $k=> $val2 )
+                                                        <div class="answerBox">
+                                                            <label class="radio_label mart_20 check_top">
+                                                                <div class="radio_icon left" ></div>
+                                                                <input type="radio" name="{{$val["serialNumber"]}}" value="{{$k}}">
+                                                                <span class="marl_10 answer">{{@$val2}}</span>
+                                                            </label>
+                                                        </div>
+                                                    @endforeach
+                                                @endif
+                                                @if($val["examCategoryFormalId"]===2||$val["examCategoryFormalId"]===3)
+                                                    @foreach($val["content"] as $k=> $val2 )
+                                                        <div class="answerBox">
                                                             <label class="check_label checkbox_input mart_20 check_top" style="">
                                                                 <div class="check_icon check_other"></div>
-                                                                <input type="checkbox" name="nosureAnswer"  value={{$k}}>
-                                                                <span class="marl_10 answer">{{@$val3}}</span>
+                                                                <input type="checkbox" name="{{$val["serialNumber"]}}" value="{{$k}}">
+                                                                <span class="check_name">{{@$val2}}</span>
                                                             </label>
-                                                        @endforeach
-                                                    @endif
-                                                </div>
+                                                        </div>
+                                                    @endforeach
+                                                @endif
                                             </div>
                                         </div>
                                     @endforeach
                                 @endif
-                                </div>
-                            @endforeach
-                        @endif
 
-                        <div class="bigTitle">
-                            <span class="font20">单选题</span>
-                            <span style="margin-left: 1em;">共<span class="subjectNum">5</span>题，</span>
-                            <span>每题<span class="subjectScore">5</span>分</span>
+                            </div>
                         </div>
 
                         <div class="p-md cBorder mart_10" style="display:none">
@@ -125,6 +143,76 @@
                         </div>
                     </div>
 
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row table-head-style1 ">
+            <div class="col-xs-6 col-md-2">
+                <h5 class="title-label">理论考试</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins" style="margin-bottom: 0;">
+                    <div class="ibox-content text-center p-md">
+                        <h2>2016年第一期OSCE考试理论考试</h2>
+                        <span>考试时间：</span>
+                        <span class="checkTime">20分钟</span>
+                        <span style="margin-left: 1em;">总分：</span>
+                        <span class="score">100分</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content text-left p-md">
+                        <div class="bigTitle">
+                            <span class="font20">单选题</span>
+                            <span style="margin-left: 1em;">共<span class="subjectNum">5</span>题，</span>
+                            <span>每题<span class="subjectScore">5</span>分</span>
+                        </div>
+                        <div class="p-md cBorder mart_10">
+                            <div class="allSubject">
+                                <div class="subjectBox">
+                                    <span class="font20 subjectNo">1.1</span>
+                                    <span class="font20 marl_10 subjectContent">下列感染中，不具有传染性的是？</span>
+                                </div>
+                                <div class="answerBox">
+                                    <label class="check_label checkbox_input mart_20 check_top" style="">
+                                        <div class="check_icon check_other"></div>
+                                        <input type="checkbox" name="nosureAnswer"  value="A">
+                                        <span class="check_name">A</span>
+                                        <span class="marl_10 answer">隐形感染</span>
+                                    </label>
+                                    <label class="radio_label mart_20 check_top">
+                                        <div class="radio_icon left" ></div>
+                                        <input type="radio" name="oneAnswer" value="B">
+                                        <span class="radio_name">B</span>
+                                        <span class="marl_10 answer">显性感染</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="btnBox" style="margin: 70px 0 50px 0;">
+                                <button class="btn btn-primary" id="nextBtn">下一题</button>
+                                <button class="btn btn-primary" id="beforeBtn">上一题</button>
+                                <button class="btn btn-warning" id="goBtn">提交试卷</button>
+                                <span class="marl_10">剩余时间</span>
+                                <span class="font24" style="color: #ff0101;font-weight: 700;">10:10</span>
+                            </div>
+                            <div class="cBorder_b"></div>
+                            <div class="chooseBox">
+                                <div class="font16" style="padding: 20px 0;">本试卷包含以下试题</div>
+                                <div class="padb choose">
+                                    <span class="haveChoose left chooseOne">1.1</span>
+                                    <span class="nowChoose left chooseOne">1.2</span>
+                                    <span class="waitChoose left chooseOne">1.3</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
