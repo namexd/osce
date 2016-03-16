e1520f290641b320cb0a6d052132c5231228b5bc
zhouqiang <candyphp@163.com>
2016-03-16 16:47:37 +0800

    解决冲突2

---
+++ modules/Osce/Resources/views/admin/resourcemanage/equipment_manage_video.blade.php
@@ -0,0 +1,150 @@
+@extends('osce::admin.layouts.admin_index')
+
+@section('only_css')
+    <style>
+        button.btn.btn-white.dropdown-toggle {
+            border: none;
+            font-weight: bolder;
+        }
+        .blank-panel .panel-heading {margin-left: -20px;}
+        #start,#end{width: 160px;}
+        .btn{
+            margin: 0!important;
+        }
+    </style>
+@stop
+
+@section('only_js')
+
+@stop
+
+
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row table-head-style1 ">
+            <div class="col-xs-6 col-md-2">
+                <h5 class="title-label">设备管理</h5>
+            </div>
+            <div class="col-xs-6 col-md-2" style="float: right;">
+                <a  href="{{route('osce.admin.machine.getAddCameras')}}" class="btn btn-primary" style="float: right;">&nbsp;&nbsp;新增&nbsp;&nbsp;</a>
+            </div>
+        </div>
+        <div class="container-fluid ibox-content">
+                <div class="panel-heading">
+                    <div class="panel-options">
+                        <ul class="nav nav-tabs">
+                            @forelse($options as $key=>$option)
+                                <li class="{{($key==1&&(!array_key_exists('cate_id',$_GET)))||(array_key_exists('cate_id',$_GET)&&$_GET['cate_id']==$option['id'])? 'active':''}}"><a href="{{route('osce.admin.machine.getMachineList',['cate_id'=>$option['id']])}}">{{$option['name']}}</a></li>
+                            @empty
+                            @endforelse
+                        </ul>
+                    </div>
+                </div>
+                <div class="input-group" style="margin-bottom: 20px;margin-top: 10px;">
+                    <form action="{{route('osce.admin.machine.getMachineList',['cate_id'=>1])}}" method="get">
+                        <input type="hidden" name="cate_id" value="1">
+                        <input type="text" placeholder="设备名称" class="form-control" style="width: 250px;margin-right: 10px;height: 36px;" name="name" value="{{(empty($name)?'':$name)}}">
+
+                        <div class="btn-group" style="margin-right: 10px;">
+                            <button type="button" class="btn btn-default dropdown-toggle"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="">
+                                @if(array_key_exists('status',$_GET))
+                                    @forelse($machineStatuValues as $status=>$machineStatuValue)
+                                        @if($_GET['status']==$status)
+                                            {{$machineStatuValue}}
+                                            <input type="hidden" name="status" value="{{$status}}">
+                                        @endif
+                                    @empty
+                                    @endforelse
+                                @else
+                                    状态
+                                @endif
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{route('osce.admin.machine.getMachineList',['cate_id'=>1])}}">全部</a></li>
+                                @forelse($machineStatuValues as $status=>$machineStatuValue)
+                                    @if(array_key_exists('name',$_GET))
+                                        <li><a href="{{route('osce.admin.machine.getMachineList',['cate_id'=>1,'status'=>$status,'name'=>$_GET['name']])}}">{{$machineStatuValue}}</a></li>
+                                    @else
+                                        <li><a href="{{route('osce.admin.machine.getMachineList',['cate_id'=>1,'status'=>$status])}}">{{$machineStatuValue}}</a></li>
+                                    @endif
+                                @empty
+                                @endforelse
+                            </ul>
+                        </div>
+                        <button type="submit" class="btn  btn-primary" id="search">&nbsp;搜索&nbsp;</button>
+                    </form>
+                </div>
+
+                <table class="table table-striped" id="table-striped">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>设备ID</th>
+                        <th>设备名称</th>
+                        <th>状态</th>
+                        <th>操作</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($list as $key => $item)
+                        <tr>
+                            <td>{{$key+1}}</td>
+                            <td>{{$item->code}}</td>
+                            <td>{{$item->name}}</td>
+                            <td style="color:
+                                @if($item->status==1)#16beb0
+                                @elseif($item->status==2)#ed5565
+                                @elseif($item->status==3)#f8ac59
+                                @endif
+                                    ">{{$machineStatuValues[$item->status]}}
+                            </td>
+                            <td>
+                                <a href="{{route('osce.admin.machine.getEditCameras',['id'=>$item->id])}}">
+                                    <span class="read  state1 detail"><i class="fa fa-pencil-square-o fa-2x"></i></span>
+                                </a>
+                                <a href="javascript:void(0)"><span class="read state2"><i class="fa fa-trash-o fa-2x" eid="{{$item->id}}"></i></span></a>
+                            </td>
+                        </tr>
+                        @empty
+                        @endforelse
+
+                    </tbody>
+                </table>
+
+                    <div class="pull-left">
+                        共{{$list->total()}}条
+                    </div>
+                    <div class="pull-right">
+                        {!! $list->appends($_GET)->render() !!}
+                    </div>
+
+        </div>
+
+    </div>
+    <script>
+        $(function(){
+            //删除用户
+            $(".fa-trash-o").click(function(){
+                var thisElement=$(this);
+                var eid=thisElement.attr("eid");
+                layer.alert('确认删除？',{title:"删除",btn:['确认','取消']},function(){
+                    $.ajax({
+                        type:'post',
+                        async:true,
+                        url:'{{route('osce.admin.machine.postMachineDelete')}}',
+                        data:{id:eid, cate_id:1},
+                        success:function(data){
+                            if(data.code == 1){
+                                location.href='{{route("osce.admin.machine.getMachineList",["cate_id"=>1])}}'
+                            }else {
+                                layer.msg(data.message,{skin:'msg-error',icon:1});
+                            }
+                        }
+                    })
+                });
+            })
+        })
+    </script>
+@stop{{-- 内容主体区域 --}}
---
+++ modules/Osce/Resources/views/admin/resourcemanage/exam_station_edit.blade.php
@@ -0,0 +1,241 @@
+@extends('osce::admin.layouts.admin_index')
+@section('only_css')
+
+@stop
+
+@section('only_js')
+    <script>
+        $(function(){
+            $('#sourceForm').bootstrapValidator({
+                message: 'This value is not valid',
+                feedbackIcons: {/*输入框不同状态，显示图片的样式*/
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
+                },
+                fields: {/*验证*/
+                    name: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            threshold :  1 , //有6字符以上才发送ajax请求，（input中输入一个字符，插件会向服务器发送一次，设置限制，6字符以上才开始）
+                            remote: {//ajax验证。server result:{"valid",true or false} 向服务发送当前input name值，获得一个json数据。例表示正确：{"valid",true}
+                                url: '{{route('osce.admin.station.postNameUnique')}}',//验证地址
+                                message: '考站已经存在',//提示消息
+                                delay :  2000,//每输入一个字符，就发ajax请求，服务器压力还是太大，设置2秒发送一次ajax（默认输入一个字符，提交一次，服务器压力太大）
+                                type: 'POST',//请求方式
+                                /*自定义提交数据，默认值提交当前input value*/
+                                data: function(validator) {
+                                    $(".btn-primary").css({"background":"#16beb0","border":"1px solid #16beb0","color":"#fff","opacity":"1"});
+
+                                    return {
+                                        id: '{{$_GET['id']}}',
+                                        title: 'station',
+                                        name: $('[name="whateverNameAttributeInYourForm"]').val()
+                                    }
+                                }
+                            },
+                            notEmpty: {/*非空提示*/
+                                message: '名称不能为空'
+                            }
+                        }
+                    },
+                    mins: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            notEmpty: {/*非空提示*/
+                                message: '时间限制不能为空'
+                            },
+                            regexp: {
+                                regexp: /^\+?[1-9][0-9]*$/,
+                                message: '请输入正确的时间'
+                            }
+                        }
+                    },
+                    subject_id: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            notEmpty: {/*非空提示*/
+                                message: '请选择科目'
+                            }
+                        }
+                    },
+                    case_id: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            notEmpty: {/*非空提示*/
+                                message: '请选择病例'
+                            }
+                        }
+                    },
+                    room_id: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            notEmpty: {/*非空提示*/
+                                message: '请选择所属考场'
+                            }
+                        }
+                    },
+                    vcr_id: {
+                        /*键名username和input name值对应*/
+                        message: 'The username is not valid',
+                        validators: {
+                            notEmpty: {/*非空提示*/
+                                message: '请选择关联摄像机'
+                            }
+                        }
+                    }
+                }
+            });
+        })
+    </script>
+@stop
+
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>编辑考站</h5>
+            </div>
+            <div class="ibox-content">
+                <div class="row">
+
+                    <div class="col-md-12 ">
+                        <form method="post" class="form-horizontal" id="sourceForm" action="{{route('osce.admin.Station.postEditStation')}}">
+                            <input type="hidden" name="id" value="{{$rollmsg['id']}}">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">考站名称</label>
+
+                                <div class="col-sm-10">
+                                    <input type="text" required class="form-control" id="name" name="name" value="{{$rollmsg['name']}}">
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">考站类型</label>
+                                <div class="col-sm-10">
+                                    <select id="type" required  class="form-control" name="type" >
+                                        @foreach($placeCate as $key=>$item)
+                                            <option value="{{$key}}"
+                                                @if($rollmsg['type'] == $key)
+                                                    selected="selected"
+                                                @endif>{{$item}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">时间限制(分钟)</label>
+                                <div class="col-sm-10">
+                                    <input type="text"  required  ng-model="num" id="code" class="form-control" name="mins" value="{{$rollmsg['mins']}}" placeholder="请输入分钟数">
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+
+                            <div class="form-group noTheory" {!! $rollmsg['type']==3? 'style="display:none;"':'' !!}>
+                                <label class="col-sm-2 control-label">科目</label>
+                                <div class="col-sm-10">
+                                    <select id="" required  class="form-control" name="subject_id">
+                                        <option value="">请选择</option>
+                                        @foreach($subject as $key=>$item)
+                                            <option value="{{$item['id']}}"
+                                                    @if($rollmsg['subject_id'] == $item['id'])
+                                                    selected="selected"
+                                                    @endif
+                                            >{{$item['title']}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed" {!! $rollmsg['type']==3? 'style="display:none;"':'' !!}></div>
+
+                            <div class="form-group noTheory" {!! $rollmsg['type']==3? 'style="display:none;"':'' !!}>
+                                <label class="col-sm-2 control-label">病例</label>
+                                <div class="col-sm-10">
+                                    <select id="" required  class="form-control" name="case_id">
+                                        <option value="">请选择</option>
+                                        @foreach($case as $key=>$item)
+                                            <option value="{{$item['id']}}"
+                                                @if($rollmsg['case_id'] == $item['id'])
+                                                    selected="selected"
+                                                @endif
+                                            >{{$item['name']}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed noTheory" {!! $rollmsg['type']==3? 'style="display:none;"':'' !!}></div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" required>所属考场</label>
+                                <div class="col-sm-10">
+                                    <select id="" required  class="form-control" name="room_id">
+                                        <option value="">请选择</option>
+                                        @foreach($room as $key=>$item)
+                                            <option value="{{$item['id']}}"
+                                                    @if($rollmsg['room_id'] == $item['id'])
+                                                    selected="selected"
+                                                    @endif
+                                            >{{$item['name']}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed noTheory"></div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">关联摄像机</label>
+                                <div class="col-sm-10">
+                                    <select id="" required  class="form-control" name="vcr_id">
+                                        <option value="">请选择</option>
+                                        @foreach($vcr as $key=>$item)
+                                            <option value="{{$item['id']}}"
+                                                    @if($rollmsg['vcr_id'] == $item['id'])
+                                                    selected="selected"
+                                                    @endif
+                                            >{{$item['name']}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+
+                            {{--<div class="form-group">--}}
+                                {{--<label class="col-sm-2 control-label">考站编号</label>--}}
+                                {{--<div class="col-sm-10">--}}
+                                    {{--<input type="text" required class="form-control" id="code" name="code" value="{{$rollmsg['code']}}">--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="hr-line-dashed"></div>--}}
+                            {{--<div class="form-group">--}}
+                                {{--<label class="col-sm-2 control-label">考站描述</label>--}}
+                                {{--<div class="col-sm-10">--}}
+                                    {{--<input type="text" required class="form-control" id="description" name="description" value="{{$rollmsg['description']}}">--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="hr-line-dashed"></div>--}}
+
+                            <div class="form-group">
+                                <div class="col-sm-4 col-sm-offset-2">
+                                    <button class="btn btn-primary" type="submit" style="display: {{($status?'none':'')}}">保存</button>
+                                    <a class="btn btn-white" href="{{route("osce.admin.Station.getStationList")}}">取消</a>
+{{--                                    <a type="button" class="btn btn-white" href="{{route('osce.admin.Station.getStationList')}}" >取消</a>--}}
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop{{-- 内容主体区域 --}}
