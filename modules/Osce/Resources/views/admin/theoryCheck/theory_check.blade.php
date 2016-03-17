6e7c30258b3165e285837ee22966d9efd56f9bf3
May <wulengmei@misrobot.com>
2016-03-17 11:38:34 +0800

    提交成绩

--- modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
+++ modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
@@ -22,25 +22,41 @@
         .check_label{cursor: pointer}
     </style>
     <link href="{{asset('osce/admin/plugins/css/plugins/iCheck/custom.css')}}" rel="stylesheet">
-    <link href="{{asset('msc/admin/plugins/css/plugins/steps/jquery.stepschange.css')}}" rel="stylesheet">
+    <link href="{{asset('osce/admin/plugins/css/plugins/steps/jquery.stepschange.css')}}" rel="stylesheet">
 
 @stop
 
 @section('only_js')
     <script type="text/javascript" src="{{asset('osce/admin/js/all_checkbox.js')}}"> </script>
-    <script src="{{asset('msc/admin/plugins/js/plugins/staps/jquery.stepschange.js')}}"></script>
+    <script src="{{asset('osce/admin/plugins/js/plugins/staps/jquery.stepschange.js')}}"></script>
     <script>
         $(document).ready(function() {
             $(".wizard").steps();
-            $(".check_label").unbind().click(function(){
+            $(".check_label").change(function(){
+                var examCategoryFormalId= $(this).parent().attr("examCategoryFormalId");//判断题型
+                var exam_question_id= $(this).parent().parent().find(".subjectBox").attr("exam_question_id");//获取题号ID
+                var answer="";//答案
                 if($(this).children("input").checked=="true"){
                     alert($(this).children("input").checked);
                     $(this).children(".check_icon").removeClass("check");
                 }else{
                     $(this).children(".check_icon").addClass("check");
                 }
+                $(this).parent().parent().find(".check").each(function(index, element){//查找被选中的项
+                    if(index==0){
+                        answer=$(element).next("input").val();
+                    }else{
+                        answer=answer+"@"+$(element).next("input").val();
+                    }
+                })
+                Set_answer(examCategoryFormalId,exam_question_id,answer);//保存成绩
             });
-            $(".radio_label").click(function(){//单选按钮
+
+            $(".radio_label").change(function(){//单选按钮
+                var examCategoryFormalId= $(this).parent().attr("examCategoryFormalId");//判断题型
+                var exam_question_id= $(this).parent().parent().find(".subjectBox").attr("exam_question_id");//获取题号ID
+                var answer = $(this).children("input").val();
+                Set_answer(examCategoryFormalId,exam_question_id,answer);//保存成绩
                 if($(this).children("input").checked=="true"){
                     $(this).children(".radio_icon").removeClass("check");
                 }else{
@@ -48,6 +64,32 @@
                     $(this).children(".radio_icon").addClass("check");
                 }
             });
+            function Set_answer(examCategoryFormalId,exam_question_id,answer){
+                var Storage_answer  = {};//单个题目成绩
+                var Storage_answer_list=[]; //页面储存成绩
+                Storage_answer.examCategoryFormalId=examCategoryFormalId;
+                Storage_answer.exam_question_id = exam_question_id;
+                Storage_answer.answer = answer;
+                var now_answer_list=localStorage.getItem("Storage_answer");
+                if(now_answer_list!=null)
+                {
+                    Storage_answer_list=JSON.parse(now_answer_list);
+                    for(i=0;i<Storage_answer_list.length;i++){
+                        if(Storage_answer_list[i].exam_question_id == exam_question_id){
+                            Storage_answer_list.splice(i,1);
+                        };
+                    }
+                }
+                Storage_answer_list.push(Storage_answer);
+                localStorage.setItem("Storage_answer",JSON.stringify(Storage_answer_list));//设置本地存储
+            }
+            $(".actions").find("a[href='#finish']").click(function(){
+
+                $.post("{{route('osce.admin.AnswerController.postSaveAnswe')}}",localStorage.getItem("Storage_answer"),function(obj){
+                    console.log(obj);
+                    //window.location="{{route("osce.admin.AnswerController.selectGrade")}}";
+                })
+            })
         });
     </script>
 @stop
@@ -86,12 +128,12 @@
                                             <span style="margin-left: 1em;">共<span class="subjectNum">{{@$val["examCategoryFormalNumber"]}}</span>题，</span>
                                             <span>每题<span class="subjectScore">{{@$val["examCategoryFormalScore"]}}</span>分</span>
                                             <div class="allSubject">
-                                                <div class="subjectBox   mart_10 ">
+                                                <div class="subjectBox   mart_10 " exam_question_id="{{$val["exam_question_id"]}}">
                                                     <span class="font16 subjectContent">{{ $val["name"]}}(　　　)</span>
                                                 </div>
                                                 @if($val["examCategoryFormalId"]===1||$val["examCategoryFormalId"]===4)
                                                     @foreach($val["content"] as $k=> $val2 )
-                                                        <div class="answerBox">
+                                                        <div class="answerBox" examCategoryFormalId="{{$val["examCategoryFormalId"]}}">
                                                             <label class="radio_label mart_20 check_top">
                                                                 <div class="radio_icon left" ></div>
                                                                 <input type="radio" name="{{$val["serialNumber"]}}" value="{{$k}}">
@@ -102,7 +144,7 @@
                                                 @endif
                                                 @if($val["examCategoryFormalId"]===2||$val["examCategoryFormalId"]===3)
                                                     @foreach($val["content"] as $k=> $val2 )
-                                                        <div class="answerBox">
+                                                        <div class="answerBox" examCategoryFormalId="{{$val["examCategoryFormalId"]}}">
                                                             <label class="check_label checkbox_input mart_20 check_top" style="">
                                                                 <div class="check_icon check_other"></div>
                                                                 <input type="checkbox" name="{{$val["serialNumber"]}}" value="{{$k}}">
