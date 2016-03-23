1b7bc86cadc69615d2863e001672f992ebb1f7ad
xumin <xumin@misrobot.com>
2016-03-23 15:23:21 +0800

    理论考试

--- modules/Osce/Resources/views/admin/theoryCheck/theory_check_volidate.blade.php
+++ modules/Osce/Resources/views/admin/theoryCheck/theory_check_volidate.blade.php
@@ -24,57 +24,6 @@
                 <h5 class="title-label">理论考试</h5>
             </div>
         </div>
-        {{--学生信息展示--}}
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox float-e-margins" style="margin-bottom: 0;">
-                    <div class="ibox-content text-center p-md">
-                        <h2>2016年第一期OSCE考试理论考试</h2>
-                        <span>考试时间：</span>
-                        <span class="checkTime">20分钟</span>
-                        <span class="marl_10">总分：</span>
-                        <span class="score">100分</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-content text-left">
-                        <div class="font20 padt_20 goCenter">当前考生</div>
-                        <div class="p-md cBorder mart_10 overflow goCenter">
-                            <div class="col-sm-6">
-                                <div style="padding: 40px 60px;">
-                                    <img src="" alt="" class="imgBorder myImg" style="height: 250px;width: 196px;">
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div style="padding: 40px 60px;">
-                                    <div class="nameBox font20 marb_25">
-                                        <span>考生姓名：</span>
-                                        <span class="stuName">江缤</span>
-                                    </div>
-                                    <div class="stuBox font20 marb_25">
-                                        <span>考生学号：</span>
-                                        <span class="stuNum">201013031401</span>
-                                    </div>
-                                    <div class="idBox font20 marb_25">
-                                        <span>身份证号：</span>
-                                        <span class="idNum">201013031401</span>
-                                    </div>
-                                    <div class="admissionBox font20 marb_25">
-                                        <span>准考证号：</span>
-                                        <span class="admissionNum">201013031401</span>
-                                    </div>
-                                    <div class="btnBox">
-                                        <button class="btn btn-primary btn-lg font16" style="padding: 9px 40px;">进入考试</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         {{--等待学生信息--}}
         <div class="row">
             <div class="col-lg-12">
