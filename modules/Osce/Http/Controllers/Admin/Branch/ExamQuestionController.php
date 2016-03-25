432687f83ce0acc553f61ad29880109990ee40b9
xumin <xumin@misrobot.com>
2016-03-25 14:22:45 +0800

    试题bug修改

--- modules/Osce/Http/Controllers/Admin/Branch/ExamQuestionController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ExamQuestionController.php
@@ -125,12 +125,12 @@ class ExamQuestionController extends CommonController
 
         $this->validate($request, [
             'examQuestionTypeId'    =>'sometimes|integer',//试题表
-            'name'                     => 'required|string',
+            'name'                     => 'sometimes|string',
             'parsing'                 => 'sometimes|string',
             'answer'                  => 'sometimes|array',
             'judge'                  => 'sometimes|integer',
 
-            'examQuestionItemName'  => 'required|array',//试题子项表
+            'examQuestionItemName'  => 'sometimes|array',//试题子项表
             'content'                 => 'sometimes|array',
 
             'examQuestionLabelId'      =>'sometimes|array',//试题和标签中间表
@@ -268,13 +268,13 @@ class ExamQuestionController extends CommonController
     public function postExamQuestionEdit(Request $request)
     {
         $this->validate($request, [
-            'id'                      =>'required|integer',//试题表
+            'id'                      =>'sometimes|integer',//试题表
             'examQuestionTypeId'    =>'sometimes|integer',
             'parsing'                 => 'sometimes|string',
             'answer'                  => 'sometimes|array',
             'judge'                  => 'sometimes|integer',
 
-            'examQuestionItemName'  => 'required|array',//试题子项表
+            'examQuestionItemName'  => 'sometimes|array',//试题子项表
             'content'                 => 'sometimes|array',
 
             'examQuestionLabelId'      =>'sometimes|array',//试题和标签中间表
