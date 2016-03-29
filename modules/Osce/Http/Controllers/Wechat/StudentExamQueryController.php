6c54b1f7ba8edc4800f7f1106368a09678978949
zhouqiang <candyphp@163.com>
2016-03-29 20:19:36 +0800

    修改微信成绩查询

--- modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
+++ modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
@@ -60,7 +60,7 @@ class StudentExamQueryController extends CommonController
             }
 
             //根据用户获得考试id
-            $studentId = Student::where('user_id', '=', $user->id)->get()->pulck('id');
+            $studentId = Student::where('user_id', '=', $user->id)->get()->pluck('id');
             $ExamIdList = ExamQueue::whereIn('student_id', '=', $studentId)->select('exam_id')->get();
             if(!$ExamIdList){
                 throw new \Exception('目前你还没有参加过考试。');
