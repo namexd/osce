6d18a20d7b0eb77f9ebc5457dbcdd573934f3b2a
zhouqiang <candyphp@163.com>
2016-03-29 20:36:18 +0800

    修改身份验证

--- modules/Osce/Http/Controllers/Api/InvigilatePadController.php
+++ modules/Osce/Http/Controllers/Api/InvigilatePadController.php
@@ -172,8 +172,10 @@ class InvigilatePadController extends CommonController
         $stationId = (int)$request->input('station_id');
         $studentModel = new  Student();
         $studentData = $studentModel->studentList($stationId);
+        if(!$studentData->avator){
+            $studentData->avator = '';
+        }
         $studentData->avator = asset($studentData->avator);
-        
         if ($studentData) {
             return response()->json(
                 $this->success_data($studentData, 1, '验证完成')
--- modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
+++ modules/Osce/Http/Controllers/Wechat/StudentExamQueryController.php
@@ -61,7 +61,6 @@ class StudentExamQueryController extends CommonController
 
             //根据用户获得考试id
             $studentId = Student::where('user_id', '=', $user->id)->get()->pluck('id');
-            dd($studentId);
             $ExamIdList = ExamQueue::whereIn('student_id', '=', $studentId)->select('exam_id')->get();
             if(!$ExamIdList){
                 throw new \Exception('目前你还没有参加过考试。');
