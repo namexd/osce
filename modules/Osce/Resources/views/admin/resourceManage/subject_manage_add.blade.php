6759f96a12e9b2465fa3947cfdc8306928ce27f6
xumin <xumin@misrobot.com>
2016-03-30 13:57:08 +0800

    图片上传

--- modules/Osce/Resources/views/admin/resourceManage/subject_manage_add.blade.php
+++ modules/Osce/Resources/views/admin/resourceManage/subject_manage_add.blade.php
@@ -43,7 +43,7 @@
                 var kb=Math.floor(files[0].size/1024);
                 if(kb>2048){
                     layer.alert('图片大小不得超过2M!');
-                    $("#file").val('');
+                    $("#picFile").val('');
                     return false;
                 }
                 $.ajaxFileUpload
@@ -124,7 +124,7 @@
                                 <div class="col-sm-10">
                                     <a href="javascript:void(0)" class="btn btn-outline btn-default" id="file" title="请选择图片">
                                         选择图片
-                                        <input type="file" multiple="multiple" id="picFile" name="image">
+                                        <input type="file" multiple="multiple" id="picFile">
                                     </a>
                                     <div class="picBox">
 
