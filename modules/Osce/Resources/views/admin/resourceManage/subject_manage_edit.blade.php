f96070473becdf0ec21a9114f08293261d5b9aa9
zengjie <zengjie@misrobot.com>
2016-03-30 10:55:56 +0800

    题库管理新增和编辑添加图片提交

--- modules/Osce/Resources/views/admin/resourceManage/subject_manage_edit.blade.php
+++ modules/Osce/Resources/views/admin/resourceManage/subject_manage_edit.blade.php
@@ -23,6 +23,9 @@
     .check_top {top: 8px;margin-right: 10px;}
     /*select2样式*/
     .select2-container--default .select2-selection--multiple{border: 1px solid #e5e6e7;border-radius:2px;}
+    /*图片上传*/
+    #file {position: relative;overflow: hidden;}
+    #file input{position: absolute;right: 0;top: 0;font-size: 100px;}
 </style>
 @stop
 
@@ -64,6 +67,19 @@
                                 <label class="col-sm-2 control-label"><span class="dot" style="color: #ed5565;">*</span>题目</label>
                                 <div class="col-sm-10">
                                     <textarea name="name" id="subjectName" cols="10" rows="5" class="form-control">{{ $data['name'] }}</textarea>
+                                </div>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">图片</label>
+                                <div class="col-sm-10">
+                                    <a href="javascript:void(0)" class="btn btn-outline btn-default" id="file" title="请选择图片">
+                                        选择图片
+                                        <input type="file" multiple="multiple" id="picFile">
+                                    </a>
+                                    <div class="picBox">
+                                        haha
+                                    </div>
                                 </div>
                             </div>
                             @if(@$data['exam_question_type_id'] != 4)
