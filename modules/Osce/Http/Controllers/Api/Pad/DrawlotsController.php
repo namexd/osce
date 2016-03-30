2a5f58bd56409b445879dbb41f64ae01528f1a49
jiangzhiheng <j5110021@misrobot.com>
2016-03-30 17:27:46 +0800

    测试提交

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -92,7 +92,7 @@ class DrawlotsController extends CommonController
             'exam_id' => 'sometimes|integer'
         ]);
 
-        try {
+//        try {
             //首先得到登陆者id
             $id = $request->input('id');
             $examId = $request->input('exam_id', null);
@@ -147,9 +147,9 @@ class DrawlotsController extends CommonController
             }
 
             return response()->json($this->success_data($students));
-        } catch (\Exception $ex) {
-            return response()->json($this->fail($ex));
-        }
+//        } catch (\Exception $ex) {
+//            return response()->json($this->fail($ex));
+//        }
     }
 
     /**
