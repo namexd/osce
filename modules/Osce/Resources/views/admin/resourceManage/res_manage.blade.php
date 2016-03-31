955851dc02f436a307c69a32afbc7be2c8d0218e
zhouchong <zhouchong@misrobot.com>
2016-03-31 10:52:10 +0800

    用物

---
+++ modules/Osce/Resources/views/admin/resourceManage/res_manage.blade.php
@@ -0,0 +1,79 @@
+@extends('osce::admin.layouts.admin_index')
+
+@section('only_css')
+    <style>
+        button.btn.btn-white.dropdown-toggle {
+            border: none;
+            font-weight: bolder;
+        }
+        .blank-panel .panel-heading {margin-left: -20px;}
+        #start,#end{width: 160px;}
+        .input-group input{height: 34px;}
+        .ibox-content{padding: 20px;}
+    </style>
+@stop
+
+@section('only_js')
+<script src="{{asset('osce/admin/resourceManage/resource_manage.js')}}" ></script>
+@stop
+
+@section('content')
+    <input type="hidden" id="parameter" value="{'pagename':'res_manage','del':'{{route('osce.admin.topic.getDelTopic')}}'}" />
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row table-head-style1 ">
+            <div class="col-xs-6 col-md-2">
+                <h5 class="title-label">用物管理</h5>
+            </div>
+            <div class="col-xs-6 col-md-2" style="float: right;">
+                 <a  href="" class="btn btn-primary" style="float: right;">&nbsp;&nbsp;新增&nbsp;&nbsp;</a>
+             </div>
+        </div>
+        <div class="container-fluid ibox-content" id="list_form">
+            <ul class="nav nav-tabs teacher-tabs">
+                <li role="presentation"><a href="{{route('osce.admin.topic.getList')}}">考试项目</a></li>
+                <li role="presentation"><a href="{{route('osce.admin.case.getCaseList')}}">病例</a></li>
+                <li role="presentation" class="active"><a href="#">用物</a></li>
+            </ul>
+            <div class="panel blank-panel">
+                <form method="get" action="">
+                    <div class="input-group" style="width: 290px;margin:20px 0;">
+                         <input type="text" name="name" placeholder="请输入用物名称" class="input-sm form-control" value="">
+                         <span class="input-group-btn">
+                               <button type="submit" class="btn btn-sm btn-primary" id="search">搜索</button>
+                         </span>
+                    </div>
+                </form>
+
+                <table class="table table-striped" id="table-striped">
+                    <thead>
+                         <tr>
+                              <th>用物名称</th>
+                              <th>操作</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>温度计</td>
+                            <td>
+                                <a href=""><span class="read  state1 detail"><i class="fa fa-pencil-square-o fa-2x"></i></span></a>
+                                <a href="javascript:void(0)"><span class="read  state2"><i class="fa fa-trash-o fa-2x"></i></span></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <br/>
+                <div class="pull-left">
+                     共3条
+                </div>
+                <div class="pull-right">
+                    <nav>
+                         <ul class="pagination">
+
+                         </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop{{-- 内容主体区域 --}}
---
+++ modules/Osce/Resources/views/admin/resourceManage/res_manage_add.blade.php
@@ -0,0 +1,48 @@
+@extends('osce::admin.layouts.admin_index')
+@section('only_css')
+
+@stop
+
+@section('only_js')
+    <script src="{{asset('osce/admin/resourceManage/resource_manage.js')}}" ></script>
+@stop
+
+@section('content')
+<input type="hidden" id="parameter" value="{'pagename':'res_manage_add','name':''}" />
+<div class="wrapper wrapper-content animated fadeInRight">
+
+    <div class="ibox float-e-margins">
+        <div class="ibox-title">
+            <h5>新增用物</h5>
+        </div>
+        <div class="ibox-content">
+            <div class="row">
+
+                <div class="col-md-12 ">
+                    <form method="post" class="form-horizontal" id="sourceForm">
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">用物名称</label>
+                            <div class="col-sm-10">
+                                <input type="text" required class="form-control" id="name" name="name" value="">
+                            </div>
+                        </div>
+                        <div class="hr-line-dashed"></div>
+
+                        <div class="form-group">
+                            <div class="col-sm-4 col-sm-offset-2">
+                                <button class="btn btn-primary" type="submit">保存</button>
+                                <a class="btn btn-white" href="">取消</a>
+                                {{--<a href="{{route('osce.admin.case.getCaseList')}}" class="btn btn-white">取消</a>--}}
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@stop{{-- 内容主体区域 --}}
