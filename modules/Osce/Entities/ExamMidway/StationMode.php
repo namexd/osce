1d5729d76b5f7e3e3d07cce498526d5fe49daec2
jiangzhiheng <j5110021@misrobot.com>
2016-03-31 19:34:44 +0800

    commit

--- modules/Osce/Entities/ExamMidway/StationMode.php
+++ modules/Osce/Entities/ExamMidway/StationMode.php
@@ -94,7 +94,6 @@ class StationMode implements ModeInterface
                 ->groupBy('student.id')
                 ->take(1)
                 ->get();
-            dd($collection);
 
             if ($collection->isEmpty()) {
                 $query = ExamQueue::leftJoin('student', 'student.id', '=', 'exam_queue.student_id')
@@ -117,6 +116,7 @@ class StationMode implements ModeInterface
                     ->groupBy('student.id')
                     ->take(1)
                     ->get();
+                dd($query);
                 //实现首位固定
                 foreach ($query as $student) {
                     $stick = ExamQueue::where('exam_id', $this->exam->id)
