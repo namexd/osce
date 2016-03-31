9d46caf4c3604cf11d7e818fe26a88be56d3ef57
zhouqiang <candyphp@163.com>
2016-03-31 16:49:18 +0800

    提交用物

---
+++ modules/Osce/Entities/Supplies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/31 0031
+ * Time: 15:37
+ */
+namespace Modules\Osce\Entities;
+
+use DB;
+use Auth;
+use Modules\Osce\Repositories\Common;
+use Modules\Osce\Entities\CommonModel;
+
+class Supplies extends CommonModel
+{
+    protected $connection = 'osce_mis';
+    protected $table = 'supplies';
+    public $timestamps = true;
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $guarded = [];
+    protected $hidden = [];
+    protected $fillable = [
+        'id',
+        'name',
+        'create_user_id',
+    ];
+    public $search = [];
+    
+    
+    //获取用物列表
+    public  function getList($name){
+        if (!is_null($name)) {
+            return $this->where('name', 'like', '%' . $name . '%')->paginate(config('osce.page_size'));
+        } else {
+            return $this->paginate(config('osce.page_size'));
+        }
+        
+        
+    }
+
+
+    
+}
---
+++ modules/Osce/Http/Controllers/Admin/SuppliesController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/31 0031
+ * Time: 15:47
+ */
+
+namespace Modules\Osce\Http\Controllers\Admin;
+
+
+use Illuminate\Http\Request;
+use Modules\Osce\Entities\Supplies;
+use Modules\Osce\Http\Controllers\CommonController;
+class SuppliesController extends CommonController
+{
+    //获取用物列表
+    public  function getList(Request $request)
+    {
+
+
+        $name = e($request->get('name'));
+
+        $suppliesModel = new Supplies();
+
+        $list = $suppliesModel->getList($name);
+
+        return view('osce::admin.resourceManage.res_manage', ['list' => $list, 'name' => $name]);
+    }
+
+    //新增用物着陆页面
+    public function getAddSupplies(){
+        return view('osce::admin.resourceManage.res_manage_add');
+    }
+
+    //提交新增用物
+    public function postAddSupplies(Request $request){
+        $this->validate($request,[
+            'name'=>'required'
+        ]);
+
+        $name = $request->get('name');
+        //添加进数据库
+        $data=[
+            'name'=>$name,
+        ];
+
+        if(!Supplies::create($data)){
+            throw new \Exception('添加用物失败');
+        }
+    }
+
+
+    //编辑用物着陆页面
+    
+
+    //提交编辑用物表单
+
+}
