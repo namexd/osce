22e37a05130c981d9b288dc3ca7114361e7e33e0
wangjiang <wangjiang@misrobot.com>
2016-04-01 11:42:27 +0800

    修改考试监控控制器

--- modules/Osce/Http/Controllers/Admin/Branch/ExamMonitorController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ExamMonitorController.php
@@ -35,7 +35,112 @@ class ExamMonitorController  extends CommonController
      * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
      */
     public function getExamMonitorNormalList () {
-        
+
+    }
+
+    /**
+     * 获得迟到的考试监控列表
+     * @method GET
+     * @url /osce/admin/exam-monitor/late
+     * @access public
+     *
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return view
+     *
+     * @version 3.3a
+     * @author wangjiang <wangjiang@misrobot.com>
+     * @date 2016-04-01 11:38
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function getExamMonitorLateList () {
+
+    }
+
+    /**
+     * 获得替考的考试监控列表
+     * @method GET
+     * @url /osce/admin/exam-monitor/replace
+     * @access public
+     *
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return view
+     *
+     * @version 3.3a
+     * @author wangjiang <wangjiang@misrobot.com>
+     * @date 2016-04-01 11:39
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function getExamMonitorReplaceList () {
+
+    }
+
+    /**
+     * 获得弃考的考试监控列表
+     * @method GET
+     * @url /osce/admin/exam-monitor/quit
+     * @access public
+     *
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return view
+     *
+     * @version 3.3a
+     * @author wangjiang <wangjiang@misrobot.com>
+     * @date 2016-04-01 11:40
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function getExamMonitorQuitList () {
+
+    }
+
+    /**
+     * 获得已完成的考试监控列表
+     * @method GET
+     * @url /osce/admin/exam-monitor/finish
+     * @access public
+     *
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return view
+     *
+     * @version 3.3a
+     * @author wangjiang <wangjiang@misrobot.com>
+     * @date 2016-04-01 11:40
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    public function getExamMonitorFinishList () {
+
+    }
+
+    /**
+     * 获得考试监控表头信息
+     * @method GET
+     * @url /osce/admin/
+     * @access public
+     *
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return array
+     *
+     * @version 3.3a
+     * @author wangjiang <wangjiang@misrobot.com>
+     * @date 2016-04-01 11:41
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    protected function getExamMonitorHeadInfo () {
+
     }
 }
 
