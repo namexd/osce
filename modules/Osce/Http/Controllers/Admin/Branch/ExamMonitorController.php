f69f018c843a2214cad8b53414b6c53f2e695cc4
wangtao <wangtao@misrobot.com>
2016-04-05 17:33:37 +0800

    监控修改

--- modules/Osce/Http/Controllers/Admin/Branch/ExamMonitorController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ExamMonitorController.php
@@ -14,6 +14,7 @@ use Modules\Osce\Http\Controllers\CommonController;
 use Modules\Osce\Entities\Exam;
 use Modules\Osce\Repositories\TestScoreRepositories;
 use Modules\Osce\Repositories\SubjectStatisticsRepositories;
+use Modules\Osce\Entities\ExamScreeningStudent;
 
 class ExamMonitorController  extends CommonController
 {
@@ -141,6 +142,50 @@ class ExamMonitorController  extends CommonController
      */
     protected function getExamMonitorHeadInfo () {
 
+    }
+
+    /**
+     * 根据状态获得考试监控列表
+     * @method GET
+     * @url /osce/admin/
+     * @access public
+     * @param $status 1迟到 2替考 3弃考 4已完成
+     * @param Request $request get请求<br><br>
+     * <b>get请求字段：</b>
+     * * string        参数英文名        参数中文名(必须的)
+     *
+     * @return array
+     *
+     * @version 3.3a
+     * @author wt <wangtao@misrobot.com>
+     * @date 2016-04-05
+     * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
+     */
+    protected function getExamMonitorListByStatus($status){
+        /*$builder=ExamScreeningStudent:: leftJoin('student', function($join){
+            $join -> on('standard.id', '=', 'exam_score.standard_id');
+        })-> leftJoin('station', function($join){
+            $join -> on('station.subject_id', '=', 'exam_score.subject_id');
+        })-> leftJoin('exam_result', function($join){
+            $join -> on('station.id', '=', 'exam_result.station_id');
+        });*/
+            switch ($status){
+                case 1://迟到
+                   // ExamScreeningStudent::
+                    break;
+                case 2://替考
+
+                    break;
+                case 3://弃考
+
+                    break;
+                case 4://已完成
+
+                    break;
+                default:
+                    return [];
+                    break;
+            }
     }
 }
 
