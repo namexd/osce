0cc70f7f47c9479b7efef84c6074100ce90511b5
jiangzhiheng <j5110021@misrobot.com>
2016-04-05 19:31:35 +0800

    测试提交

--- modules/Osce/Entities/ExamMidway/RoomMode.php
+++ modules/Osce/Entities/ExamMidway/RoomMode.php
@@ -65,7 +65,7 @@ class RoomMode implements ModeInterface
         try {
             //获取首位固定
             $sticks = ExamQueue::where('exam_id', $this->exam->id)->where('room_id', $this->room->room_id)->where('stick', $this->room->room_id)->get();
-            
+
             if (count($sticks) < $this->_T_Count) {
                 $collection = ExamQueue::leftJoin('student', 'student.id', '=', 'exam_queue.student_id')
                     ->whereIn('exam_queue.room_id', $this->room->id)
@@ -126,11 +126,12 @@ class RoomMode implements ModeInterface
                             //将item的考场信息修改
                             $item->room_id = $this->room->room_id;
                             $item->save();
+                            dd($array);
                             $array->push($item);
                         }
                     }
                 }
-                dd($array);
+
                 //实现首位固定
                 foreach ($array as $student) {
 //                    $stick = ExamQueue::where('exam_id', $this->exam->id)
