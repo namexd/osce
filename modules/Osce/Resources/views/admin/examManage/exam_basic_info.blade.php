d4aaf546228533faaa1ba2443e074b20bf518527
zhouchong <zhouchong@misrobot.com>
2016-04-05 17:48:39 +0800

    考官安排

--- modules/Osce/Resources/views/admin/examManage/exam_basic_info.blade.php
+++ modules/Osce/Resources/views/admin/examManage/exam_basic_info.blade.php
@@ -52,6 +52,7 @@
                     <ul class="nav nav-tabs">
                         <li class="active"><a href="{{route('osce.admin.exam.getEditExam',['id'=>$id])}}">基础信息</a></li>
                         <li class=""><a href="{{route('osce.admin.exam.getChooseExamArrange',['id'=>$id])}}">考场安排</a></li>
+                        <li class=""><a href="">考官安排</a></li>
                         <li class=""><a href="{{route('osce.admin.exam.getExamineeManage',['id'=>$id])}}">考生管理</a></li>
                         <li class=""><a href="{{route('osce.admin.exam.getIntelligence',['id'=>$id])}}">智能排考</a></li>
                         <li class=""><a href="{{route('osce.admin.exam.getExamRemind',['id'=>$id])}}">待考区说明</a></li>
