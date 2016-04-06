f4a5199ae4a50b44de4bb676952e6a2951fd4f37
zhouqiang <candyphp@163.com>
2016-04-06 12:28:53 +0800

    考试安排新增考站

--- modules/Osce/Http/Controllers/Admin/ExamArrangeController.php
+++ modules/Osce/Http/Controllers/Admin/ExamArrangeController.php
@@ -9,12 +9,21 @@
 namespace Modules\Osce\Http\Controllers\Admin;
 
 
+use Illuminate\Http\Request;
+use Modules\Osce\Entities\Subject;
 use Modules\Osce\Http\Controllers\CommonController;
 
 class ExamArrangeController extends CommonController
 {
     //考试安排着陆页
-    
+    //获取考场接口
+    //获取考站接口
+    public function getStationData(){
+        //查询出该站是否已选择过该考站
+        //如果有就不再显示该考站数据
+        //如果没有就显示所有考站数据
+        
+    }
 
 
 
