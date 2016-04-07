8c5a97474610fc5d2aea2005943d44274d68ed4b
zhoufuxiang <zhoufuxiang@163.com>
2016-04-07 18:36:13 +0800

    考场安排

--- modules/Osce/Entities/ExamDraftFlow.php
+++ modules/Osce/Entities/ExamDraftFlow.php
@@ -22,4 +22,53 @@ class ExamDraftFlow extends CommonModel
 
 
 
+    public function handleBigData($data){
+        switch ($data['ctrl_type']){
+            case 1 : $this->bigOne($data);
+                break;
+            case 2 : $this->bigTwo($data);
+                break;
+            case 3 : $this->bigTwo($data);                //删
+                break;
+            case 4 :
+                break;
+            case 5 :
+                break;
+            default: throw new \Exception('操作有误！');
+        }
+    }
+
+    public function bigOne($data){
+        $item   =$data['item'];
+        $draftFlowData = [
+            'order'             => $item['order'],
+            'name'              => $item['name'],
+            'exam_screening_id' => $item['exam_screening_id'],
+            'exam_gradation_id' => $item['exam_gradation_id'],
+            'exam_id'           => $item['exam_id'],
+        ];
+
+        $result = ExamDraftFlow::create($draftFlowData);
+        $item->adasdad=$result->id;
+        if(!$item->save())
+        {
+            throw new \Exception('');
+        }
+
+    }
+
+    public function bigTwo($data){
+        $draftFlowData = [
+            'order'             => $data['order'],
+            'name'              => $data['name'],
+            'exam_screening_id' => $data['exam_screening_id'],
+            'exam_gradation_id' => $data['exam_gradation_id'],
+            'exam_id'           => $data['exam_id'],
+        ];
+
+        $result = ExamDraftFlow::create($draftFlowData);
+
+
+    }
+
 }
