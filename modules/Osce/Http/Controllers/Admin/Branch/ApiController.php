616e344e875140684b0f68ed1f7e2face9474a9f
weihuiguo <weihuiguo@misrobot.com>
2016-04-07 16:42:22 +0800

    调试

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -585,7 +585,7 @@ class ApiController extends CommonController
             }
         }
 
-//dd($examData);
+dd($examData);
         return view('osce::admin.theoryCheck.theory_check_student_volidate', [
             'userInfo'   => @$userInfo,
             'examData' => @$examData
