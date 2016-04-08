50a05942f94f5dee50e547f9a874bc6951d866c8
zengjie <zengjie@misrobot.com>
2016-04-08 15:31:02 +0800

    正在考试添加刷新

--- modules/Osce/Resources/views/admin/testMonitor/monitor_test.blade.php
+++ modules/Osce/Resources/views/admin/testMonitor/monitor_test.blade.php
@@ -25,6 +25,9 @@
             <div class="col-xs-6 col-md-2">
                 <h5 class="title-label">考试监控</h5>
             </div>
+            <div class="col-xs-6 col-md-2 right">
+                <button class="btn btn-sm btn-primary marl_10 right" id="refresh">刷新</button>
+            </div>
         </div>
         @if(!empty($data['examName']['name']))
         <div class="container-fluid ibox-content">
--- public/osce/admin/testMonitor/test_monitor.js
+++ public/osce/admin/testMonitor/test_monitor.js
@@ -13,6 +13,10 @@ $(function(){
 
 //考试监控正在考试页面
 function monitor_test(){
+    //刷新页面
+    $("#refresh").click(function(){
+        window.location.href=window.location.href;
+    });
     //终止考试
     $(".stop").click(function () {
         //终止考试弹出层标题赋值
