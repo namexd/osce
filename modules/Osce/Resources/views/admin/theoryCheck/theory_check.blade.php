49e1b9d7d0487aa4e746d828fc635b52a590a807
zengjie <zengjie@misrobot.com>
2016-04-12 13:44:59 +0800

    修改bug

--- modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
+++ modules/Osce/Resources/views/admin/theoryCheck/theory_check.blade.php
@@ -187,9 +187,6 @@
         },3000);
         function countDown(time,id){
             var day_elem = $(id).find('.day');
-            var hour_elem = $(id).find('#hour');
-            var minute_elem = $(id).find('#minute');
-            var second_elem = $(id).find('#second');
             var end_time = new Date(time).getTime(),//月份是实际月份-1
                     sys_second = (end_time-new Date().getTime())/1000;
             var timer = setInterval(function(){
@@ -200,10 +197,9 @@
                     var minute = Math.floor((sys_second / 60) % 60);
                     var second = Math.floor(sys_second % 60);
                     day_elem && $(day_elem).text(day);//计算天
-                    hour_elem.text(hour<10?"0"+hour:hour);//计算小时
-                    minute_elem.text(minute<10?"0"+minute:minute);//计算分钟
-                    second_elem.text(second<10?"0"+second:second);//计算秒杀
-                    console.log(minute+"        "+$("#second").text()+"    "+$("#minute"));
+                    $("#hour").text(hour<10?"0"+hour:hour);//计算小时
+                    $("#minute").text(minute<10?"0"+minute:minute);//计算分钟
+                    $("#second").text(second<10?"0"+second:second);//计算秒杀
                 } else {
                     //var postnew=localStorage.getItem("Storage_answer")+"{{$examPaperFormalData["id"]}}";
                     var examPaperFormalId=$('#examPaperFormalId').val();
