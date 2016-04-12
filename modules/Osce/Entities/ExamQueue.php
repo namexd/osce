3288d2d593a85529c6a23cd71d9e5c9a4bb44cda
xumin <xumin@misrobot.com>
2016-04-12 15:01:47 +0800

    理论考试

--- modules/Osce/Entities/ExamQueue.php
+++ modules/Osce/Entities/ExamQueue.php
@@ -327,7 +327,7 @@ class ExamQueue extends CommonModel
             //拿到正在考的考试
             $exam = Exam::where('status', '=', 1)->first();
 
-//                查询学生是否已开始考试
+            //查询学生是否已开始考试
             //dd($studentId);
             $examQueue = ExamQueue::where('student_id', '=', $studentId)
                 ->where('station_id', '=', $stationId)
@@ -340,10 +340,9 @@ class ExamQueue extends CommonModel
             if ($examQueue->status == 2) {
                 return true;
             }
-//            修改队列状态
+            //修改队列状态
             $examQueue->status=2;
-            $examQueue->stick=null;
-
+            //$examQueue->stick=null;
             if ( $examQueue->save()) {
                 $studentTimes = ExamQueue::where('student_id', '=', $studentId)
                     ->whereIn('exam_queue.status', [0, 2])
