384b16887dc0543cf63cb36025e426279ec0a0ec
weihuiguo <weihuiguo@misrobot.com>
2016-04-12 16:38:03 +0800

    接口修改

--- modules/Osce/Http/Controllers/Api/InvigilatePadController.php
+++ modules/Osce/Http/Controllers/Api/InvigilatePadController.php
@@ -792,11 +792,11 @@ class InvigilatePadController extends CommonController
 
             foreach($boundWatchInfo as $k=>$v){
                 if($v['status'] < 2){
-                    $boundWatchInfo[$k]['status'] = '等待中';
+                    $boundWatchInfo[$k]['status'] = '0';
                 }elseif($v['status'] == 2){
-                    $boundWatchInfo[$k]['status'] = '考试中';
+                    $boundWatchInfo[$k]['status'] = '1';
                 }else{
-                    $boundWatchInfo[$k]['status'] = '已结束';
+                    $boundWatchInfo[$k]['status'] = '2';
                 }
             }
             return response()->json(
@@ -1165,7 +1165,18 @@ class InvigilatePadController extends CommonController
      */
     public function getWatchUnbundlingReportLog($station_id,$exam_id,$student_id,$type,$description){
         $ExamMonitor = new ExamMonitor();
+        $data = array();
+        $user = Auth::user();
+        $data = [
+            'station_id'        => $station_id,
+            'exam_id'           => $exam_id,
+            'student_id'        => $student_id,
+            'created_user_id'   => $user->id,
+            'type'              => $type,
+            'description'       => $description
+        ];
 
+        $ExamMonitor->create($data);
     }
     /**
      *  解除腕表绑定并上报
