05796df3a0ec5980ee99729939e30ea14cbac641
zhouqiang <candyphp@163.com>
2016-04-14 17:22:35 +0800

    修改考试新增弹框

--- modules/Osce/Entities/ExamArrange/ExamArrange.php
+++ modules/Osce/Entities/ExamArrange/ExamArrange.php
@@ -9,6 +9,9 @@
 namespace Modules\Osce\Entities\ExamArrange;
 
 
+use Modules\Osce\Entities\Invite;
+use Modules\Osce\Entities\StationTeacher;
+
 class ExamArrange
 {
 
@@ -19,7 +22,7 @@ class ExamArrange
     //清空考试安排
     public function getEmptyExamArrange($examId){
         
-        $
+
 
 
     }
@@ -27,20 +30,26 @@ class ExamArrange
 
     //清除考官安排
     public function getTeacherArrange($examId){
-
-
-
-
-
+        
+        if(!StationTeacher::where('exam_id','=',$examId)->delete()){
+            throw new \Exception('删除老师安排失败');
+        }
+        return true;
+        
     }
 
 
     //归档考试邀请
     public function getTeacherInvite($examId){
+        
+        $TeacherInvite = Invite::where('exam_id'.'=',$examId)->get();
+        foreach ($TeacherInvite as $item){
+            $item->status =3;
+            if(!$item -> save()){
+                throw new \Exception('修改老师邀请失败');
+            }
+        }
 
-
-
-
-
+        return true;
     }
 }
