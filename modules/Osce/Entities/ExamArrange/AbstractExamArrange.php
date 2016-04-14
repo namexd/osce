1cc4cc82ab17e99dce277d3989b53b0c163fbdc3
zhouqiang <candyphp@163.com>
2016-04-14 17:01:51 +0800

    修改清空考试

---
+++ modules/Osce/Entities/ExamArrange/AbstractExamArrange.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/4/14 0014
+ * Time: 15:55
+ */
+
+namespace Modules\Osce\Entities\ExamArrange;
+
+use Illuminate\Container\Container as App;
+
+abstract class AbstractExamArrange
+{
+    private $app;
+
+    protected $model;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+        $this->makeModel();
+    }
+
+    public function makeModel()
+    {
+        $model = $this->app->make($this->getModel());
+
+        return $this->model = $model;
+    }
+
+    abstract public function getModel();
+
+
+}
---
+++ modules/Osce/Entities/ExamArrange/ExamArrange.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/4/14 0014
+ * Time: 16:35
+ */
+
+namespace Modules\Osce\Entities\ExamArrange;
+
+
+class ExamArrange
+{
+
+
+
+
+
+    //清空考试安排
+    public function getEmptyExamArrange($examId){
+        
+        $
+
+
+    }
+
+
+    //清除考官安排
+    public function getTeacherArrange($examId){
+
+
+
+
+
+    }
+
+
+    //归档考试邀请
+    public function getTeacherInvite($examId){
+
+
+
+
+
+    }
+}
