f162b9364a9c601a7a0926748011c7766e33cd64
weihuiguo <weihuiguo@misrobot.com>
2016-04-15 16:51:22 +0800

    xxx

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -335,8 +335,8 @@ class DrawlotsController extends CommonController
             $teacher_id =(int)$request->input('teacher_id');
             ExamQueue::where('id',$examQueueId)->increment('next_num', 1);//下一次次数增加
             $exam = Exam::doingExam();
-            $studentModel = new  Student();
-            $studentData = $studentModel->nextStudentList($stationId, $exam);
+            //$studentModel = new  Student();
+           // $studentData = $studentModel->nextStudentList($stationId, $exam);
 
             list($room_id, $stations) = $this->getRoomIdAndStation($teacher_id, $exam);
             if ($exam->sequence_mode == 1) {
@@ -346,8 +346,12 @@ class DrawlotsController extends CommonController
             } else {
                 throw new \Exception('考试模式不存在！', -703);
             }
+            $request['id']=$teacher_id;
+            $request['exam_id']=$exam->id;
+            $this->getNextExaminee($request);//推送下一组
+            $this->getExaminee($request);//推送当前小组
             return response()->json(
-                $this->success_data($studentData['nextTester'], 1, '验证完成')
+                $this->success_data($examQueue, 1, '验证完成')
             );
 
         } catch (\Exception $ex) {
