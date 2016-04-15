a34e26671ce26a1f1635caae7294b6b67eef9d72
jiangzhiheng <j5110021@misrobot.com>
2016-04-15 11:22:25 +0800

    添加了清空智能排考数据的方法

--- modules/Osce/Entities/ExamArrange/Traits/SqlTraits.php
+++ modules/Osce/Entities/ExamArrange/Traits/SqlTraits.php
@@ -10,6 +10,9 @@ namespace Modules\Osce\Entities\ExamArrange\Traits;
 
 
 use Modules\Osce\Entities\ExamDraftFlow;
+use Modules\Osce\Entities\ExamOrder;
+use Modules\Osce\Entities\ExamPlan;
+use Modules\Osce\Entities\ExamPlanRecord;
 
 trait SqlTraits
 {
@@ -27,5 +30,56 @@ trait SqlTraits
                 'exam_draft.subject_id as subject_id'
             )
             ->get();
+    }
+
+    /**
+     * 清空plan表
+     * @param $examId
+     * @return bool
+     * @throws \Exception
+     * @author Jiangzhiheng
+     * @time 2016-04-15 11:14
+     */
+    public function emptyingPlan($examId)
+    {
+        if (!ExamPlan::where('exam_id', $examId)->delete()) {
+            throw new \Exception('清空排考表失败！');
+        };
+
+        return true;
+    }
+
+    /**
+     * 清空plan_record表
+     * @param $examId
+     * @return bool
+     * @throws \Exception
+     * @author Jiangzhiheng
+     * @time 2016-04-15 11:17
+     */
+    public function emptyingPlanRecord($examId)
+    {
+        if (!ExamPlanRecord::where('exam_id', $examId)->delete()) {
+            throw new \Exception('清空排考数据表失败！');
+        }
+
+        return true;
+    }
+
+    /**
+     * 清空order表
+     * @param $examId
+     * @return bool
+     * @throws \Exception
+     * @author Jiangzhiheng
+     * @time 2016-04-15 11:20
+     */
+    public function emptyingOrder($examId)
+    {
+        if (!ExamOrder::where('exam_id', $examId)->delete()) {
+            throw new \Exception('清空腕表顺序表失败！');
+        }
+
+        return true;
     }
 }
