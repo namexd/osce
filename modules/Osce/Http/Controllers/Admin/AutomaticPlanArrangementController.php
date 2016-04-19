79ad8683c86a2fa459a173c1fbdda9563c3effeb
jiangzhiheng <j5110021@misrobot.com>
2016-04-19 16:51:33 +0800

    增强了代码的健壮性

--- modules/Osce/Http/Controllers/Admin/AutomaticPlanArrangementController.php
+++ modules/Osce/Http/Controllers/Admin/AutomaticPlanArrangementController.php
@@ -79,7 +79,7 @@ class AutomaticPlanArrangementController extends CommonController
 
         try {
             $exam = \Modules\Osce\Entities\Exam::doingExam($examId);
-//            $app = new App();
+            Common::valueIsNull($exam, -999, '当前的考试错误');
             $smartArrangeRepository = new SmartArrangeRepository($app);
 
             return response()->json($this->success_data($smartArrangeRepository->plan($exam)));
@@ -111,11 +111,10 @@ class AutomaticPlanArrangementController extends CommonController
 //        $user = Auth::user();
 
 
-
 //        try {
 //            $examPlan->storePlan($examId, $user);
 
-            return redirect()->route('osce.admin.exam.getIntelligence', ['id' => $examId]);
+        return redirect()->route('osce.admin.exam.getIntelligence', ['id' => $examId]);
 //        } catch (\Exception $ex) {
 //            return redirect()->back()->withErrors($ex->getMessage());
 //        }
