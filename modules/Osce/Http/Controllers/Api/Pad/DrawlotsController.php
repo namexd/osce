dd0b2bccc4afacd379899b00011523282bbdde81
jiangzhiheng <j5110021@misrobot.com>
2016-04-19 18:27:33 +0800

    测试提交

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -468,7 +468,7 @@ class DrawlotsController extends CommonController
             'room_id' => 'required|integer',
             'teacher_id' => 'required|integer'
         ]);
-        try {
+//        try {
             $examId = $request->input('exam_id', null);
             //获取uid和room_id
             $uid = $request->input('uid');
@@ -572,11 +572,11 @@ class DrawlotsController extends CommonController
             $inv->getAuthentication_arr($request);//当前考生推送
             return response()->json($this->success_data($result));
 
-        } catch (\Exception $ex) {
-            $connection->rollBack();
-            
-            return response()->json($this->fail($ex));
-        }
+//        } catch (\Exception $ex) {
+//            $connection->rollBack();
+//
+//            return response()->json($this->fail($ex));
+//        }
     }
 
     /**
