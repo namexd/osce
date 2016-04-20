ab33dc31d25211c00f7f490d6a244571d2b47a6c
zhoufuxiang <zhoufuxiang@163.com>
2016-04-20 15:48:45 +0800

    修改；考场安排

--- modules/Osce/Entities/ExamArrange/Traits/SqlTraits.php
+++ modules/Osce/Entities/ExamArrange/Traits/SqlTraits.php
@@ -19,7 +19,7 @@ trait SqlTraits
     public function checkExamArrange($examId)
     {
         return ExamDraftFlow::join('exam_draft', 'exam_draft.exam_draft_flow_id', '=', 'exam_draft_flow.id')
-            ->where('exam_id', $examId)
+            ->where('exam_draft_flow.exam_id', $examId)
             ->select(
                 'exam_draft_flow.order as exam_draft_flow_order',
                 'exam_draft_flow.exam_screening_id as exam_screening_id',
@@ -27,7 +27,8 @@ trait SqlTraits
                 'exam_draft.station_id as station_id',
                 'exam_draft.room_id as room_id',
                 'exam_draft.exam_draft_flow_id as exam_draft_flow_id',
-                'exam_draft.subject_id as subject_id'
+                'exam_draft.subject_id as subject_id',
+                'exam_draft.id as exam_draft_id'
             )
             ->get();
     }
