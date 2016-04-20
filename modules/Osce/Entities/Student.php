953f5a978d01228b1cebec0e81ab4b44970d3b26
wangtao <wangtao@misrobot.com>
2016-04-20 09:49:31 +0800

    查询学生加上对应考试条件

--- modules/Osce/Entities/Student.php
+++ modules/Osce/Entities/Student.php
@@ -800,6 +800,7 @@ class Student extends CommonModel
         })
             ->where('exam_queue.station_id', '=', $stationId)
             ->where('exam_queue.exam_id','=',$exam->id)
+            ->where('station_teacher.exam_id','=', $exam->id)
             ->whereIn('exam_queue.status', [1, 2])
             ->where('exam_queue.blocking', 1)
             ->orderBy('exam_queue.begin_dt', 'asc')
