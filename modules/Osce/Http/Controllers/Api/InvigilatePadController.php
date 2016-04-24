4ec3d4515888a47323929603d0e856085c85cfb9
zhouqiang <candyphp@163.com>
2016-04-24 11:38:58 +0800

    修改时间锚点上传

--- modules/Osce/Http/Controllers/Api/InvigilatePadController.php
+++ modules/Osce/Http/Controllers/Api/InvigilatePadController.php
@@ -640,9 +640,9 @@ class InvigilatePadController extends CommonController
             $examId = $request->input('exam_id');
             $timeAnchor = $request->input('time_anchors');
             $teacherId = $request->input('user_id');
-            \Log::alert('anchor', $timeAnchor);
             //将戳过来的字符串变成数组
             $timeAnchor = explode(',', $timeAnchor);
+            \Log::alert('anchor', $timeAnchor);
 
             return response()->json($this->success_data(self::storeAnchor($stationId, $studentId, $examId, $teacherId, $timeAnchor)));
         } catch (\Exception $ex) {
