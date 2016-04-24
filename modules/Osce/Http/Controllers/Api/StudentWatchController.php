c1b7a09f226b5b79c359feb159d342e1b0d84c56
zhouqiang <candyphp@163.com>
2016-04-24 15:58:15 +0800

    xiugai wanbia

--- modules/Osce/Http/Controllers/Api/StudentWatchController.php
+++ modules/Osce/Http/Controllers/Api/StudentWatchController.php
@@ -335,7 +335,6 @@ class StudentWatchController extends CommonController
             ->where('exam_screening_id', '=', $item->exam_screening_id)
             ->where('station_id', '=', $stationId)
             ->first();
-            dd($stationId,$item->exam_id,$item->exam_screening_id);
         if ($instance->status == 0) {
             return [
                 'code'  => 0, // 0，等待状态（对应界面：Prepare_fragment）
