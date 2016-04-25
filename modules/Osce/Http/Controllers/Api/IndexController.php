4715ca70ddf6ec57c51c58bf04512c5ce17cb9c3
zhouqiang <candyphp@163.com>
2016-04-25 16:14:50 +0800

    修改绑定

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -129,6 +129,7 @@ class IndexController extends CommonController
         if(count($check) > 0){
             //dd($check->id);
             $watchLog = WatchLog::where('watch_id','=',intval($check->id))->first();
+            dump($watchLog,$check);
             if($watchLog->action == '绑定'){
                 return \Response::json(array('code'=>11)); //判断当前腕表已绑定身份证
             }
