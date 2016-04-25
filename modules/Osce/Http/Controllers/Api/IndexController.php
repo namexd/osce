c43314a17b0fbe73e10e091bf877ce959b6e4de2
weihuiguo <weihuiguo@misrobot.com>
2016-04-25 16:02:49 +0800

    调试

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -127,8 +127,8 @@ class IndexController extends CommonController
         $check = $watchModel->where('watch.code','=',$code)->first();
 
         if(count($check) > 0){
-            dd($check->id);
-            $watchLog = WatchLog::where('watch_id','=',$check->id)->orderBy('id','desc')->first();
+            //dd($check->id);
+            $watchLog = WatchLog::where('watch_id','=',intval($check->id))->orderBy('id','desc')->first();
             dd($watchLog);
             if($watchLog->action == '绑定'){
                 return \Response::json(array('code'=>11)); //判断当前腕表已绑定身份证
