e5322ecc62a0adfe42a446b204e3cf42764f469e
zhouqiang <candyphp@163.com>
2016-04-25 16:24:26 +0800

    修改绑定

--- modules/Osce/Http/Controllers/Api/IndexController.php
+++ modules/Osce/Http/Controllers/Api/IndexController.php
@@ -129,9 +129,12 @@ class IndexController extends CommonController
         if(count($check) > 0){
             //dd($check->id);
             $watchLog = WatchLog::where('watch_id','=',intval($check->id))->first();
-            if($watchLog->action == '绑定'){
-                return \Response::json(array('code'=>11)); //判断当前腕表已绑定身份证
+            if(!is_null($watchLog)){
+                if($watchLog->action == '绑定'){
+                    return \Response::json(array('code'=>11)); //判断当前腕表已绑定身份证
+                }
             }
+
         }
         //获取腕表id
         $id     = Watch::where('code',$code)->select('id')->first()->id;
