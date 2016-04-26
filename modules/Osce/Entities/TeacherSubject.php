08e0f728e2b23c78b673cea15e42ec438b8a57dd
zhouqiang <candyphp@163.com>
2016-04-26 11:18:03 +0800

    修改考官安排去重

--- modules/Osce/Entities/TeacherSubject.php
+++ modules/Osce/Entities/TeacherSubject.php
@@ -25,7 +25,18 @@ class TeacherSubject extends CommonModel
         return $this->hasOne('\Modules\Osce\Entities\Subject','id','subject_id');
     }
 
-    public function getTeachers($type, $subject_id){
+    public function getTeachers($type, $subject_id,$examGradationId,$examId){
+        //查询出阶段下的大站id和小站里的数据拿到所有的考站id
+            $data = ExamDraftFlow::leftJoin('exam_draft', 'exam_draft.exam_draft_flow_id', '=','exam_draft_flow.id')
+                ->leftJoin('station_teacher', 'station_teacher.station_id', '=', 'exam_draft.station_id')
+                ->where('exam_draft_flow.exam_id',$examId)
+                ->where('exam_draft_flow.exam_gradation_id',$examGradationId)
+                ->where('station_teacher.exam_id',$examId)
+                ->select([
+                    'station_teacher.user_id'
+                ])
+                ->get()->toArray();
+
         if ($subject_id == 0){
 
             return Teacher::where('archived','=',0)->where('type','=', $type)->select(['id as teacher_id', 'name'])->get();
