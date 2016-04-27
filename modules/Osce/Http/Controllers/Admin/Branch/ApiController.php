a1f2ec3e165e72def6be964c096b3ce1984dfafe
weihuiguo <weihuiguo@misrobot.com>
2016-04-27 17:58:41 +0800

    调试

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -437,6 +437,7 @@ class ApiController extends CommonController
                 $user = Auth::user();
                 $student_id = Student::where('user_id','=',$user->id)->first()->pluck('id');
                 $examId = Exam::where('status','=',1)->first()->pluck('id');
+                dd($student_id.'-'.$examId);
                 $studentInfo = ExamQueue::where('exam_queue.student_id','=',$student_id)->where('exam_queue.exam_id','=',$examId)->leftjoin('station_teacher',function($join){
                     $join->on('station_teacher.exam_id','=','exam_queue.exam_id');
                 })->first();
