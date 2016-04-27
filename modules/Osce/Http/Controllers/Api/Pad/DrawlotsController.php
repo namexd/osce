ae4125945a4a6f15039c82b7f36c53f31e127b32
zhouqiang <candyphp@163.com>
2016-04-27 15:45:20 +0800

    修改抽签

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use Modules\Osce\Entities\Exam;
 use Modules\Osce\Entities\ExamDraft;
 use Modules\Osce\Entities\ExamDraftFlow;
+use Modules\Osce\Entities\ExamGradation;
 use Modules\Osce\Entities\ExamScreening;
 use Modules\Osce\Entities\QuestionBankEntities\ExamPaper;
 use Modules\Osce\Entities\Student;
@@ -1010,9 +1011,21 @@ class DrawlotsController extends CommonController
                 'station_id'
             )
             ->get();*/
+
+         $gradationOrder = ExamScreening::find($examScreeingId);
+        if(!$gradationOrder){
+
+            throw new \Exception('没有找到对应的阶段');
+
+        }else{
+            $gradationOrderId = ExamGradation::where('exam_id','=',$examId)->where('order','=',$gradationOrder->gradation_order)->get()->pluck('id');
+
+        }
+
         $stationIds = ExamDraft::leftJoin('exam_draft_flow', 'exam_draft_flow.id', '=', 'exam_draft.exam_draft_flow_id')
             ->where('exam_draft_flow.exam_id', '=', $examId)
             ->where('exam_draft.room_id',$roomId)
+            ->whereIn('exam_draft_flow.exam_gradation_id',$gradationOrderId)
             ->select(
                 'exam_draft.station_id as station_id'
             )
