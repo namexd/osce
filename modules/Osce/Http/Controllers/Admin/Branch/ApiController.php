d91489a4013914671931a7820062e3612b238934
weihuiguo <weihuiguo@misrobot.com>
2016-04-27 14:26:10 +0800

    调试

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -577,7 +577,7 @@ class ApiController extends CommonController
             }
 
 
-
+            dd($examing);
             //整理考试数据
             $examData = array();
             $StationTeacher = new StationTeacher();
