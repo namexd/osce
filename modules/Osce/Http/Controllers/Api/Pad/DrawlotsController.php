62f360e2eebc3d90a7dc0814ff00478401ee9cb1
wangtao <wangtao@misrobot.com>
2016-04-27 16:47:16 +0800

    测试

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -97,7 +97,7 @@ class DrawlotsController extends CommonController
      * @copyright 2013-2015 MIS misrobot.com Inc. All Rights Reserved
      */
     public function getExaminee(Request $request)
-    {
+    {dd($_SERVER['SERVER_NAME']);
         $this->validate($request, [
             'id' => 'required|integer',
             'exam_id' => 'sometimes|integer'
