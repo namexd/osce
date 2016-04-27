634e8a441da8c67fe3318b40c7ce869fbf11bdf2
zhouqiang <candyphp@163.com>
2016-04-27 16:16:20 +0800

    修改抽签

--- modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
+++ modules/Osce/Http/Controllers/Api/Pad/DrawlotsController.php
@@ -1015,7 +1015,7 @@ class DrawlotsController extends CommonController
          $gradationOrder = ExamScreening::find($examScreeingId);
         if(!$gradationOrder){
 
-            throw new \Exception('没有找到对应的阶段');
+            throw new \Exception('没有找到对应的阶段',3660);
 
         }else{
             $gradationOrderId = ExamGradation::where('exam_id','=',$examId)->where('order','=',$gradationOrder->gradation_order)->get()->pluck('id');
@@ -1033,7 +1033,7 @@ class DrawlotsController extends CommonController
         //$stationIds为还没有被使用的考站
         $stationIds = array_diff($stationIds->pluck('station_id')->toArray(), $stationIdeds);
         if(empty($stationIds)){
-            throw new \Exception('当前没有空闲考站，请等待！！',3601);
+            throw new \Exception('当前没有空闲考站，请等待！！',3670);
         }
         //$ranStationId为随机选择的一个考站
         $ranStationId = $stationIds[array_rand($stationIds)];
