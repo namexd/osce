1dc9cf74295c1cc1a9a9fd55098ea0ffd6272955
weihuiguo <weihuiguo@misrobot.com>
2016-04-28 17:01:26 +0800

    教学成绩分析

--- public/osce/admin/statisticalanalysis/statistics_teach.js
+++ public/osce/admin/statisticalanalysis/statistics_teach.js
@@ -66,7 +66,7 @@ function statistics_teach_score(){
             async:false,
             success:function(res){
                 $(res.data.datalist).each(function(){
-                    $(".student_select").append('<option value="'+this.id+'">'+this.title+'</option>');
+                    $(".student_select").append('<option value="'+this.id+'">'+this.name+'</option>');
                     console.log(this.title+'-option-'+this.id)
                 });
             }
