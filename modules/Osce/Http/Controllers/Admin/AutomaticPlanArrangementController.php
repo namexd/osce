f7ac7e6d893e5890212d7dd66b8f829f719762c6
jiangzhiheng <j5110021@misrobot.com>
2016-04-28 11:06:21 +0800

    完善了轮循的排考

--- modules/Osce/Http/Controllers/Admin/AutomaticPlanArrangementController.php
+++ modules/Osce/Http/Controllers/Admin/AutomaticPlanArrangementController.php
@@ -71,16 +71,16 @@ class AutomaticPlanArrangementController extends CommonController
 
         $examId = $request->input('exam_id');
         
-//        try {
+        try {
             set_time_limit(0);
             $exam = \Modules\Osce\Entities\Exam::doingExam($examId);
             Common::valueIsNull($exam, -999, '当前的考试错误');
             $smartArrangeRepository = new SmartArrangeRepository($app);
 
             return response()->json($this->success_data($smartArrangeRepository->plan($exam)));
-//        } catch (\Exception $ex) {
-//            return response()->json($this->fail($ex));
-//        }
+        } catch (\Exception $ex) {
+            return response()->json($this->fail($ex));
+        }
     }
 
     /**
