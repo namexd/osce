c3c4243f6d2118ca9f75873eb53897eae3d1679f
weihuiguo <weihuiguo@misrobot.com>
2016-04-29 13:54:55 +0800

    调试

--- modules/Osce/Repositories/TestScoreRepositories.php
+++ modules/Osce/Repositories/TestScoreRepositories.php
@@ -338,9 +338,7 @@ class TestScoreRepositories  extends BaseRepository
     public function getTeacherData($examid,$subjectid){
         $DB = \DB::connection('osce_mis');
         $ExamResult = new ExamResult();
-        echo $subjectid;
-        dd(intval($subjectid));
-        if(!empty($subjectid)){
+        if(intval($subjectid)){
             $examlist = $ExamResult->where('exam_paper.id','=',$subjectid)->where('exam_screening.exam_id','=',$examid)->leftjoin('exam_screening',function($join){
                 $join->on('exam_screening.id','=','exam_result.exam_screening_id');
             })->leftjoin('station',function($join){
