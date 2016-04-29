7f476ead70663c381d418db82926637827203668
weihuiguo <weihuiguo@misrobot.com>
2016-04-29 10:34:58 +0800

    调试

--- public/osce/admin/statisticalanalysis/statistics_teach.js
+++ public/osce/admin/statisticalanalysis/statistics_teach.js
@@ -115,7 +115,7 @@ function statistics_teach_score(){
                 var subname = $('.student_select option:selected').html();
                 if(avgStr){echartsSubject(teacherStr,avgStr,maxScore,minScore);}
                 $(res.data.data.datalist).each(function(i){
-                    var jumpUrl = '/osce/admin/testscores/grade-score-list?examid='+this.exam_id+'&classid='+this.grade_class+'&subname='+subname+'&subid='+this.subid+'&classid='+this.grade_class;
+                    var jumpUrl = '/osce/admin/testscores/grade-score-list?examid='+this.exam_id+'&classid='+this.grade_class+'&subname='+subname+'&subid='+this.pid+'&classid='+this.grade_class;
                     $(".subjectBody").append('<tr>' +
                         '<td>'+(i+1)+'</td>' +
                         '<td>'+this.teacher_name+'</td>' +
