cb3670c6b0078e4db5462fced08a31d730ed24cc
wangtao <wangtao@misrobot.com>
2016-05-01 19:01:02 +0800

    腕表

--- modules/Osce/Entities/Watch.php
+++ modules/Osce/Entities/Watch.php
@@ -252,10 +252,10 @@ class Watch extends CommonModel implements MachineInterface
         $builder = $builder->where('watch.status','=',$status)
             ->where('exam_screening_student.is_end','=',0)
             ->where('exam_screening_student.is_signin','=',1)
-            ->where('watch_log.action','=','绑定')
+            //->where('watch_log.action','=','绑定')
             ->where('exam_queue.exam_id','=',$examId)
 
-            ->leftjoin('watch_log',function($watchLog){
+            /*->leftjoin('watch_log',function($watchLog){
             $watchLog->on('watch_log.watch_id','=','watch.id');
         })->leftjoin('exam_queue',function($examQueue){
             $examQueue->on('exam_queue.student_id','=','watch_log.student_id');
@@ -268,7 +268,27 @@ class Watch extends CommonModel implements MachineInterface
             ->where('exam_queue.exam_screening_id',$exam_screening_id)
             ->select('watch.id','watch.code as nfc_code','watch.nfc_code as code','student.name','exam_queue.status')
             ->orderBy('watch_log.id','desc')->get();
+*/
+
+        ->rightjoin('exam_screening_student',function($examQueue){
+            $examQueue->on('exam_screening_id.watch_id','=','watch.id');
+        })->leftjoin('student',function($examQueue){
+            $examQueue->on('student.id','=','exam_screening_student.student_id');
+        })->leftjoin('exam_order',function($join){
+            $join->on('exam_order.student_id','=','exam_screening_student.student_id');
+        })->groupBy('watch_log.student_id')
+            ->where('exam_screening_student.exam_screening_id',$exam_screening_id)
+            ->where('exam_queue.exam_screening_id',$exam_screening_id)
+            ->where('exam_order.exam_screening_id',$exam_screening_id)
+            ->where('exam_order.exam_id',$examId)
+            ->select('watch.id','watch.code as nfc_code','watch.nfc_code as code','student.name','exam_queue.status')
+            ->orderBy('exam_order.id','desc')->get();
+
 //        dd($builder->toArray());
+
+
+
+
         return $builder;
     }
 
