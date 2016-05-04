b47b0e4051519a0b0cb7b6176cfa74cf4e8421ac
zhouqiang <candyphp@163.com>
2016-05-04 19:24:47 +0800

    修改替考警告

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -982,7 +982,7 @@ class ApiController extends CommonController
                             'end_dt'            => date('Y-m-d H:i:s', time()),
                             'score'             => 0,
                             'score_dt'          => date('Y-m-d H:i:s', time()),
-                            'create_user_id'    => Auth::user()->id,
+//                            'create_user_id'    => Auth::user()->id,
                         ];
                         if(!ExamResult::create($examResultData)){
                             throw new \Exception(' 向考试结果记录表插入数据失败！',-106);
