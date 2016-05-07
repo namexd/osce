c43fbef0f34c3a83656be1e6992471c46fe06e29
wangtao <wangtao@misrobot.com>
2016-05-07 15:35:42 +0800

    门牌启动页

---
+++ modules/Osce/Resources/views/doorplate/login.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" ng-app="myApp" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" ng-app="myApp" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" ng-app="myApp" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" ng-app="myApp" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>OSCE考试智能管理系统</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script type="text/javascript" src="{{asset('osce/admin/plugins/js/jquery-2.1.1.min.js')}}" ></script>
+	<link rel="stylesheet" type="text/txt" src="{{asset('osce/common/css/bootstrapValidator.css')}}"/>
+	<script type="text/javascript" src="{{asset('osce/common/js/bootstrapValidator.js')}}"> </script>
+
+	<style>
+
+		*{outline:none;}
+		.hide{display: none}
+		body {
+			background-color: #364150;
+		}
+		h1{text-align: center;font-family: 黑体;}
+		.box{
+			width: 460px;height: 300px;position: absolute;left: 50%;top: 45%;margin-top: -150px;margin-left: -230px;
+		}
+		select{border-radius: 5px;width: 450px;padding: 10px;font-size: 20px;font-family: 黑体;margin: 10px 0}
+		.hide{display: none}
+		.sub{
+			border-radius: 5px;width: 450px;height: 50px;margin-top: 20px;background: #16BEB0;border: none;
+			color: #fff;font-family: 黑体;
+			font-size: 20px;
+
+		}
+		.red{color: #F53E3F}
+		.white{color: #ffffff;}
+	</style>
+
+	<?php
+	$errorsInfo =(array)$errors->getMessages();
+	if(!empty($errorsInfo)){
+		$errorsInfo = array_shift($errorsInfo);
+	}
+	?>
+
+
+</head>
+<body>
+<div class="box">
+	<h1><span class="red">OSCE</span><span class="white">考试智能管理系统</span></h1>
+	<form class="m-t" role="form"  onsubmit="return jufe()" method="get" action="{{ route('osce.doorplate.getdoorplatemsg') }}">
+
+		<select name="examId" id="examId"  class="input-sm form-control subject_select" style="height: 50px">
+
+			@if(!empty(@$examList))
+
+				@foreach(@$examList as $val)
+					<option value="{{ $val['exam_id'] }}" selected="selected" >
+						{{ $val['name'] }}
+					</option>
+				@endforeach
+			@else
+				<option value="">当前没有考试</option>
+			@endif
+		</select>
+		<p style="margin-top: 5px;margin-bottom: 10px;color: red" class="hide exam">　请选择考试</p>
+		<h3 class="tt">
+
+		</h3>
+		<select name="roomId" id="roomId" class="input-sm form-control subject_select" style="height: 50px">
+			<option value=''>请选择考场</option>
+			@if(!empty(@$roomList))
+				@foreach(@$roomList as $val)
+					<option value="{{ $val['id'] }}" >
+						{{ $val['name'] }}
+					</option>
+				@endforeach
+			@endif
+		</select>
+		<p style="margin-top: 5px;margin-bottom: 10px;color: red" class="hide room" >　请选择考场</p>
+		@forelse($errorsInfo as $errorItem)
+			<div class="pnotice" style="display: block;color: red">{{$errorItem}}</div>
+		@empty
+		@endforelse
+		<button type="submit"  class="btn btn-primary block full-width m-b sub">
+			启　动
+		</button>
+	</form>
+
+</div>
+
+<script>
+
+	var m='';
+	$('#examId').change(function(){
+		var examId= $.trim($(this).val())
+		var opstr='<option value="1">请选择考场</option>';
+		if(examId!=''){
+
+			$.ajax({
+				type: "GET",
+				url: "{{route('osce.api.LoginPullDown.getRoomList')}}",
+				data: {'exam_id':examId},
+				success: function(msg){
+					if(msg){
+						$(msg.data).each(function(i,k){
+							opstr += '<option value="'+ k.id+'">'+ k.name+'</option>　';
+						});
+						$('#roomId').html(opstr);
+					}
+				}
+			});
+
+		}else{
+			$('#roomId').html('<option value="">没有相关房间信息</option>');
+		}
+
+	})
+	$('#roomId').change(function(){
+		$('.pnotice').css('display','none')
+	})
+	function jufe(){
+		$('.exam').addClass('hide');
+		$('.room').addClass('hide');
+		var exam= $.trim($('#examId').val());
+		var room= $.trim($('#roomId').val());
+
+		if(exam==''){
+			$('.exam').removeClass('hide')
+			return false;
+		}
+		if(room==''){
+			$('.room').removeClass('hide')
+			return false;
+		}
+		return true;
+	}
+</script>
+</body>
+</html>
