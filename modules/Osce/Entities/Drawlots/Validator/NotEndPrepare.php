694c2b067177c8786ebddc195819bda6669b375e
zhouqiang <candyphp@163.com>
2016-05-07 22:11:26 +0800

    修改了抽签的验证

--- modules/Osce/Entities/Drawlots/Validator/NotEndPrepare.php
+++ modules/Osce/Entities/Drawlots/Validator/NotEndPrepare.php
@@ -11,6 +11,7 @@ namespace Modules\Osce\Entities\Drawlots\Validator;
 
 use Modules\Osce\Entities\Drawlots\DrawValidatorInterface;
 use Modules\Osce\Entities\Drawlots\Station;
+use Modules\Osce\Entities\ExamQueue;
 use Modules\Osce\Entities\ExamStationStatus;
 
 class NotEndPrepare implements DrawValidatorInterface
@@ -27,14 +28,23 @@ class NotEndPrepare implements DrawValidatorInterface
         // TODO: Implement validate() method.
         $stations = $this->station->site($examId, $roomId, $screenId);
         $stationIds = $stations->pluck('station_id')->toArray();
-
-        if (!is_null(ExamStationStatus::whereIn('station_id', $stationIds)
+        $ready = ExamStationStatus::whereIn('station_id', $stationIds)
             ->whereExamScreeningId($screenId)
-            ->whereStatus(0)
-            ->first())
-        ) {
-            throw new \Exception('上场考试未完成，请稍后签到', -13);
+            ->whereStatus(1)
+            ->count();
+
+        if ($ready > 1) {
+            if (ExamQueue::where('status', 2)
+                ->where('exam_screening_id', $screenId)
+                ->where('room_id', $roomId)
+                ->count() == count($stationIds)) {
+                throw new \Exception('上场考试未完成，请稍后签到', -13);
+            }
         }
+
+//        if (!$ready) {
+//            //throw new \Exception('上场考试未完成，请稍后签到', -13);
+//        }
 
         return true;
     }
