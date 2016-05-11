3e4403a7f304c1bdd5c96c8387aba60243680a04
zhouqiang <candyphp@163.com>
2016-05-11 20:14:10 +0800

    修改腕表信息

--- modules/Osce/Repositories/WatchReminderRepositories.php
+++ modules/Osce/Repositories/WatchReminderRepositories.php
@@ -792,6 +792,7 @@ class WatchReminderRepositories extends BaseRepository
 
     public function getWatchPublish($studentId, $stationId, $roomId)
     {
+        \Log::info('测试数据',[1212]);
         $exam = Exam::doingExam();  //拿到考试实例
         $student = null;
         $station = null;
