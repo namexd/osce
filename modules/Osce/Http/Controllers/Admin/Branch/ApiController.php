e0d73d6cc7fcf7b2adc3692a2136271f9b28120b
jiangzhiheng <j5110021@misrobot.com>
2016-05-13 16:09:19 +0800

    修改了一个bug

--- modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
+++ modules/Osce/Http/Controllers/Admin/Branch/ApiController.php
@@ -853,7 +853,7 @@ class ApiController extends CommonController
 //            $join->on('exam_draft.exam_draft_flow_id', '=', 'exam_draft_flow.id');
 //        })->where('exam_draft_flow.exam_id',$examId)
 //            ->where('exam_draft.room_id',$roomId)->select('exam_draft.station_id')->get()->pluck('station_id')->toArray();
-        $examScreeningId = $draw->getScreening($examId);
+        $examScreeningId = $draw->getScreening($examId)->id;
         $stationArr = $draw->getStationNum($examId, $roomId, $examScreeningId);
         if(!$stationArr->isEmpty()){
             //查询exam_station_status表（考试-场次-考站状态表）中该考试该考场下status是否全为1，如果是，修改其状态值为2
