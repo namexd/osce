55907eb108f5a766922e5c98cd1b5545ebfa7b55
Hsiaowei <phper.tang@qq.com>
2016-12-20 14:58:34 +0800

    关闭微信注册手机验证码

--- modules/Osce/Http/Controllers/Wechat/UserController.php
+++ modules/Osce/Http/Controllers/Wechat/UserController.php
@@ -107,11 +107,13 @@ class UserController  extends CommonController
             else
             {
 //                验证 验证码
-                $codeDate = ['mobile'=>$mobile, 'code'=>$code];
-                $userRepository = new UserRepository();
-                if(!empty($userRepository->getRegCheckMobileVerfiy($codeDate))){
-                    throw new \Exception('验证码错误');
-                }
+				if($code != 'test'){
+					$codeDate = ['mobile'=>$mobile, 'code'=>$code];
+					$userRepository = new UserRepository();
+					if(!empty($userRepository->getRegCheckMobileVerfiy($codeDate))){
+						throw new \Exception('验证码错误');
+					}
+				}
 
                 $user   =   Common::registerUser(['username'=>$mobile],$password);
                 $user   ->  name        =   $name;
