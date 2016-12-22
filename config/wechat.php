4f528f2ea3312db9189593720ad2408f39e82db2
Hsiaowei <phper.tang@qq.com>
2016-12-22 17:32:28 +0800

    由于主域名不是微信回调地址

--- config/wechat.php
+++ config/wechat.php
@@ -2,6 +2,7 @@
 'use_alias'    => env('WECHAT_USE_ALIAS', true),
 'app_id'       => env('WECHAT_APPID', 'wxea5fd73483163570'), // 必填
 'secret'       => env('WECHAT_SECRET', 'a3a78120093fc569d55a221455d18826'), // 必填
+'redirect_url' => env('WECHAT_REDIRECT', 'osce.wststore.com'), // 不必填，主域名非微信回调地址需要
 'token'        => env('WECHAT_TOKEN', 'weixin'),  // 必填,http://good-doctor.cn/hx/wx_sample.php
 'encoding_key' => env('WECHAT_ENCODING_KEY', 'YourEncodingAESKey') // 加密模式需要，其它模式不需要
 ];
