6f3bc626720878029ad8c62680876ddb464b25cb
fandian <fandian@misrobot.com>
2016-12-23 11:03:21 +0800

    增加模板类型5

--- config/message.php
+++ config/message.php
@@ -12,6 +12,8 @@ return array (
       'TemplateCode2' => 'SMS_35005390',
       'TemplateCode3' => 'SMS_34860490',
       'TemplateCode4' => 'SMS_35070529',
+       'TemplateCode5' => 'SMS_35085054',
+
       'request_host' => 'http://sms.market.alicloudapi.com',
       'request_uri' => '/singleSendSms',
     ),
--- modules/Osce/Entities/Notice.php
+++ modules/Osce/Entities/Notice.php
@@ -373,13 +373,105 @@ class Notice extends CommonModel
 
 
     public function sendSms($notice,$to,$url){
-        $sender =   \App::make('messages.sms');
+/*        $sender =   \App::make('messages.sms');
         $content=   [];
         $content[]  =   $notice->exam->name. ' ' .$notice->title;
-        $content[]  =   '详情查看'.$url;
+        $content[]  =   '详情查看'.$url;*/
+        $TemplateCode5 = config('message.messages.sms.TemplateCode5');
+        $app_key = config('message.messages.sms.app_key');
+        $app_secret = config('message.messages.sms.app_secret');
+        $request_uri = config('message.messages.sms.request_uri');
+        $request_host = config('message.messages.sms.request_host');
+        $request_method = 'GET';
+        //签名名称
+        $SingName = config('message.messages.sms.SingName');
+        $url = preg_match('/id.(\d*)/',$url,$a)?$a[1]:0;
+        $content = "{\"name\":\"$notice->exam->name\",\"val\":\"$notice->title\",\"val1\":\"$url\"}";
+
+        $info = "";
+
         foreach($to as $mobile)
         {
-            $sender ->  send($mobile,implode('',$content).' 【敏行医学】');
+            //$sender ->  send($mobile,implode('',$content).' 【敏行医学】');
+
+            $request_paras = array(
+                'ParamString' => $content,
+                'RecNum' => $mobile,
+                'SignName' =>$SingName,
+                'TemplateCode' =>$TemplateCode5
+            );
+
+            $this->do_get($app_key, $app_secret, $request_host, $request_uri, $request_method, $request_paras, $info);
         }
+    }
+
+    /**
+     * 发送短信（阿里云）
+     * @param $mobile
+     * @param $message
+     * @author fandian <phpinfo@foxmail.com>
+     * @date    2016-12-20
+     * @copyright  2013-2017 sulida.com  Inc. All Rights Reserved
+     */
+    public function do_get($app_key, $app_secret, $request_host, $request_uri, $request_method, $request_paras, &$info) {
+        ksort($request_paras);
+        $request_header_accept = "application/json;charset=utf-8";
+        $content_type = "";
+        $headers = array(
+            'X-Ca-Key' => $app_key,
+            'Accept' => $request_header_accept
+        );
+        ksort($headers);
+        $header_str = "";
+        $header_ignore_list = array('X-CA-SIGNATURE', 'X-CA-SIGNATURE-HEADERS', 'ACCEPT', 'CONTENT-MD5', 'CONTENT-TYPE', 'DATE');
+        $sig_header = array();
+        foreach($headers as $k => $v) {
+            if(in_array(strtoupper($k), $header_ignore_list)) {
+                continue;
+            }
+            $header_str .= $k . ':' . $v . "\n";
+            array_push($sig_header, $k);
+        }
+        $url_str = $request_uri;
+        $para_array = array();
+        foreach($request_paras as $k => $v) {
+            array_push($para_array, $k .'='. $v);
+        }
+        if(!empty($para_array)) {
+            $url_str .= '?' . join('&', $para_array);
+        }
+        $content_md5 = "";
+        $date = "";
+        $sign_str = "";
+        $sign_str .= $request_method ."\n";
+        $sign_str .= $request_header_accept."\n";
+        $sign_str .= $content_md5."\n";
+        $sign_str .= "\n";
+        $sign_str .= $date."\n";
+        $sign_str .= $header_str;
+        $sign_str .= $url_str;
+
+        $sign = base64_encode(hash_hmac('sha256', $sign_str, $app_secret, true));
+        $headers['X-Ca-Signature'] = $sign;
+        $headers['X-Ca-Signature-Headers'] = join(',', $sig_header);
+        $request_header = array();
+        foreach($headers as $k => $v) {
+            array_push($request_header, $k .': ' . $v);
+        }
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $request_host . $url_str);
+        //curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $ret = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch);
+        return $ret;
     }
 }
