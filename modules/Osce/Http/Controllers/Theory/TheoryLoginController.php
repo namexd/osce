c29d7d06a428a054ad2c0859f21d11132893e813
Hsiaowei <phper.tang@qq.com>
2017-08-04 11:30:03 +0800

    update login

--- modules/Osce/Http/Controllers/Theory/TheoryLoginController.php
+++ modules/Osce/Http/Controllers/Theory/TheoryLoginController.php
@@ -37,19 +37,19 @@ class TheoryLoginController extends Controller
                 $isExist = Student::where('exam_id', $test->exam_id)->where('user_id', $userid)->first();
                 if (empty($isExist)) {
                     \Auth::logout();
-                    return redirect()->back()->withErrors('你不属于当前考试');
+                    return view('osce::theory.theory_login')->withErrors('你不属于当前考试');
                 }
                 $isAnswer = TestRecord::where(['logid' => $test->id, 'stuid' => $userid])->first();
                 if (!empty($isAnswer)) {
                     \Auth::logout();
-                    return redirect()->back()->withErrors('你已经参加过当前考试');
+                    return view('osce::theory.theory_login')->withErrors('你已经参加过当前考试');
                 }
                 session(['enterTime' => date('Y-m-d H:i:s')]);
                 return redirect()->route('osce.cexam.examinfo', ['testlog_id' => $test->id]);
 
             } else {
                 \Auth::logout();
-                return redirect()->back()->withErrors('当前时间没有考试');
+                return view('osce::theory.theory_login')->withErrors('当前时间没有考试');
             }
         }
         return view('osce::theory.theory_login');
