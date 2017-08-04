49bf5d1ca7fedd24205295fad5b7aeb7efaae8f7
fandian <fandian@misrobot.com>
2017-08-04 17:02:32 +0800

    增加对应模板

---
+++ modules/Osce/Entities/TestStatistics.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j5110
+ * Date: 2015/12/28
+ * Time: 12:01
+ */
+
+namespace Modules\Osce\Entities;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+
+class TestStatistics extends Model
+{
+    protected $connection = 'osce_mis';
+    protected $table='g_test_statistics';
+    protected $primaryKey='id';
+    public $timestamps=false;
+    protected $guarded      = [];
+    protected $hidden       = [];
+    protected $fillable     = [
+        'id','logid','stuid','objective','subjective','time','ifexam'
+    ];
+
+
+  public function student(){
+      return $this->hasOne('App\Entities\User', 'id', 'stuid');
+  }
+
+    public  function logdata(){
+        return $this->hasOne('Modules\Osce\Entities\TestLog', 'id', 'logid');
+    }
+
+
+}
+
+
