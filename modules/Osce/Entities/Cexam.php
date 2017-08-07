480eca8bae0094cddaef98bf0b7c7a7a275e42eb
Hsiaowei <phper.tang@qq.com>
2017-08-07 15:58:00 +0800

    add score

--- modules/Osce/Entities/Cexam.php
+++ modules/Osce/Entities/Cexam.php
@@ -126,7 +126,7 @@ class Cexam extends CommonModel
             ->join('g_test_content','g_test_content.id','=','g_test_record.cid')
             ->join('g_test_statistics','g_test_statistics.logid','=','g_test_log.id')
             ->join('g_test','g_test.id','=','g_test_log.tid')
-            ->select('g_test_record.*','student.name as stuname','g_test_content.question','g_test_content.answer as rightanswer','g_test_statistics.time as alltime','g_test_statistics.objective','g_test_content.content','g_test_content.poins as score','g_test.name as examname','g_test.score as examscore')
+            ->select('g_test_record.*','g_test_record.poins as score','g_test_record.score as poins','student.name as stuname','g_test_content.question','g_test_content.answer as rightanswer','g_test_statistics.time as alltime','g_test_statistics.objective','g_test_content.content','g_test_content.poins as score','g_test.name as examname','g_test.score as examscore','g_test_log.times')
             ->where('g_test_log.id',$logid)
             ->where('g_test_record.stuid',$stuid)
             ->orderBy('g_test_log.id')
