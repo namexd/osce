796289830bd875a8b48b781c9a2ad644d6cbd9a5
fandian <fandian@misrobot.com>
2017-08-08 14:56:49 +0800

    增加状态

--- modules/Osce/Entities/TestStatistics.php
+++ modules/Osce/Entities/TestStatistics.php
@@ -21,7 +21,7 @@ class TestStatistics extends Model
     protected $guarded      = [];
     protected $hidden       = [];
     protected $fillable     = [
-        'id','logid','stuid','objective','subjective','time','ifexam'
+        'id','logid','stuid','objective','subjective','time','ifexam','status'
     ];
 
 
