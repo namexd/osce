336f31f174560a9191c0b1598ab85a8bd3301796
Hsiaowei <phper.tang@qq.com>
2017-08-22 15:01:15 +0800

    update route post to get

--- modules/Osce/Http/routes.php
+++ modules/Osce/Http/routes.php
@@ -18,7 +18,7 @@ Route::group(['prefix' => "osce", 'namespace' => 'Modules\Osce\Http\Controllers'
 		Route::get('index', ['uses'=>'TestController@index','as'=>'osce.theory.index']);//考试管理
 		Route::get('examquestion', ['uses'=>'TestController@examquestion', 'as' => 'osce.theory.examquestion']);//题库管理
 		Route::get('autoquestion', ['uses'=>'TestController@autoquestion', 'as' => 'osce.theory.autoquestion']);//自动组卷
-		Route::post('autoexampreview', ['uses'=>'TestController@autoexampreview', 'as' => 'osce.theory.autoexampreview']);//自动组卷提交
+		Route::get('autoexampreview', ['uses'=>'TestController@autoexampreview', 'as' => 'osce.theory.autoexampreview']);//自动组卷提交
 		Route::post('autoexam', ['uses'=>'TestController@autoexam', 'as' => 'osce.theory.autoexam']);//自动组卷提交
 		Route::post('onceautoexam', ['uses'=>'TestController@onceautoexam', 'as' => 'osce.theory.onceautoexam']);//重新组卷提交
 		Route::get('delquestion', ['uses'=>'TestController@delquestion', 'as' => 'osce.theory.delquestion']);//题库管理
