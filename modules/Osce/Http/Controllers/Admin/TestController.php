a5c152c2dd36ebe49e0d5661eaaae9839d6f917c
fandian <fandian@misrobot.com>
2018-02-02 17:06:47 +0800

    增加理伦考试查询分数

--- modules/Osce/Http/Controllers/Admin/TestController.php
+++ modules/Osce/Http/Controllers/Admin/TestController.php
@@ -366,6 +366,20 @@ class TestController extends CommonController
         //dd($data);
         return view('osce::theory.exam_score',['data'=>$data]);
     }
+    //学生对应理伦考试列表
+    public function studentlscore(){
+
+        $stuid = \Auth::user()->id;
+        $data = TestStatistics::join('g_test_log', 'g_test_statistics.logid', '=', 'g_test_log.id')
+            ->join('student','g_test_statistics.stuid','=','student.user_id')
+            ->join('g_test','g_test_log.tid','=','g_test.id')
+            ->select('g_test_statistics.objective','g_test_statistics.subjective','g_test_statistics.logid','g_test.name','student.name as stuname','student.user_id')
+            ->where('student.user_id', $stuid)
+            ->where('student.test_id','<>',0)
+            ->first();
+
+        return view('osce::theory.student_lscore',['data'=>$data]);
+    }
 
     public function examcheck(){
         $data = TestLog::where('end','<',date('Y-m-d H:i:s'))->paginate(10);
